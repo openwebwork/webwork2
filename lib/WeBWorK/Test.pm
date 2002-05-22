31bf7e417e32473e53690fca18cb2ea510d06636
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2002-05-22 19:17:00 +0000

    fixed it
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@284 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Test.pm
+++ lib/WeBWorK/Test.pm
@@ -1,15 +1,30 @@
 package WeBWorK::Test;
 
-# print() the page requested.
-# args: form data, course-env
-sub gen_page() {
-	print<<EOT;
-Content-type: text/html
+sub new($$$$$) {
+	my $class = shift;
+	my ($r, $courseEnvironment, $user, $key) = @_;
+	
+	my $self = {
+		request			=> $r,
+		courseEnvironment	=> $courseEnvironment,
+		user			=> $user,
+		key			=> $key,
+	};
+}
 
+sub go {
+	my $self = shift;
+	$self{request}->content_type("text/html");
+	$self{request}->send_http_header;
+print<<EOT;
 <html>
 <head><title>Welcome to Hell.</title></head>
 <body>
 <h1>There you go.</h1>
+<pre>
+user = $self{user}
+key = $self{key}
+</pre>
 </body>
 </html>
 EOT
