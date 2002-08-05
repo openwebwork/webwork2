c2353d2fa26145fe25f7ea961eaccbdf9cf8ee4d
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2002-08-05 21:50:53 +0000

    strict/warnings/=head1 NAME -sam
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@456 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Constants.pm
+++ lib/WeBWorK/Constants.pm
@@ -1,7 +1,20 @@
+################################################################################
+# WeBWorK mod_perl (c) 1995-2002 WeBWorK Team, Univeristy of Rochester
+# $Id$
+################################################################################
+
 package WeBWorK::Constants;
 
+=head1 NAME
+
+WeBWorK::Constants - provide constant values for other WeBWorK modules.
+
+=cut
+
+use strict;
+use warnings;
 use base qw(Exporter);
-our @EXPORT    = ();
+our @EXPORT    = qw();
 our @EXPORT_OK = qw(SECRET);
 
 use constant SECRET => 'fkjOPIiSUfeT6dm5pevSrM1xgFmsex7.Z/.6Wjcxqb9Pi4Zm9JUGygwv^FdG8^yth^*KbDFWMXiLtNDggWNA370llFj68JxNKMCyCeSJxCHRfU2P6br10HtPS!NvcaJ7';
