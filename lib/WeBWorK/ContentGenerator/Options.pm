b5350490d64d709975f24b50c76554d3250278d6
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2002-11-23 00:15:15 +0000

    finished this module. -sam
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@645 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Options.pm
+++ lib/WeBWorK/ContentGenerator/Options.pm
@@ -18,7 +18,7 @@ use Apache::Constants qw(:common);
 use CGI qw();
 use WeBWorK::ContentGenerator;
 use WeBWorK::DB::WW;
-use WeBWorK::Utils qw(formatDateTime);
+use WeBWorK::Utils qw(formatDateTime dequoteHere);
 
 sub initialize {
 	my $self = shift;
@@ -26,7 +26,8 @@ sub initialize {
 	my $ce = $self->{courseEnvironment};
 	
 	$self->{cldb} = WeBWorK::DB::Classlist->new($ce);
-	$self->{userName} = $r->param('user');
+	$self->{authdb} = WeBWorK::DB::Auth->new($ce);
+	$self->{user} = $self->{cldb}->getUser($r->param('user'));
 }
 
 sub path {
@@ -44,14 +45,97 @@ sub path {
 
 sub title {
 	my $self = shift;
-	my $user = $self->{cldb}->getUser($self->{userName});
 	
-	return "User Options for " . $user->first_name
-		. " " . $user->last_name;
+	return "User Options for " . $self->{user}->first_name
+		. " " . $self->{user}->last_name;
 }
 
 sub body {
 	my $self = shift;
+	my $r = $self->{r};
+	my $user = $self->{user};
+	
+	my $changeOptions = $r->param("changeOptions");
+	my $newP = $r->param("newPassword");
+	my $confirmP = $r->param("confirmPassword");
+	my $newA = $r->param("newAddress");
+	my $confirmA = $r->param("confirmAddress");
+		
+	print CGI::start_form(-method=>"POST", -action=>$r->uri);
+	print $self->hidden_authen_fields;
+	print CGI::h2("Change Password");
+	if ($changeOptions) {
+		if ($newP or $confirmP) {
+			if ($newP eq $confirmP) {
+				# possibly do some format checking?
+				eval { $self->{authdb}->setPassword($user->id, $newP) };
+				if ($@) {
+					print CGI::p("Couldn't change your
+					password: $@");
+				} else {
+					print CGI::p("Your password has been
+					changed.");
+				}
+			} else {
+				print CGI::p("The passwords you entered in the
+				New Password and Confirm Password fields don't
+				match. Please retype your new password and try
+				again.");
+			}
+		}
+	}
+	print CGI::table(
+		CGI::Tr(
+			CGI::td("New Password"),
+			CGI::td(CGI::password_field("newPassword")),
+		),
+		CGI::Tr(
+			CGI::td("Confirm Password"),
+			CGI::td(CGI::password_field("confirmPassword")),
+		),
+	);
+	print CGI::h2("Change Email Address");
+	if ($changeOptions) {
+		if ($newA or $confirmA) {
+			if ($newA eq $confirmA) {
+				# possibly do some format checking?
+				my $oldA = $user->email_address;
+				$user->email_address($newA);
+				eval { $self->{cldb}->setUser($user) };
+				if ($@) {
+					$user->email_address($oldA);
+					print CGI::p("Couldn't change your
+					email address: $@");
+				} else {
+					print CGI::p("Your email address has
+					been changed.");
+					$newA = $confirmA = "";
+				}
+			} else {
+				print CGI::p("The addresses you entered in the
+				New Address and Confirm Address fields don't
+				match. Please retype your new address and try
+				again.");
+			}
+		}
+	}
+	print CGI::table(
+		CGI::Tr(
+			CGI::td("Current Address"),
+			CGI::td($user->email_address),
+		),
+		CGI::Tr(
+			CGI::td("New Address"),
+			CGI::td(CGI::textfield("newAddress", $newA)),
+		),
+		CGI::Tr(
+			CGI::td("Confirm Address"),
+			CGI::td(CGI::textfield("confirmAddress", $confirmA)),
+		),
+	);
+	print CGI::br();
+	print CGI::submit("changeOptions", "Change User Options");
+	print CGI::end_form();
 	
 	return "";
 }
