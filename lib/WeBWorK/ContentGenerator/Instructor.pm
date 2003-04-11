b58ac7d9ce5d73a2aafd040c66bf4b9b539bf2b6
malsyned <malsyned@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-04-11 22:25:10 +0000

    Added Instructor.pm, the main menu for the instructor pages. -dennis
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@805 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/WeBWorK/ContentGenerator/Instructor.pm
@@ -0,0 +1,13 @@
+package WeBWorK::ContentGenerator::Instructor;
+
+=head1 NAME
+
+WeBWorK::ContentGenerator::Instructor - Menu interface to the Instructor pages
+
+=cut
+
+use strict;
+use warnings;
+use base qw(WeBWorK::ContentGenerator);
+use CGI qw();
+
