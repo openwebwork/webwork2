f81219a732560db9cc91fa069c2bad31bb8e841d
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-04-16 04:43:40 +0000

    added a dummy driver and schema. -sam
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@806 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/WeBWorK/DB/Schema/Null.pm
@@ -0,0 +1,64 @@
+################################################################################
+# WeBWorK mod_perl (c) 2000-2002 WeBWorK Project
+# $Id$
+################################################################################
+
+package WeBWorK::DB::Schema::Null;
+
+=head1 NAME
+
+WeBWorK::DB::Schema::Null - a dummy schema with no backend.
+
+=cut
+
+use strict;
+use warnings;
+
+use constant TABLES => qw(password permission key user set set_user problem problem_user);
+use constant STYLE  => "dummy";
+
+################################################################################
+# static functions
+################################################################################
+
+sub tables() {
+	return TABLES;
+}
+
+sub style() {
+	return STYLE;
+}
+
+################################################################################
+# constructor
+################################################################################
+
+sub new($$$$$) {
+	my ($proto, $driver, $table, $record, $params) = @_;
+	my $class = ref($proto) || $proto;
+	die "$table: unsupported table"
+		unless grep { $_ eq $table } $proto->tables();
+	die $driver->style(), ": style mismatch"
+		unless $driver->style() eq $proto->style();
+	my $self = {
+		driver => $driver,
+		table  => $table,
+		record => $record,
+		params => $params,
+	};
+	bless $self, $class;
+	return $self;
+}
+
+################################################################################
+# table access functions
+################################################################################
+
+sub list   { return ();    }
+sub exists { return 1;     }
+sub add    { return 0;     }
+sub get    { return undef; }
+sub put    { return 0;     }
+sub delete { return 0;     }
+
+1;
