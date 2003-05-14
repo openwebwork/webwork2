3964917b6a660109ce2251f08f744188902c19a2
malsyned <malsyned@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-05-14 20:57:28 +0000

    Added links to the old Professor pages. -Dennis
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@834 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/Index.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/Index.pm
@@ -23,14 +23,38 @@ sub title {
 
 sub body {
 	my $self = shift;
+	my $r = $self->{r};
+	my $ce = $self->{ce};
+	my $courseName = $ce->{courseName};
+	my $authen_args = $self->url_authen_args();
+
+	my $prof_url = $ce->{webworkURLs}->{oldProf};
+	my $full_url = "$prof_url?course=$courseName&$authen_args";
 	my $userEditorURL = "userList/?" . $self->url_args;
 	my $problemSetEditorURL = "problemSetList/?" . $self->url_args;
-	my $courseName = $self->{ce}->{courseName};
 
-	return CGI::p("\n".
-		CGI::a({href=>$userEditorURL}, "Users"). " - View and edit data and settings for users of $courseName" . CGI::br(). "\n".
-		CGI::a({href=>$problemSetEditorURL}, "Problem Sets"). " - View and edit settings for problem sets in $courseName".CGI::br()."\n"
-	)
+	return 
+		CGI::p("\n".
+			CGI::a({href=>$userEditorURL}, "Users"). " - View and edit data and settings for users of $courseName" . CGI::br(). "\n".
+			CGI::a({href=>$problemSetEditorURL}, "Problem Sets"). " - View and edit settings for problem sets in $courseName".CGI::br()."\n"
+		)."\n".CGI::hr()."\n".
+		CGI::p(
+			CGI::b("NOTE: ") . 
+			"The Instructor Tools in this preview release of WeBWorK
+			2.0 are not stable or complete.  If you reliable and
+			stable course editing features, than at this time you
+			will need to use the Professor tools from WeBWorK 1.8
+			or earlier.  Use the links below to go to the Professor
+			pages of your system's WeBWorK 1.x installation."
+		)."\n".
+		CGI::ul(
+			CGI::li(
+				CGI::a({-href=>$full_url}, "Go to Professor pages")
+			). 
+			CGI::li(
+				CGI::a({-href=>$full_url, -target=>"_new"}, "Open Professor pages in new window")
+			)
+		);
 }
 
 1;
