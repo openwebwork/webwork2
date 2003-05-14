b7ae160f6988abb2000a7ed9a5dfbe04c9dbe120
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-05-14 20:55:07 +0000

    Modified links subroutine so that it returns a string rather than an array.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@833 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor.pm
+++ lib/WeBWorK/ContentGenerator/Instructor.pm
@@ -11,5 +11,34 @@ use strict;
 use warnings;
 use CGI qw();
 
+ sub links {
+ 	my $self 		= shift;
+ 	
+ 	# keep the links from the parent
+ 	my $pathString 	= "";
+ 	
+	
+	my $ce = $self->{ce};
+	my $db = $self->{db};
+	my $userName = $self->{r}->param("user");
+	my $courseName = $ce->{courseName};
+	my $root = $ce->{webworkURLs}->{root};
+	my $permLevel = $db->getPermissionLevel($userName)->permission();
+	my $key = $db->getKey($userName)->key();
+	return "" unless defined $key;
+	
+	# new URLS
+	my $classList	= "$root/$courseName/instructor/userList/?". $self->url_authen_args();
+	my $problemSetList = "$root/$courseName/instructor/problemSetList/?". $self->url_authen_args();
+	
+	$pathString .="<hr>";
+	$pathString .= ($permLevel > 0
+			? CGI::a({-href=>$classList}, "Class List editor") . CGI::br()
+			: "");
+	$pathString .= ($permLevel > 0
+			? CGI::a({-href=>$problemSetList}, "Problem Set editor") . CGI::br()
+			: "");
+	return $self->SUPER::links(), $pathString;
+}
 
 1;
