1db9b9dbfe54746721481d63924add3f7bb79db2
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-05-28 03:30:45 +0000

    Problem Editor now creates a temporary file to edit ( adding .tmp to the file name) Refresh causes a redirect to the Problem.pm with a GET command. Some of the arguments are unnecessary and need to be cleaned up.
    The seed and display mode in the problem editor are not properly
    initialized from the problem set.
    
    Problem.pm has been modified to check for these arguments and to read
    the temporary source file, seed and displayMode.  These are passed
    through to a PG.pm object.
    
    PG also needed to be modified so that the source_file and seed values
    are overridden
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@925 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/PGProblemEditor.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/PGProblemEditor.pm
@@ -12,6 +12,8 @@ use strict;
 use warnings;
 use CGI qw();
 use WeBWorK::Utils qw(readFile);
+use Apache::Constants qw(:common REDIRECT);
+
 
 our $libraryName;
 our $rowheight;
@@ -21,6 +23,48 @@ sub title {
 	#FIXME  don't need the entire path  ??
 	return "Instructor Tools - PG Problem Editor for ". $self->{ce}->{problemPath};
 }
+sub go {
+	my $self 			= shift;
+	my ($setName, $problemNumber) = @_;
+	my $r 				= 	$self->{r};
+	my $ce				=	$self->{ce};
+	my $submit_button 	= $r->param('submit');  # obtain submit command from form
+
+	# various actions depending on state.
+	if (     defined($submit_button) and ($submit_button eq 'Save' or $submit_button eq 'Refresh')    ) {
+	
+		$self->initialize($setName,$problemNumber);  # write the necessary files
+													 # return file path for viewing problem
+													 # in $self->{ce}->{currentSourceFilePath}
+		#redirect to view the problem
+		
+		my $hostname 		= 	$r->hostname();
+		my $port     		= 	$r->get_server_port();
+		my $uri		 		= 	$r->uri;
+		my $courseName		=	$self->{ce}->{courseName};
+		my $editFileSuffix  =	$self->{ce}->{editFileSuffix};
+		my $seed			= 	($r->param('seed')) ? $r->param('seed') : '';
+		my $displayMode		=	($r->param('displayMode')) ? $r->param('displayMode') : '';
+
+		my $viewURL  		= 	"http://$hostname:$port";
+		$viewURL		   .= 	"/webwork/$courseName/$setName/$problemNumber/?";
+		$viewURL		   .=	$self->url_authen_args;
+		$viewURL		   .=   "&displayMode=$displayMode&seed=$seed";   # optional displayMode and seed overrides
+		$viewURL		   .=	"&editMode=temporaryFile";
+		$viewURL		   .=	'&sourceFilePath='.$self->{ce}->{currentSourceFilePath}; # path to pg text for viewing
+		$viewURL		   .=	"&submit_button=$submit_button";                         # allows Problem.pg to recognize state
+		$viewURL		   .=   '&editErrors='.$self->{ce}->{editErrors};																				 # of problem being viewed.
+		$r->header_out(Location => $viewURL );
+		return REDIRECT;
+	} else {
+		# initialize and 
+		# display the editing window
+		
+		$self->SUPER::go(@_);
+	}
+
+}
+
 
 sub body {
 	my $self = shift;
@@ -42,49 +86,48 @@ sub body {
 	#my ($problemPath,$formURL,$tmpProblemPath) = $self->initialize();
 	my $problemPath 	= 	$ce->{problemPath};
 
-	my $tmpProblemPath	=	$ce->{tmpProblemPath};
+	#my $tmpProblemPath	=	$ce->{tmpProblemPath};
 	
 	
 
 	
 	
 
-	my $header = 'Problem Editor';
+	my $header = "Problem Editor:  $problemPath";
 
 	#########################################################################
 	# Find the text for the problem, either in the tmp file, if it exists
 	# or in the original file in the template directory
 	#########################################################################
 	my $problemContents = '';
-	my $editMode		= (defined($r->param('problemContents')))? 
-								'tmpMode':'startMode';
-	
-	if ( $editMode eq 'tmpMode') {
-		$problemContents	=	$r->param('problemContents');
-
-	} else{
-		 eval { $problemContents	=	WeBWorK::Utils::readFile($problemPath)  };  # try to read file
-		 $problemContents = $@ if $@;
-	} 
+# 	my $editMode		= (defined($r->param('problemContents')))? 
+# 								'tmpMode':'startMode';
+# 	
+# 	if ( $editMode eq 'tmpMode') {
+# 		$problemContents	=	$r->param('problemContents');
+# 
+# 	} else{
+	eval { $problemContents	=	WeBWorK::Utils::readFile($problemPath)  };  # try to read file
+	$problemContents = $@ if $@;
+#	} 
 		
 	#  save Action  FIXME  -- is this the write place for this?
-	my $actionString = '';
-	if ($r->param('submit') eq 'Save') {
-		$actionString = "File saved to $problemPath";
-		#FIXME  it would be MUCH better to work with temporary files
-		open(FILE,">$problemPath") or die "Can't open $problemPath";
-		print FILE $problemContents;
-		close(FILE);
-		
-	}
+# 	my $actionString = '';
+# 	if ($r->param('submit') eq 'Save') {
+# 		$actionString = "File saved to $problemPath";
+# 		#FIXME  it would be MUCH better to work with temporary files
+# 		open(FILE,">$problemPath") or die "Can't open $problemPath";
+# 		print FILE $problemContents;
+# 		close(FILE);
+# 		
+#	}
+
 	
-		# create tmp file version for later use 
-		# So that you don't need duplicate information in forms
-		# FIXME
-		# open(FILE,">$tmpProblemPath") || die "Failed to open $tmpProblemPath";
-		# print FILE "tmpfileVersion\n\n" . $problemContents;
-		# close(FILE);
-		
+			
+			
+	#########################################################################
+	# Format the page
+	#########################################################################
 	# Define parameters for textarea
 	# FIXME these parameters should be capable of being updated dynamically.
 	my $rows 		= 	20;
@@ -92,6 +135,7 @@ sub body {
 	my $mode_list 	= 	['HTML', 'HTML_tth','HTML_dpng', 'Latex2HTML'];
 	my $mode	  	= 	( defined($r->param('mode')) 	) ? $r->param('mode') : 'HTML_tth';
 	my $seed		=	( defined($r->param('seed'))	) ? $r->param('seed') : '1234';	
+	my $uri			=	$r->uri;
 	########################################################################
 	# Define a link to view the problem
 	#FIXME
@@ -100,12 +144,11 @@ sub body {
 
 	
 
-	#########################################################################
-	# Format the page
-	#########################################################################
-	           
+
+			   
 	return CGI::p($header),
-		CGI::startform("POST",$r->uri),
+		#CGI::start_form("POST",$r->uri,-target=>'_problem'),  doesn't pass on the target parameter???
+		qq!<form method="POST" action="$uri" enctype="application/x-www-form-urlencoded", target="_problem">!,
 		$self->hidden_authen_fields,
 		CGI::div(
 		CGI::textfield(-name=>'seed',-value=>$seed),
@@ -123,25 +166,32 @@ sub body {
 			),
 		),
 		CGI::p(
+			CGI::submit(-value=>'Refresh',-name=>'submit'),
 			CGI::submit(-value=>'Save',-name=>'submit'),
-			$actionString
+#			$actionString
 		),
-		CGI::a({-href=>$ce->{viewProblemURL},-target=>'_viewProblem'},'view problem'),
+		
+		#CGI::a({-href=>$ce->{viewProblemURL},-target=>'_viewProblem'},'view problem'),
 		CGI::end_form(),
 		"<p> the parameters passed are "  #FIXME -- debugging code
 		. join("<BR>", %{$r->param()}) . 
 		"</p> and the gatheredInfo is ",
-		"problemPath=$problemPath<br> formURL=".$r->uri . "<br> tmpProblemPath=$tmpProblemPath<br>"   ,
-		"viewProblemURL ".$ce->{viewProblemURL}."<br>",
-		"problem_obj =". $ce->{problem_obj}."<br>",
-		"path_components ". $ce->{path_components};
+		"problemPath=$problemPath<br> formURL=".$r->uri . "<br>"   ,
+#		"viewProblemURL ".$ce->{viewProblemURL}."<br>",
+#		"problem_obj =". $ce->{problem_obj}."<br>",
+		"path_components ". $ce->{path_components}.'<br>',
+# 		"hostname =$hostname<br>",
+# 		"port =$port <br>",
+ 		"uri = $uri <br>",
+# 		"viewURL =".$ce->{viewURL}."<br>",
 		 
 	;
+
 
 }
 
 sub initialize {
-	#fix me.  This is very much hacked together.  In particular can we pass the key inside the post?
+	
 	my ($self, $setName, $problemNumber) = @_;
 	my $ce 						= 	$self->{ce};
 	my $r						=	$self->{r};
@@ -153,14 +203,75 @@ sub initialize {
 	
 	my $set            			= 	$db->getGlobalUserSet($effectiveUserName, $setName);
 	my $setID					=	$set->set_id;
-	# Find URL for viewing problem
-	my $viewProblemURL		=	"/webwork/$courseName/".join("/",$setID,$problemNumber)."?" .$self->url_authen_args();
 	
-	my $problem_record		=	$db->getUserProblem($user,$setID,1);
+	# Find URL for viewing problem
+#	my $viewProblemURL		=	"/webwork/$courseName/".join("/",$setID,$problemNumber)."?" .$self->url_authen_args();
+	
+	# find path to pg file for the problem
+	# FIXME  there is a descrepency in the way that the problems are found.
+	# my $problem_record		=	$db->getUserProblem($user,$setID,1);
+	my $problem_record          = 	$db->getGlobalUserProblem($effectiveUserName, $setName, $problemNumber);
 	my $templateDirectory	=	$ce->{courseDirs}->{templates};
 	my $problemPath			=	$templateDirectory."/".$problem_record->source_file;
+	my $editFileSuffix		=	'tmp';
+	my $submit_button		= 	$r->param('submit');
+	
+	my $problemContents	= '';
+	my $currentSourceFilePath	=	'';
+	# update the .pg and .pg.tmp files in the directory
+	if (not defined($submit_button) ) {
+		# this is a fresh editing job
+		# copy the pg file to a new file with the same name with .tmp added
+		# store this name in the $ce->currentSourceFilePath for use in body 
+		
+		eval { $problemContents			=	WeBWorK::Utils::readFile($problemPath)  
+		};  # try to read file
+		$problemContents = $@ if $@;
+		$currentSourceFilePath			=	"$problemPath.$editFileSuffix"; 
+		$ce->{currentSourceFilePath}	=	$currentSourceFilePath; 
+	} elsif ($submit_button	eq 'Refresh' ) {
+		# grab the problemContents from the form and save it to the tmp file
+		# store tmp file name in the $ce->currentSourceFilePath for use in body 
+		
+		$problemContents				=	$r->param('problemContents');
+		$currentSourceFilePath			=	"$problemPath.$editFileSuffix";	
+		$ce->{currentSourceFilePath}	=	$currentSourceFilePath;
+	} elsif ($submit_button eq 'Save') {
+		# grab the problemContents from the form and save it to the permanent file
+		# unlink (delete) the temporary file
+		# store the permanent file name in the $ce->problemContents for use in body 
+		
+		$problemContents				=	$r->param('problemContents');
+		$currentSourceFilePath			=	"$problemPath"; 		
+		$ce->{currentSourceFilePath}	=	$currentSourceFilePath;		
+	} else {
+		# give a warning
+		die "Unrecognized submit command $submit_button";
+	}
+	# print changed pg files
+	# FIXME  make sure that the permissions are set correctly!!!
+	# Make sure that the warning is being transmitted properly.
+	eval {
+		local *OUTPUTFILE;
+		open OUTPUTFILE, ">", $currentSourceFilePath
+				or die "Failed to write to $currentSourceFilePath: $!";
+		print OUTPUTFILE $problemContents;
+		close OUTPUTFILE;
+	};
+	my $errors = $@ if $@;
+	if (  $errors)   {
+	
+		$ce->{editErrors}	= "Unable to write to $currentSourceFilePath: $errors";
+		
+	} else {	# unlink the temporary file if there are no errors.
+		$ce->{editErrors}	=	'';
+		unlink("$problemPath.$editFileSuffix") if defined($submit_button) and $submit_button eq 'Save';
+		
+	};
+	
+		
 	# return values.  FIXME  -- is this the right way to pass the values to body??
-	$ce->{viewProblemURL}	=	$viewProblemURL;
+	# $ce->{viewProblemURL}	=	$viewProblemURL;
 	$ce->{problemPath} 		= 	$problemPath;
 	$ce->{path_components}	=	join("/",$setID,$problemNumber);
 	
