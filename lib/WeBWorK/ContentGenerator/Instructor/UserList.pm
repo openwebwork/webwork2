6e0e73bb660c3faea345ea66b22983136e0bfe77
malsyned <malsyned@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-06-10 22:40:36 +0000

    Added the beginings of a classlist editor.  It currently only displays the class list in a form. -Dennis
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1110 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/UserList.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/UserList.pm
@@ -12,19 +12,6 @@ use warnings;
 use CGI qw();
 
 sub initialize {
-	my ($self, $setID) = @_;
-	my $r = $self->{r};
-	my $authz = $self->{authz};
-	my $user = $r->param('user');
-	
-	unless ($authz->hasPermissions($user, "assign_problem_sets")) {
-		$self->{submitError} = "You are not authorized to assign problem sets";
-		return;
-	}
-	
-	if (defined $r->param('assignToAll')) {
-		$self->assignSetToAllUsers($setID);
-	}
 }
 
 sub body {
@@ -32,14 +19,38 @@ sub body {
 	my $r = $self->{r};
 	my $authz = $self->{authz};
 	my $user = $r->param('user');
+	my $db = $self->{db};
 	
         return CGI::em("You are not authorized to access the Instructor tools.") unless $authz->hasPermissions($user, "access_instructor_tools");
 
-	
-	print CGI::start_form({method=>"post", action=>$r->uri});
-	print $self->hidden_authen_fields;
-	print CGI::submit({name=>"assignToAll", value=>"Assign to All Users"});
-	print CGI::end_form();
+	my @users = $db->listUsers;
+	print CGI::start_table({});
+	print CGI::Tr({},
+		CGI::td({}, [
+			$db->{user}->{record}->FIELDS(),
+			grep {! m/^user_id$/} $db->{permission}->{record}->FIELDS(),
+		])
+	);
+	foreach my $currentUser (@users) {
+		my $userRecord = $db->getUser($currentUser);
+		my $permissionLevel = $db->getPermissionLevel($currentUser);
+		
+		print CGI::Tr({},
+			CGI::td({}, [
+				(map {CGI::input({type=>"text", size=>"8", value=>$userRecord->$_})} $userRecord->FIELDS()), 
+				(map {CGI::input({type=>"text", size=>"8", value=>$permissionLevel->$_})} grep {! m/^user_id$/} $permissionLevel->FIELDS()),
+			])
+		);
+		
+#		foreach my $key ($userRecord->FIELDS) {
+#			print "$key: ", $userRecord->$key, CGI::br();
+#		}
+#		foreach my $key ($permissionLevel->FIELDS) {
+#			print "$key: ", $permissionLevel->$key, CGI::br();
+#		}
+#		print CGI::p();
+	}
+	print CGI::end_table();
 	
 	return "";
 }
