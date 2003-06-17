bc666ad0bce46a91a60aa248919336bdf39d7e57
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-06-17 18:30:42 +0000

    documentation.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1182 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Timing.pm
+++ lib/WeBWorK/Timing.pm
@@ -9,6 +9,35 @@ package WeBWorK::Timing;
 
 WeBWorK::Timing - Log timing data.
 
+head1 SYNOPSIS
+
+ use WeBWorK::Timing;
+ 
+ my $timer = WeBWorK::Timing->new("do some processesing");
+ $timer->start;
+ do_some_processing();
+ $timer->continue("
+ do_some_more_processing();
+ $timer->stop;
+ $timer->save;
+ 
+ my $timer0 = WeBWorK::Timing->new("main task");
+ my $timer1 = WeBWorK::Timing->new("subtask 1");
+ my $timer2 = WeBWorK::Timing->new("subtask 1");
+ 
+ $timer0->start;
+ $timer1->start;
+ sub_task(1);
+ $timer1->stop;
+ $timer2->start;
+ sub_task(2);
+ $timer2->stop;
+ $timer0->stop;
+ 
+ $timer0->save;
+ $timer1->save;
+ $timer2->save;
+
 =cut
 
 use strict;
@@ -16,6 +45,18 @@ use warnings;
 use Time::HiRes qw(gettimeofday tv_interval);
 
 our $TASK_COUNT = 0; # number of tasks processed in this child process
+
+=head1 CONSTRUCTOR
+
+=over
+
+=item new($task)
+
+C<new> creates a new timing object, with the task given in $task.
+
+=back
+
+=cut
 
 sub new {
 	my ($invocant, $task) = @_;
@@ -27,20 +68,54 @@ sub new {
 	return bless $self, ref $invocant || $invocant
 }
 
+=head1 METHODS
+
+=over
+
+=item start(), begin()
+
+Marks the current time as the start time for the task.
+
+=cut
+
 sub start {
 	my ($self) = @_;
 	$self->{start} = gettimeofday();
 }
+
+sub begin { shift->start(@_); }
+
+=item continue($data)
+
+Stores the current time as an intermediate time, associated with the string
+given in $data.
+
+=cut
 
 sub continue {
 	my ($self, $data) = @_;
 	push @{$self->{steps}}, [ scalar gettimeofday(), $data ];
 }
 
-sub finish {
+=item stop(), finish(), end()
+
+Marks the current time as the stop time for the task.
+
+=cut
+
+sub stop {
 	my ($self) = @_;
-	$self->{finish} = gettimeofday();
+	$self->{stop} = gettimeofday();
 }
+
+sub finish { shift->stop(@_); }
+sub end    { shift->stop(@_); }
+
+=item save()
+
+Writes the timing data for this task to the standard error stream.
+
+=cut
 
 sub save {
 	my ($self) = @_;
@@ -65,13 +140,27 @@ sub save {
 		}
 	}
 	
-	if ($self->{finish}) {
-		my $finish = sprintf("%.6f", $self->{finish});
-		print STDERR "TIMING $$ $id $finish $task: FINISH\n";
+	if ($self->{stop}) {
+		my $stop = sprintf("%.6f", $self->{stop});
+		print STDERR "TIMING $$ $id $stop $task: END\n";
 	} else {
 		$now = sprintf("%.6f", $now);
-		print STDERR "TIMING $$ $id $now $task: FINISH (assumed)\n";
+		print STDERR "TIMING $$ $id $now $task: END (assumed)\n";
 	}
 }
+
+=head1 AUTHOR
+
+Written by Sam Hathaway, sh002i (at) math.rochester.edu.
+
+=head1 BUGS
+
+Currently outputs to STDERR instead of something more graceful.
+
+=head1 SEE ALSO
+
+The F<timing> utility can be used to parse and sort log output.
+
+=cut
 
 1;
