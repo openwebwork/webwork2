1ed85922a51e5745bc9cb969a6201d9155a78278
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-09-06 15:56:58 +0000

    Added the ability to enter a new student from this page.  It is much faster than from the class list.
    Of course it doesn't have full functionality yet.
    --Mike
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1486 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/Index.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/Index.pm
@@ -15,7 +15,57 @@ WeBWorK::ContentGenerator::Instructor::Index - Menu interface to the Instructor 
 use strict;
 use warnings;
 use CGI qw();
+sub initialize {
+	my ($self) = @_;
+	my $r = $self->{r};
+	my $db = $self->{db};
+	my $ce = $self->{ce};
+	my $authz = $self->{authz};
+	my $user = $r->param('user');
 
+	unless ($authz->hasPermissions($user, "modify_student_data")) {
+		$self->{submitError} = "You are not authorized to modify student data";
+		return;
+	}
+
+	if (defined($r->param('addStudent'))) {
+		my $newUser = $db->newUser;
+		my $newPermissionLevel = $db->newPermissionLevel;
+		my $newPassword = $db->newPassword;
+		$newUser->user_id($r->param('new_user_id'));
+		$newPermissionLevel->user_id($r->param('new_user_id'));
+		$newPassword->user_id($r->param('new_user_id'));
+		$newUser->last_name($r->param('last_name'));
+		$newUser->first_name($r->param('first_name'));
+		$newUser->student_id($r->param('student_id'));
+		$newUser->email_address($r->param('email_address'));
+		$newUser->section($r->param('section'));
+		$newUser->recitation($r->param('recitation'));
+		$newUser->comment($r->param('comment'));
+		$newUser->status('C');
+		$newPermissionLevel->permission(0);
+		#FIXME  handle errors if user exists already
+		$db->addUser($newUser);
+		$db->addPermissionLevel($newPermissionLevel);
+		$db->addPassword($newPassword);
+	 	$self->{studentEntryReport} = join("",
+	 		"Entered student", CGI::br(), 
+	        "Name: ", $newUser->last_name, ", ",$newUser->first_name,CGI::br(),
+	        " login/studentID: ", $newUser->user_id, "/",$newUser->student_id,CGI::br(),
+	        " email: ", $newUser->email_address,CGI::br(),
+	        " section: ", $newUser->section,CGI::br(),
+	       
+	    );
+	}
+
+
+
+
+
+
+
+
+}
 sub path {
 	my $self          = shift;
 	my $args          = $_[-1];
@@ -85,9 +135,53 @@ sub body {
 		),
 		
 		CGI::end_table(),
+		CGI::hr(),
+		CGI::p( defined($self->{studentEntryReport}) ? $self->{studentEntryReport}:''
+		),
+		$self->addStudentForm,
 	);
 }
+sub addStudentForm {
+	my $self = shift;
+	my $r = $self->{r};
+	
+	# Add a student form
+	join( "",
+		CGI::p("Add new students"),	
+		CGI::start_form({method=>"post", action=>$r->uri()}),
+		$self->hidden_authen_fields(),
+		CGI::start_table({border=>'1', cellpadding=>'2'}),
+		CGI::Tr({},
+			CGI::th({},
+				['Last Name', 'First Name', 'Student ID', 'Login Name', 'Email Address', 'Section','Recitation', 'Comment']
+			)
+		),
+		CGI::Tr({},
+			CGI::td({},
+				[ CGI::input({name=>'last_name'}),
+				  CGI::input({name=>'first_name'}),
+				  CGI::input({name=>'student_id',size=>'16'}),
+				  CGI::input({name=>'new_user_id',size=>'10'}),
+				  CGI::input({name=>'email_address'}),
+				  CGI::input({name=>'section',size=>'10'}),
+				  CGI::input({name=>'recitation',size=>'10'}),
+				  CGI::input({name=>'comment'}),
+				
+				
+				]
+			)
+		),
+		CGI::end_table(),
+		CGI::submit({name=>"addStudent", value=>"Add Student"}),
+		CGI::end_form(),
+	);
 
+
+
+
+
+
+}
 1;
 
 __END__
