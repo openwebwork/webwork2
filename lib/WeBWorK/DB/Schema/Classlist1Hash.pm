c4ef61b14b2ba8447268d7c019a2f56ec0ef4a39
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2003-12-09 01:12:32 +0000

    Normalized headers. All files now contain the text below as a header. This is important since all files now (a) use the full name of the package, (b) assign copyright to "The WeBWorK Project", (c) give the full path of the file (relative to CVSROOT) instead of simply the file name, and (d) include license and warranty information.
    Here is the new header:
    
    ################################################################################
    # WeBWorK Online Homework Delivery System
    # Copyright © 2000-2003 The WeBWorK Projcct, http://openwebwork.sf.net/
    # $CVSHeader$
    #
    # This program is free software; you can redistribute it and/or modify it under
    # the terms of either: (a) the GNU General Public License as published by the
    # Free Software Foundation; either version 2, or (at your option) any later
    # version, or (b) the "Artistic License" which comes with this package.
    #
    # This program is distributed in the hope that it will be useful, but WITHOUT
    # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
    # FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
    # Artistic License for more details.
    ################################################################################
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1663 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/DB/Schema/Classlist1Hash.pm
+++ lib/WeBWorK/DB/Schema/Classlist1Hash.pm
@@ -1,6 +1,17 @@
 ################################################################################
-# WeBWorK mod_perl (c) 2000-2002 WeBWorK Project
-# $Id$
+# WeBWorK Online Homework Delivery System
+# Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
+# $CVSHeader$
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
 ################################################################################
 
 package WeBWorK::DB::Schema::Classlist1Hash;
