b7a9de4089fdab4baf1c67b6e793494e5b0544ee
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-01-23 22:04:20 +0000

    Removed warning if a single tie fails.  If many ties fail then the processes dies with a message of how many ties were attempted. --Mike
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1745 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/DB/Driver/GDBM.pm
+++ lib/WeBWorK/DB/Driver/GDBM.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader$
+# $CVSHeader: webwork-modperl/lib/WeBWorK/DB/Driver/GDBM.pm,v 1.7 2003/12/09 01:12:31 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -63,16 +63,18 @@ sub connect($$) {
 		$self->disconnect;
 	}
 	my $flags = lc $mode eq "rw" ? GDBM_WRCREAT() : GDBM_READER();
+	my $error_message;
 	foreach (1 .. MAX_TIE_ATTEMPTS) {
 		return 1 if tie %$hash,
 			"GDBM_File",    # class
 			$source,        # file name
 			$flags,         # I/O flags
 			TIE_PERMISSION; # access mode
-		warn __PACKAGE__, ": tie failed with error $!, (attempt $_)\n";
+		#warn __PACKAGE__, ": tie failed with error $!, (attempt $_)\n";
+		$error_message    =  ": tie failed with error $!, (attempt $_)\n";
 		sleep TIE_RETRY_DELAY;
 	}
-	die "$source: connection failed";
+	die "$source: connection failed.  $error_message";
 }
 
 sub disconnect($) {
