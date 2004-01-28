c506b67bfd72f2da31a658c79be913efd0646fc1
toenail <toenail@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-01-28 21:56:26 +0000

    Content Generator to create an error message while still logging the user in.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1763 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/WeBWorK/ContentGenerator/Error.pm
@@ -0,0 +1,56 @@
+################################################################################
+# WeBWorK mod_perl (c) 2000-2002 WeBWorK Project
+# $Id$
+################################################################################
+
+package WeBWorK::ContentGenerator::Error;
+use base qw(WeBWorK::ContentGenerator);
+
+=head1 NAME
+
+WeBWorK::ContentGenerator::Error - display debugging information.
+
+=cut
+
+use strict;
+use warnings;
+use CGI qw();
+use WeBWorK::Form;
+use WeBWorK::Utils qw(ref2string);
+
+sub initialize {
+	my $self = shift;
+}
+
+sub path {
+	my $self = shift;
+	my $args = $_[-1];
+	return $self->pathMacro($args, Home => "../", Error => "");
+}
+
+sub siblings {
+	my $self = shift;
+	return "";
+}
+
+sub nav {
+	my $self = shift;
+	return "";
+}
+
+sub title {
+	my $self = shift;
+	my $error = shift;
+	(!defined $error or $error eq "") ? return "An Error Occurred" : return $error;
+}
+
+sub body {
+	my $self = shift;
+	my $error = shift;
+	my $msg = shift;
+	my $formFields = WeBWorK::Form->new_from_paramable($self->{r});
+	my $courseEnvironment = $self->{ce};
+	( defined $msg and $msg eq "") ? return $self->errorOutput() : return $msg;
+}
+
+1;
