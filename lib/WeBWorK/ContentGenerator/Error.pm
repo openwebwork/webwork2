aaae56dbd8a6375d3b027e43fc484efdc0a7f41e
toenail <toenail@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-02-21 10:13:23 +0000

    Eliminated direct parameter calls, error message now passed through $r
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1820 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Error.pm
+++ lib/WeBWorK/ContentGenerator/Error.pm
@@ -19,49 +19,56 @@ use WeBWorK::Form;
 use WeBWorK::Utils qw(ref2string);
 
 sub initialize {
-#	my $self = shift;
-	my ($self, $setID, $problemID, $error, $msg) = @_;
-
+	my $self = shift;
 }
 
 sub path {
-	my ($self, $setID, $problemID, $error, $msg) = @_;
-
-#	my $self = shift;
+	my $self = shift;
+	my $r = $self->{r};
+	my $problemID = $r->param("problem");
 	my $args = $_[-1];
-	return $self->pathMacro($args, Home => "../", Error => "");
+
+	#make sure the Home link will point to the right place
+	my $home = (defined $problemID && $problemID ne "") ? "../../" : "../";
+
+	return $self->pathMacro($args, Home => $home, Error => "");
 }
 
 sub siblings {
-	my ($self, $setID, $problemID, $error, $msg) = @_;
+	my $self = shift;
 
-#	my $self = shift;
-	return "";
+	return "";	
 }
 
 sub nav {
-	my ($self, $setID, $problemID, $error, $msg) = @_;
+	my $self = shift;
 
-#	my $self = shift;
 	return "";
 }
 
 sub title {
-	my ($self, $setID, $problemID, $error, $msg) = @_;
-#	my $self = shift;
-#	my $error = shift;
-	(!defined $error or $error eq "") ? return "An Error Occurred" : return $error;
+	my $self = shift;
+	my $r = $self->{r};
+	my $error = $r->param("error");
+
+	(!defined $error  or $error eq "") ? return "An Error Occurred" : return $error;
 }
 
 sub body {
-	my ($self, $setID, $problemID, $error, $msg) = @_;
+	my $self = shift;
+	my $r = $self->{r};
+	my $error = $r->param("error");
+	my $msg = $r->param("msg");
 
-#	my $self = shift;
-#	my $error = shift;
-#	my $msg = shift;
 	my $formFields = WeBWorK::Form->new_from_paramable($self->{r});
 	my $courseEnvironment = $self->{ce};
-	( defined $msg and $msg eq "") ? return $self->errorOutput() : return $msg;
+	( !defined $msg or $msg eq "") ? return $self->errorOutput() : return $msg;
 }
 
+sub errorOutput() {
+	return "An error has occured while processing the requested action.  
+		If you typed a URL, check that it is correct.  
+		If you submitted a form, you may not have entered all the necessary information.  
+		If you believe there is a bug, please report it to your professor.";
+}
 1;
