ec16d70d9feae714971877a1f5c756a9bea17de2
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-03-01 06:33:34 +0000

    implemented UI using ScrollingRecordList
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1825 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/Assigner.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/Assigner.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Instructor/Assigner.pm,v 1.13 2004/01/21 20:53:32 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Instructor/Assigner.pm,v 1.14 2004/01/23 16:49:09 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -26,19 +26,16 @@ WeBWorK::ContentGenerator::Instructor::Assigner - Assign problem sets to users.
 use strict;
 use warnings;
 use CGI qw();
-
-sub initialize {
-	my ($self, $setID) = @_;
-}
+use WeBWorK::HTML::ScrollingRecordList qw/scrollingRecordList/;
 
 sub path {
 	my $self          = shift;
-    my @components    = @_;
 	my $args          = $_[-1];
 	
 	my $ce = $self->{ce};
 	my $root = $ce->{webworkURLs}->{root};
 	my $courseName = $ce->{courseName};
+	
 	return $self->pathMacro($args,
 		"Home"             => "$root",
 		$courseName        => "$root/$courseName",
@@ -48,15 +45,65 @@ sub path {
 }
 
 sub title {
-	my ($self, @components) = @_;
+	my ($self) = @_;
 	return "Set Assigner"
 }
 
 sub body {
-	my ($self, $setID) = @_;
+	my ($self) = @_;
+	my $r = $self->{r};
+	my $db = $self->{db};
 	
-	print CGI::p("This module will allow the assignment of multiple sets to"
-		. "multiple users, all in one shot. First, we write it.");
+	print CGI::p("Select one or more sets and one or more users below to assign"
+		. "each selected set to all selected users.");
+	
+	my @userIDs = $db->listUsers;
+	my @Users = $db->getUsers(@userIDs);
+	
+	my $scrolling_user_list = scrollingRecordList({
+			name => "selected_users",
+			request => $r,
+			default_sort => "lnfn",
+			default_format => "lnfn_uid",
+			size => 20,
+			multiple => 1,
+		}, @Users);
+	
+	my @globalSetIDs = $db->listGlobalSets;
+	my @GlobalSets = $db->getGlobalSets(@globalSetIDs);
+	
+	my $scrolling_set_list = scrollingRecordList({
+		name => "selected_sets",
+		request => $r,
+		default_sort => "set_id",
+		default_format => "set_id",
+		size => 20,
+		multiple => 1,
+	}, @GlobalSets);
+	
+	print CGI::start_form({method=>"post", action=>$r->uri()});
+	print $self->hidden_authen_fields();
+	
+	print CGI::table({class=>"layout"},
+		CGI::Tr(
+			CGI::th("Users"),
+			CGI::th("Sets"),
+		),
+		CGI::Tr(
+			CGI::td({-align=>"center"}, $scrolling_user_list),
+			CGI::td({-align=>"center"}, $scrolling_set_list),
+		),
+		CGI::Tr(
+			CGI::td({colspan=>2, align=>"center"},
+				CGI::submit(
+					-name => "assign",
+					-value => "Assign selected sets to selected users",
+				),
+			),
+		),
+	);
+	
+	print CGI::end_form();
 	
 	return "";
 }
