adabc93c7bb40ab502a7dd2f9c77a3d582f46df5
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-03-11 03:15:03 +0000

    updated to use the "unified request" object. removed unused variables and subroutines.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1875 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Error.pm
+++ lib/WeBWorK/ContentGenerator/Error.pm
@@ -18,51 +18,30 @@ use CGI qw();
 use WeBWorK::Form;
 use WeBWorK::Utils qw(ref2string);
 
-sub initialize {
-	my $self = shift;
-}
-
-sub path {
-	my $self = shift;
-	my $r = $self->{r};
-	my $problemID = $r->param("problem");
-	my $args = $_[-1];
-
-	#make sure the Home link will point to the right place
-	my $home = (defined $problemID && $problemID ne "") ? "../../" : "../";
-
-	return $self->pathMacro($args, Home => $home, Error => "");
-}
-
-sub siblings {
-	my $self = shift;
-
-	return "";	
-}
-
-sub nav {
-	my $self = shift;
-
-	return "";
-}
-
 sub title {
 	my $self = shift;
 	my $r = $self->{r};
+	
 	my $error = $r->param("error");
-
-	(!defined $error  or $error eq "") ? return "An Error Occurred" : return $error;
+	
+	if (!defined $error  or $error eq "") {
+		return "An Error Occurred";
+	} else {
+		return $error;
+	}
 }
 
 sub body {
 	my $self = shift;
-	my $r = $self->{r};
-	my $error = $r->param("error");
+	my $r = $self->r;
+	
 	my $msg = $r->param("msg");
-
-	my $formFields = WeBWorK::Form->new_from_paramable($self->{r});
-	my $courseEnvironment = $self->{ce};
-	( !defined $msg or $msg eq "") ? return $self->errorOutput() : return $msg;
+	
+	if (!defined $msg or $msg eq "") {
+		return $self->errorOutput();
+	} else {
+		return $msg;
+	}
 }
 
 sub errorOutput() {
@@ -71,4 +50,5 @@ sub errorOutput() {
 		If you submitted a form, you may not have entered all the necessary information.  
 		If you believe there is a bug, please report it to your professor.";
 }
+
 1;
