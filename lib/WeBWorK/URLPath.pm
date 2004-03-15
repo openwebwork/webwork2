0195d83a452a0d8d02cf0e81bb42367f3f0738a2
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-03-15 22:24:02 +0000

    made hardcopy_preselect_set a child of hardcopy (as it should be)
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1890 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/URLPath.pm
+++ lib/WeBWorK/URLPath.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/URLPath.pm,v 1.4 2004/03/09 15:42:37 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/URLPath.pm,v 1.5 2004/03/15 20:17:14 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -149,7 +149,7 @@ our %pathTypes = (
 	hardcopy => {
 		name    => 'Hardcopy Generator',
 		parent  => 'set_list',
-		kids    => [ qw// ],
+		kids    => [ qw/hardcopy_preselect_set/ ],
 		match   => qr|^hardcopy/|,
 		capture => [ qw// ],
 		produce => 'hardcopy/',
