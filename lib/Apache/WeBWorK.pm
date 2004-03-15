1a11b13609d9cb0c906fc38fe27375288ffea701
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-03-15 02:25:11 +0000

    removed old dispatch() function, renamed dispatch_new() -> dispatch(), added upload handling code (from old dispatch function).
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1879 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/Apache/WeBWorK.pm
+++ lib/Apache/WeBWorK.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/Apache/WeBWorK.pm,v 1.63 2004/01/21 19:47:38 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/Apache/WeBWorK.pm,v 1.64 2004/03/04 21:02:27 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -73,7 +73,7 @@ sub handler($) {
 			die $error;
 		};
 		
-		$result = eval { WeBWorK::dispatch_new($r) };
+		$result = eval { WeBWorK::dispatch($r) };
 	}
 	
 	if ($@) {
