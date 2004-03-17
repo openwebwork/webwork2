d75572895987bff3ca77dfe2a21492487a8fbd0b
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-03-17 08:17:19 +0000

    URLPath conversion
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1902 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Logout.pm
+++ lib/WeBWorK/ContentGenerator/Logout.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Logout.pm,v 1.7 2003/12/09 01:12:31 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Logout.pm,v 1.8 2003/12/23 06:03:33 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -28,9 +28,10 @@ use warnings;
 use CGI qw();
 
 sub pre_header_initialize {
-	my $self = shift;
-	my $r = $self->{r};
-	my $ce = $self->{ce};
+	my ($self) = @_;
+	my $r = $self->r;
+	my $ce = $r->ce;
+	
 	my $cookie = Apache::Cookie->new($r,
 		-name => "WeBWorKAuthentication",
 		-value => "",
@@ -42,44 +43,47 @@ sub pre_header_initialize {
 	$r->headers_out->set("Set-Cookie" => $cookie->as_string);
 }
 
-sub title {
-	return "Logout";
+# This content generator is NOT logged in.
+sub if_loggedin {
+	my ($self, $arg) = @_;
+	
+	return !$arg;
 }
 
+# suppress links
 sub links {
 	return "";
 }
 
 sub body {
-	my $self = shift;
-	my $r = $self->{r};
-	my $ce = $self->{ce};
-	my $db = $self->{db};
+	my ($self) = @_;
+	my $r = $self->r;
+	my $ce = $r->ce;
+	my $db = $r->db;
+	my $urlpath = $r->urlpath;
 	
-	my $root = $ce->{webworkURLs}->{root};
-	my $courseName = $ce->{courseName};
-	my $userName = $r->param("user");
+	my $courseID = $urlpath->arg("courseID");
+	my $userID = $r->param("user");
 	
-	eval { $db->deleteKey($userName) };
+	eval { $db->deleteKey($userID) };
 	if ($@) {
-		print CGI::p("Something went wrong while logging out of WeBWorK: $@");
+		print CGI::div({class=>"ResultsWithError"},
+			CGI::p("Something went wrong while logging out of WeBWorK: $@")
+		);
 	}
 	
+	my $problemSets = $urlpath->newFromModule("WeBWorK::ContentGenerator::ProblemSets", courseID => $courseID);
+	my $loginURL = $r->location . $problemSets->path;
+	
 	print CGI::p("You have been logged out of WeBWorK.");
-	print CGI::start_form(-method=>"POST", -action=>"$root/$courseName/");
-	print CGI::hidden("user", $userName);
+	
+	print CGI::start_form(-method=>"POST", -action=>$loginURL);
+	print CGI::hidden("user", $userID);
 	print CGI::hidden("force_passwd_authen", 1);
-	print CGI::p({-align=>"center"}, CGI::submit("submit", "Log In Again"));
+	print CGI::p({align=>"center"}, CGI::submit("submit", "Log In Again"));
 	print CGI::end_form();
 	
 	return "";
-}
-
-# This content generator is NOT logged in.
-sub if_loggedin($$) {
-	my ($self, $arg) = (@_);
-	
-	return !$arg;
 }
 
 1;
