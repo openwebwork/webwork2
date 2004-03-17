2c48e8d55b8379b3c34f6c33047e90ff97a765ee
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-03-17 08:15:51 +0000

    fixed a path
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1899 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/URLPath.pm
+++ lib/WeBWorK/URLPath.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/URLPath.pm,v 1.5 2004/03/15 20:17:14 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/URLPath.pm,v 1.7 2004/03/15 22:29:54 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -306,7 +306,7 @@ our %pathTypes = (
 		match   => qr|^pgProblemEditor/|,
 		capture => [ qw// ],
 		produce => 'pgProblemEditor/',
-		display => '',
+		display => 'WeBWorK::ContentGenerator::Instructor::PGProblemEditor',
 	},
 	instructor_problem_editor_withset => {
 		name    => 'Problem Editor',
