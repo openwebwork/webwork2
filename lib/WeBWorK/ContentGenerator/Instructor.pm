dec19ba5aea3c44f572e6c5d881d5227b06c4464
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-03-23 01:10:14 +0000

    removed appendix
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1913 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor.pm
+++ lib/WeBWorK/ContentGenerator/Instructor.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Instructor.pm,v 1.35 2004/02/12 04:26:33 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Instructor.pm,v 1.36 2004/03/04 21:04:02 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -27,21 +27,11 @@ tools, providing useful utility functions.
 use strict;
 use warnings;
 use CGI qw();
-use WeBWorK::DB::Utils qw(global2user initializeUserProblem);
+use WeBWorK::DB::Utils qw(initializeUserProblem);
 
 =head1 METHODS
 
 =cut
-
-################################################################################
-# template escapes
-################################################################################
-
-# FIXME: remove this method, it does nothing.
-#sub links {
-# 	my $self = shift;
-#	return $self->SUPER::links();
-#}
 
 ################################################################################
 # Primary assignment methods
