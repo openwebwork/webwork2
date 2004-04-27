ba537be4e1d38ffd260da60303404d9f1bb0a088
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-04-27 03:37:56 +0000

    return auto-created permission level records
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@1976 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/DB.pm
+++ lib/WeBWorK/DB.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/DB.pm,v 1.44 2003/12/18 23:15:33 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/DB.pm,v 1.45 2004/01/03 20:07:03 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -518,6 +518,7 @@ sub getPermissionLevels {
 				if ($@ and $@ !~ m/permission level exists/) {
 					die "error while auto-creating permission level record for user $userID: \"$@\"";
 				}
+				$PermissionLevels[$i] = $PermissionLevel;
 			}
 		}
 	}
