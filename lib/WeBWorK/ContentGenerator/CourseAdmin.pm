e24d948f8b46cc938048ecd22ed81fd73e7c9682
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-05-21 20:55:38 +0000

    added missing "use" line. "use WeBWorK::CourseEnvironment".
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@2138 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/CourseAdmin.pm
+++ lib/WeBWorK/ContentGenerator/CourseAdmin.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/CourseAdmin.pm,v 1.9 2004/05/08 02:01:13 gage Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/CourseAdmin.pm,v 1.10 2004/05/13 20:49:18 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -28,6 +28,7 @@ use warnings;
 use CGI::Pretty qw();
 use Data::Dumper;
 use File::Temp qw/tempfile/;
+use WeBWorK::CourseEnvironment;
 use WeBWorK::Utils qw(cryptPassword);
 use WeBWorK::Utils::CourseManagement qw(addCourse deleteCourse listCourses);
 use WeBWorK::Utils::DBImportExport qw(dbExport dbImport);
