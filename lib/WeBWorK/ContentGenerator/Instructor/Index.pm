7c4ea8e2670d2374dfa6fd9f200992279a189cb2
mschmitt <mschmitt@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-05-24 01:34:51 +0000

    Additionaly tweaks to fail gracefully
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@2195 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/Index.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/Index.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Instructor/Index.pm,v 1.35 2004/05/24 00:39:18 mschmitt Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Instructor/Index.pm,v 1.36 2004/05/24 01:27:48 mschmitt Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -219,10 +219,15 @@ sub body {
 ## Mark's Edits for filtering
 	my @myUsers;
 	my $user = $r->param("user");
-	my @viewable_sections = @{$ce->{viewable_sections}->{$user}};
-	my @viewable_recitations = @{$ce->{viewable_recitations}->{$user}};
+	
+	my (@viewable_sections,@viewable_recitations);
+	
+	if (defined @{$ce->{viewable_sections}->{$user}})
+		{@viewable_sections = @{$ce->{viewable_sections}->{$user}};}
+	if (defined @{$ce->{viewable_recitations}->{$user}})
+		{@viewable_recitations = @{$ce->{viewable_recitations}->{$user}};}
 
-	if (@viewable_sections or @viewables_recitations){
+	if (@viewable_sections or @viewable_recitations){
 		foreach my $student (@Users){
 			my $keep = 0;
 			foreach my $sec (@viewable_sections){
