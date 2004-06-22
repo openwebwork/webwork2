6ab61d47b4ff0c0e98d2424571e1dfde91d275ea
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-06-22 17:13:46 +0000

    added stub for new constants file
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@2362 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/WeBWorK/Constants.pm
@@ -0,0 +1,27 @@
+################################################################################
+# WeBWorK Online Homework Delivery System
+# Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
+# $CVSHeader: webwork-modperl/lib/WeBWorK/Authz.pm,v 1.14 2004/03/15 20:17:35 sh002i Exp $
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
+
+package WeBWorK::Constants;
+
+=head1 NAME
+
+WeBWorK::Constants - provide constant values for other WeBWorK modules.
+
+=cut
+
+use strict;
+use warnings;
+
