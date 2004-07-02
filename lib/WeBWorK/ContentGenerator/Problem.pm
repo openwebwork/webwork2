ae7b850da43627dd62fc41dd6c7026c8d7218dcb
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-07-02 20:39:12 +0000

    Fixed uninitialized value.  (It comes into play when there are no answers required, as is true for some problems in tour orientation set.)
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@2436 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Problem.pm
+++ lib/WeBWorK/ContentGenerator/Problem.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Problem.pm,v 1.148 2004/06/28 14:26:10 jj Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Problem.pm,v 1.149 2004/06/28 14:31:40 jj Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -917,7 +917,7 @@ sub attemptResults {
 	$header .= $showAttemptResults ? CGI::th("Result")   : "";
 	$header .= $showMessages       ? CGI::th("Messages") : "";
 	my @tableRows = ( $header );
-	my $numCorrect;
+	my $numCorrect = 0;
 	foreach my $name (@answerNames) {
 		my $answerResult  = $pg->{answers}->{$name};
 		my $studentAnswer = $answerResult->{student_ans}; # original_student_ans
