af5a428409f04a69efa928dd435d22f43582ad1c
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-08-14 19:37:49 +0000

    Added functions to macro file needed for new samples.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@2614 c0722133-6baf-4dd8-8699-98d999cd4f06

--- doc/parser/macros/parserUtils.pl
+++ doc/parser/macros/parserUtils.pl
@@ -6,6 +6,41 @@ loadMacros(
 $bHTML = '\begin{rawhtml}';
 $eHTML = '\end{rawhtml}';
 
+#  HTML(htmlcode)
+#  HTML(htmlcode,texcode)
+#
+#  Insert $html in HTML mode or \begin{rawhtml}$html\end{rawhtml} in
+#  Latex2HTML mode.  In TeX mode, insert nothing for the first form, and
+#  $tex for the second form.
+#
+sub HTML {
+  my ($html,$tex) = @_;
+  return('') unless (defined($html) && $html ne '');
+  $tex = '' unless (defined($tex));
+  MODES(TeX => $tex, Latex2HTML => $bHTML.$html.$eHTML, HTML => $html);
+}
+
+#
+#  Begin and end <TT> mode
+#
+$BTT = HTML('<TT>','\texttt{');
+$ETT = HTML('</TT>','}');
+
+#
+#  Begin and end <SMALL> mode
+#
+$BSMALL = HTML('<SMALL>','{\small ');
+$ESMALL = HTML('</SMALL>','}');
+
+#
+#  Smart-quotes in TeX mode, regular quotes in HTML mode
+#
+$LQ = MODES(TeX => '``', Latex2HTML => '"', HTML => '"');
+$RQ = MODES(TeX => "''", Latex2HTML => '"', HTML => '"');
+
+#
+#  make sure all characters are displayed
+#
 sub protectHTML {
     my $string = shift;
     $string =~ s/&/\&amp;/g;
