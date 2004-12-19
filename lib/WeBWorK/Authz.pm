e0a88bb13c904a90ccec1ebd9eb3fdcde0a05d37
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-12-19 22:08:36 +0000

    The permission levels are now checked when the authz instance is created.  This reduces the number of calls to the database. It could cause trouble if there is a change in permission status between the time when the instance is created and when it is used. Since this all occurs while rendering one webpage, I don't think that this time difference will ever cause trouble.
    -- Mike
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@3053 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Authz.pm
+++ lib/WeBWorK/Authz.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/Authz.pm,v 1.16 2004/07/12 23:37:54 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/Authz.pm,v 1.17 2004/09/02 22:52:02 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -25,30 +25,37 @@ WeBWorK::Authz - check user permissions.
 use strict;
 use warnings;
 
+
 sub new {
-	my ($invocant, $r) = @_;
+	my ($invocant, $r, $ce, $db) = @_;
 	my $class = ref($invocant) || $invocant;
 	my $self = {
 		r => $r,
 	};
-	bless $self, $class;
-	return $self;
-}
-
-# This currently only uses two of it's arguments, but it accepts any number, in
-# case in the future calculating certain permissions requires more information.
-sub hasPermissions {
-	my ($self, $user, $activity) = @_;
-	my $r = $self->{r};
-	my $ce = $r->ce;
-	my $db = $r->db;
 	
+	my $user = $r->param("user");   
+	return 0 unless defined($user);
 	my $Permission = $db->getPermissionLevel($user); # checked
 	return 0 unless defined $Permission;
 	my $permissionLevel = $Permission->permission();
 	return 0 unless defined $permissionLevel and $permissionLevel ne "";
 	
 	my $permissionLevels = $ce->{permissionLevels};
+	$self->{permissionLevels} = $permissionLevels;
+	$self->{permissionLevel}  = $permissionLevel;
+	bless $self, $class;
+	return $self;
+}
+
+# This module assumes that neither the permissionLevels nor the permissionLevel
+# changes between the time an authz module is created and the time it's used.
+
+# This currently only uses two of it's arguments, but it accepts any number, in
+# case in the future calculating certain permissions requires more information.
+sub hasPermissions {
+	my ($self, $user, $activity) = @_;
+	my $permissionLevels = $self->{permissionLevels};
+	my $permissionLevel = $self->{permissionLevel};
 	if (exists $permissionLevels->{$activity}) {
 		if (defined $permissionLevels->{$activity}) {
 			return $permissionLevel >= $permissionLevels->{$activity};
@@ -58,6 +65,7 @@ sub hasPermissions {
 	} else {
 		die "Activity '$activity' not found in %permissionLevels. Can't continue.\n";
 	}
+
 }
 
 1;
