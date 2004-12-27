ad02887f1c816fa4d2a10dd6773bee4ffeb233f9
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2004-12-27 17:20:16 +0000

    Catch multiply defined user warning.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@3066 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Constants.pm
+++ lib/WeBWorK/Constants.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/Constants.pm,v 1.21 2004/10/11 19:53:06 gage Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/Constants.pm,v 1.22 2004/10/15 04:30:20 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -54,7 +54,7 @@ $WeBWorK::Timing::Enabled = 0;
 
 # If non-empty, timing data will be sent to the file named rather than STDERR.
 # 
-$WeBWorK::Timing::Logfile = "";
+$WeBWorK::Timing::Logfile = "/home/gage/webwork2/logs/timing.log";
 
 ################################################################################
 # WeBWorK::ContentGenerator::Hardcopy
