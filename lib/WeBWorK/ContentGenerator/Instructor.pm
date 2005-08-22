59e8e3a7926fe54e40b5e7e59344c7e7a98bcf0c
jj <jj@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-08-22 19:22:31 +0000

    Fixed function unassignSetsFromUsers.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@3550 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor.pm
+++ lib/WeBWorK/ContentGenerator/Instructor.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Instructor.pm,v 1.48 2005/07/14 13:15:25 glarose Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Instructor.pm,v 1.49 2005/08/12 02:47:29 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -441,8 +441,8 @@ Unassign each of the given sets from each of the given users.
 
 sub unassignSetsFromUsers {
 	my ($self, $setIDsRef, $userIDsRef) = @_;
-	my @setIDs = $setIDsRef;
-	my @userIDs = $userIDsRef;
+	my @setIDs = @$setIDsRef;
+	my @userIDs = @$userIDsRef;
 	
 	foreach my $setID (@setIDs) {
 		foreach my $userID (@userIDs) {
