4aca1bf407115c6bc950e479e710b3e1201f0082
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-09-05 23:43:38 +0000

    fix for security aspect of bug #715 -- reject calls with @_ != 2.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@3597 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Authz.pm
+++ lib/WeBWorK/Authz.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/Authz.pm,v 1.19 2004/12/20 21:08:02 sh002i Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/Authz.pm,v 1.20 2005/07/13 18:03:18 glarose Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -143,6 +143,8 @@ assumes that the user does not have permission.
 # This currently only uses two of it's arguments, but it accepts any number, in
 # case in the future calculating certain permissions requires more information.
 sub hasPermissions {
+	die "hasPermissions called with != 2 arguments" unless @_ == 2;
+	
 	my ($self, $userID, $activity) = @_;
 	my $r = $self->{r};
 	my $ce = $r->ce;
