ea19d36a6fc14094d05a4bd83a3a33988a99287b
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-09-14 23:44:52 +0000

    Hide the "important message" text unless there is actually a message in site_info.txt
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@3619 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Login.pm
+++ lib/WeBWorK/ContentGenerator/Login.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Login.pm,v 1.27 2005/06/27 00:06:30 gage Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Login.pm,v 1.28 2005/07/14 13:15:25 glarose Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -58,7 +58,7 @@ sub info {
 				print CGI::div({class=>"ResultsWithError"},
 					CGI::p("$@"),
 				);
-			} else {
+			} elsif($text) {
 				print CGI::p(CGI::b("Important Message")), $text,CGI::hr();
 			}
 		}
