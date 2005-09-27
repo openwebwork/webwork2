6ebb613ecf9fbe5b011ac511a828992dc67bb5ed
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-09-27 23:32:41 +0000

    set a default value for hardcopy_format if it's not given.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@3659 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Hardcopy.pm
+++ lib/WeBWorK/ContentGenerator/Hardcopy.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2003 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Hardcopy.pm,v 1.61 2005/09/13 18:00:13 apizer Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Hardcopy.pm,v 1.62 2005/09/23 23:26:35 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -102,6 +102,9 @@ sub pre_header_initialize {
 	
 	if ($generate_hardcopy) {
 		my $validation_failed = 0;
+		
+		# set default format
+		$hardcopy_format = $HC_DEFAULT_FORMAT unless defined $hardcopy_format;
 		
 		# make sure format is valid
 		unless (grep { $_ eq $hardcopy_format } keys %HC_FORMATS) {
