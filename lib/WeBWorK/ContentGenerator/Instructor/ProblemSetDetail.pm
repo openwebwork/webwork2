38a444614c6c9471c16ff96a535d2cf9b2b8895f
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2005-10-17 14:01:00 +0000

    Changed wording on link.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@3720 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/ProblemSetDetail.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/ProblemSetDetail.pm
@@ -1338,7 +1338,7 @@ HERE
 	my $editNewProblemPage = $urlpath->new(type => 'instructor_problem_editor_withset_withproblem', args => { courseID => $courseID, setID => $setID, problemID =>'new_problem'    });
     my $editNewProblemLink = $self->systemLink($editNewProblemPage, params => { make_local_copy => 1, file_type => 'blank_problem'  });
 
-	print CGI::p( CGI::a({href=>$editNewProblemLink},'Create'). 'a new blank problem');
+	print CGI::p( CGI::a({href=>$editNewProblemLink},'Edit'). 'a new blank problem');
 
 	print CGI::end_form();
 	
