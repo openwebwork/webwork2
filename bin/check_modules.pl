a3086cc1908d05c6238329a88995de28311172af
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-01-27 17:53:48 +0000

    forward-port from rel-2-2-dev: (Updated modules/executables list, cleaned up output.
    Output now specifies that $PATH is being searched for executables, and
    lists the contents of $PATH. Same with @INC. It's a little easier to
    tell if a module/executable has not been found: The "found" lines are
    prefixed by some space, which makes the not found '**' prefix stand out.
    When a module fails to load, but it's not because it wasn't found, the
    error message is given.)
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@3985 c0722133-6baf-4dd8-8699-98d999cd4f06

--- bin/check_modules.pl
+++ bin/check_modules.pl
@@ -1,12 +1,17 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+
+use strict;
+use warnings;
 
 my @modulesList = qw(
+	Apache
 	Apache::Constants
 	Apache::Cookie
+	Apache::Log
 	Apache::Request
+	Benchmark
 	Carp
 	CGI
-	CGI::Pretty
 	Data::Dumper
 	Data::UUID
 	Date::Format
@@ -15,54 +20,90 @@ my @modulesList = qw(
 	DBI
 	Digest::MD5
 	Errno
+	Fcntl
 	File::Copy
+	File::Find
 	File::Path
+	File::Spec
+	File::stat
 	File::Temp
+	Getopt::Long
+	Getopt::Std
 	HTML::Entities
+	IO::File
 	Mail::Sender
-	Net::SMTP
+	MIME::Base64
 	Opcode
+	Pod::Usage
 	Safe
-	SOAP::Lite
+	Socket
+	String::ShellQuote
 	Text::Wrap
 	Time::HiRes
+	Time::Zone
 	URI::Escape
 	XML::Parser
 	XML::Parser::EasyTree
 	XML::Writer
+	WheeWhaa
 );
+
+# modules used by disabled code
+#	Class::Data::Inheritable (DBv3)
+#	Class::DBI::Plugin::AbstractCount (DBv3)
+#	DateTime::Format::DBI (DBv3)
+#	GDBM_File (Driver::GDBM)
+#	RQP::Render (RQP)
+#	SOAP::Lite (PG::Remote)
 
 my @applicationsList = qw(
-	dvipng 
-	giftopnm
+	mkdir
+	mv
+	mysql
+	tar
 	latex
 	pdflatex
-	pngtopnm
-	pnmtopng
-	pnmtops
-	ppmtopgm
+	dvipng
 	tth
+	giftopnm
+	ppmtopgm
+	pnmtops
+	pnmtopng
+	pngtopnm
 );
 
-print "\nSearching for executables\n\n";
+print "\nChecking your \$PATH for executables required by WeBWorK...\n";
+my @path = split(/:/, $ENV{PATH});
+print "\$PATH=", shift @path, "\n";
+print join ("\n", map("      $_", @path)), "\n\n";
 
 foreach my $app (@applicationsList)  {
 	my $result = `which $app`;
 	chomp($result);
-	if ($result)  {
-		print "$app found at $result\n";
+	if ($result) {
+		print "   $app found at $result\n";
 	} else {
-		print "***** Can't find $app\n";
+		print "** $app not found in \$PATH\n";
 	}
 }
 
-print "\nSearching for modules needed for WeBWorK\n\n";
+print "\nLoading Perl modules required by WeBWorK...\n";
+my @inc = @INC;
+print "\@INC=", shift @inc, "\n";
+print join ("\n", map("     $_", @inc)), "\n\n";
 
 foreach my $module (@modulesList)  {
 	eval "use $module";
 	if ($@) {
-		print "***** Can't find $module\n";
+		my $file = $module;
+		$file =~ s|::|/|g;
+		$file .= ".pm";
+		if ($@ =~ /Can't locate $file in \@INC/) {
+			print "** $module not found in \@INC\n";
+		} else {
+			print "** $module found, but failed to load: $@";
+		}
 	} else {
-		print "$module found. \n";
+		print "   $module found and loaded\n";
 	}
 }
