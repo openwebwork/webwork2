23e4d9adfd4b3631fe7b15eb045d2562e3cca7c5
apizer <apizer@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-05-08 19:38:40 +0000

    removed warn message used to debug changes.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4065 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/ProblemSetDetail.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/ProblemSetDetail.pm
@@ -1084,7 +1084,6 @@ sub body {
 		foreach my $userID (@editForUser) {
 		    my $u = $db->getUser($userID);
 		    my $email_address = $u->email_address;
-		    warn "email address is $email_address";
 			my $line = $u->last_name.", ".$u->first_name."&nbsp;&nbsp;(".CGI::a({-href=>"mailto:$email_address"},"email "). $u->user_id."). Assigned to ";
 			my $editSetsAssignedToUserURL = $self->systemLink(
 	           $urlpath->newFromModule(
