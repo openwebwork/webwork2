aec573db3dfb68e20ff38d4df32d3070969ef017
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-06-23 20:22:39 +0000

    New files to bring jsMath up to version 3.3b.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4140 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ htdocs/jsMath/plugins/CHMmode.js
@@ -0,0 +1,36 @@
+/*
+ *  CHMmode.js
+ *  
+ *  Part of the jsMath package for mathematics on the web.
+ *
+ *  This file makes jsMath work with MicroSoft's HTML Help system
+ *  from within .chm files (compiled help archives).
+ *  
+ *  This file should be loaded BEFORE jsMath.js.
+ *
+ *  ---------------------------------------------------------------------
+ *
+ *  Copyright 2006 by Davide P. Cervone
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+if (!window.jsMath) {window.jsMath = {}}
+if (!jsMath.Controls) {jsMath.Controls = {}}
+if (!jsMath.Controls.cookie) {jsMath.Controls.cookie = {}}
+
+jsMath.noChangeGlobal = 1;
+jsMath.noShowGlobal = 1;
+jsMath.noImgFonts = 1;
+jsMath.Controls.cookie.global = 'always';
+jsMath.Controls.cookie.hiddenGlobal = 1;
