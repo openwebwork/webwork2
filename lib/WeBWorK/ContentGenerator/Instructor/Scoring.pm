0c5801c7dbf8fabc358054858929d223c8f4dfd8
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-07-11 16:26:20 +0000

    Added end form statement
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4229 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/Scoring.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/Scoring.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Instructor/Scoring.pm,v 1.55 2006/01/25 23:13:53 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Instructor/Scoring.pm,v 1.56 2006/07/08 14:07:35 gage Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -203,6 +203,7 @@ sub body {
 				)),
 			
 		   CGI::end_table(),
+		   CGI::end_form(),
 	);
 
 	
