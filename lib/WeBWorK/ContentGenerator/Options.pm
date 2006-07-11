cf11b76e22d285dc5f32eb0a3502e634ac69ef07
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-07-11 16:39:29 +0000

    Changed submit button so that the name and label keys are explicitly entered.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4230 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Options.pm
+++ lib/WeBWorK/ContentGenerator/Options.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Options.pm,v 1.20 2006/01/25 23:13:52 sh002i Exp $
+# $CVSHeader: webwork-modperl/lib/WeBWorK/ContentGenerator/Options.pm,v 1.21 2006/07/08 14:07:34 gage Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -25,7 +25,8 @@ WeBWorK::ContentGenerator::Options - Change user options.
 
 use strict;
 use warnings;
-use CGI qw(-nosticky );
+#use CGI qw(-nosticky );
+use WeBWorK::CGI;
 use WeBWorK::Utils qw(cryptPassword dequote);
 
 sub body {
@@ -168,7 +169,7 @@ sub body {
 			),
 			CGI::Tr(
 				CGI::td("$e_user_name\'s New Address"),
-				CGI::td(CGI::textfield("newAddress", $newA)),
+				CGI::td(CGI::textfield(-name=>"newAddress", -text=>$newA)),
 			),
 		);
 	} else {
