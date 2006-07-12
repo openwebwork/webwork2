a50e89214c907c5b5f7ff8add78eb344e2c54a76
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-07-12 04:38:24 +0000

    added missing "1;" at endo f file
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4242 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/File/Classlist.pm
+++ lib/WeBWorK/File/Classlist.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/File/Classlist.pm,v 1.3 2004/10/10 21:44:23 sh002i Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/File/Classlist.pm,v 1.4 2006/01/25 23:13:55 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -103,3 +103,5 @@ sub write_classlist($@) {
 	
 	$fh->close;
 }
+
+1;
