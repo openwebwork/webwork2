024ae2aa8a2beb5058187440e95c06bcfa7076b4
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-07-18 14:59:05 +0000

    Added CGIParamShim as a choice
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4283 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/CGI.pm
+++ lib/WeBWorK/CGI.pm
@@ -1,8 +1,27 @@
 # choose one
 
+# standard CGI -- will not work properly with apache2
 
 use CGI qw(*ul *li escapeHTML);
 
+# there are 2 CGI substitutes for apache2:
+
+# below is a front end for CGIeasytags that imitates some of the 
+# shortcuts of CGI. 
+# It is probably not ready for prime time.
+# It only concatenates arguments if the first 
+# argument is a hash (possibly empty) of params.
+# This enforces uniformity, but it is also annoying
+# The error reporting however is fairly good.
+# It is shorter than CGI, but it may not be easy to maintain
+# if it seems desirable to use it then the code should be cleaned up
+# There are many places where code can be factored out to improve speed
+# increase reliability and readability
+
 #use WeBWorK::CGIeasytags;
 
+# below is a subclass of CGI that forces CGI to use the WeBWorK Request 
+# object when finding "sticky" parameters
+
+#use WeBWorK::CGIParamShim;
 1;
