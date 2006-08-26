186b417376f3950d96ce93ff4bcd032114970656
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-08-26 16:55:12 +0000

    only recognize comments that start at the beginning of a line. this reduces the changes that a "#" in a field, will cause a real line to be truncated. (it's unlikely that the student ID will start with a "#".)
    yeah, i know the real solution is to use escaping.
    
    should-be-backported-to-rel-2-3-dev
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4417 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/File/Classlist.pm
+++ lib/WeBWorK/File/Classlist.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/File/Classlist.pm,v 1.4 2006/01/25 23:13:55 sh002i Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/File/Classlist.pm,v 1.5 2006/07/12 04:38:24 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -45,7 +45,7 @@ sub parse_classlist($) {
 	
 	while (<$fh>) {
 		chomp;
-		s/#.*$//;
+		next if /^#/;
 		next unless /\S/;
 		s/^\s*//;
 		s/\s*$//;
