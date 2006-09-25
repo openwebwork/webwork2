2e4156055c6ada7bcda6cb2a1a83d7a3d2f5f693
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-09-25 22:35:05 +0000

    use a single username/password for connecting to the SQL database. this means that only one DBI handle is created instead of two, and the Driver's connect/disconnect methods are no-ops.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4521 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Utils/CourseManagement/sql_single.pm
+++ lib/WeBWorK/Utils/CourseManagement/sql_single.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/Utils/CourseManagement/sql_single.pm,v 1.11 2006/06/15 14:47:56 gage Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/Utils/CourseManagement/sql_single.pm,v 1.12 2006/09/15 22:09:36 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -129,8 +129,8 @@ sub addCourseHelper {
 		
 		# add username and password to hashes
 		
-		$usernames{$source} = $params{usernameRW};
-		$passwords{$source} = $params{passwordRW};
+		$usernames{$source} = $params{username};
+		$passwords{$source} = $params{password};
 		
 		#warn "\n";
 	}
@@ -519,8 +519,8 @@ sub deleteCourseHelper {
 		
 		# add username and password to hashes
 		
-		$usernames{$source} = $params{usernameRW};
-		$passwords{$source} = $params{passwordRW};
+		$usernames{$source} = $params{username};
+		$passwords{$source} = $params{password};
 		
 		#warn "\n";
 	}
