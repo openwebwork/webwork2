8d1b00869323cdf0b14cc7b1a07fcbb029102904
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-10-11 16:16:51 +0000

    remove unused functions (left over from GDBM days)
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4567 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/DB/Utils.pm
+++ lib/WeBWorK/DB/Utils.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/DB/Utils.pm,v 1.16 2006/01/25 23:13:54 sh002i Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/DB/Utils.pm,v 1.17 2006/09/26 15:57:41 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -28,119 +28,10 @@ use warnings;
 
 our @EXPORT    = ();
 our @EXPORT_OK = qw(
-	record2hash
-	hash2record
-	hash2string
-	string2hash
 	global2user
 	user2global
 	initializeUserProblem
-	findDefaults
 );
-
-################################################################################
-# WWDBv2 record <-> WWDBv1 hash
-#  not in the Record classes, since they are for legacy support
-################################################################################
-
-# FIXME: this should go away -- let WW1Hash and Classlist1Hash handle their own
-# stupid conversions!
-
-# RECORDHASH defines the correspondance between WWDBv1 hash keys and WWDBv2
-# record fields.
-use constant RECORDHASH => {
-	"WeBWorK::DB::Record::User" => [
-		['stfn', "first_name"   ],
-		['stln', "last_name"    ],
-		['stea', "email_address"],
-		['stid', "student_id"   ],
-		['stst', "status"       ],
-		['clsn', "section"      ],
-		['clrc', "recitation"   ],
-		['comt', "comment"      ],
-	],
-#	"WeBWorK::DB::Record::UserSet" => [
-#		['stlg', "user_id"       ],
-#		['stnm', "set_id"        ],
-#		['shfn', "set_header"    ],
-#		['phfn', "hardcopy_header"],
-#		['opdt', "open_date"     ],
-#		['dudt', "due_date"      ],
-#		['andt', "answer_date"   ],
-#	],
-#	# a hash destined to be converted into a UserProblem must be converted
-#	# so that the hash keys, rather than containing the problem number,
-#	# contain the character '#'. Also, a new hash key '#' must be added
-#	# which contains the problem number.
-#	"WeBWorK::DB::Record::UserProblem" => [
-#		['stlg',  "user_id"      ],
-#		['stnm',  "set_id"       ],
-#		['#',     "problem_id"   ],
-#		['pfn#',  "source_file"  ],
-#		['pva#',  "value"        ],
-#		['pmia#', "max_attempts" ],
-#		['pse#',  "problem_seed" ],
-#		['pst#',  "status"       ],
-#		['pat#',  "attempted"    ],
-#		['pan#',  "last_answer"  ],
-#		['pca#',  "num_correct"  ],
-#		['pia#',  "num_incorrect"],
-#	],
-};
-
-sub record2hash($) {
-	my ($record) = @_;
-	my $map = RECORDHASH->{ref $record};
-	die ref $record, ": unknown record type" unless defined $map;
-	my %hash;
-	for (my $i = 0; $i < @$map; $i++) {
-		my ($v1, $v2) = @{$map->[$i]};
-		$hash{$v1} = $record->$v2;
-	}
-	return %hash;
-}
-
-sub hash2record($@) {
-	my ($type, %hash) = @_;
-	my $map = RECORDHASH->{$type};
-	die $type, ": unknown record type" unless defined $map;
-	my $record = $type->new();
-	for (my $i = 0; $i < @$map; $i++) {
-		my ($v1, $v2) = @{$map->[$i]};
-		my $value = defined $hash{$v1} ? $hash{$v1} : ""; # promote undef to ""
-		$record->$v2($value);
-	}
-	return $record;
-}
-
-################################################################################
-# WWDBv1 hash <-> WWDBv1 string
-################################################################################
-
-sub hash2string(@) {
-	my (%hash) = @_;
-	my $string;
-	return "" unless keys %hash;
-	foreach (keys %hash) {
-		$hash{$_} = "" unless defined $hash{$_}; # promote undef to ""
-		$hash{$_} =~ s/(=|&)/\\$1/g; # escape & and =
-		$string .= "$_=$hash{$_}&";
-	}
-	chop $string; # remove final '&' from string for old code :p
-	return $string;
-}
-
-sub string2hash($) {
-	my $string = shift;
-	return unless defined $string and $string;
-	my %hash = $string =~ /(.*?)(?<!\\)=(.*?)(?:(?<!\\)&|$)/gs; # /s allows . to match \n
-	$hash{$_} =~ s/\\(&|=)/$1/g foreach keys %hash; # unescape & and =
-	return %hash;
-}
-
-################################################################################
-# global <-> user record conversion
-################################################################################
 
 sub global2user($$) {
 	my ($userRecordClass, $GlobalRecord) = @_;
@@ -172,51 +63,6 @@ sub initializeUserProblem {
 	$userProblem->problem_seed(int(rand(5000)));
 
 	return $userProblem;
-}
-
-################################################################################
-# default generation
-################################################################################
-
-sub findDefaults($@) {
-	my ($globalClass, @Records) = @_;
-	
-	my %fields = map { $_ => {} } $globalClass->FIELDS();
-	#delete $fields{$_} foreach $globalClass->KEYFIELDS();
-	
-	foreach my $Record (@Records) {
-		foreach my $field (keys %fields) {
-			my $value = $Record->$field();
-			if ($value eq "UNDEFINED") {
-				die "Uh oh... value eq \"UNDEFINED\"\n";
-			}
-			unless (defined $value) {
-				$value = "UNDEFINED";
-			}
-			$fields{$field}{$value}++;
-		}
-	}
-	
-	my $Defaults = $globalClass->new();
-	foreach my $field (keys %fields) {
-		my $maxFreq = 0;
-		my $maxValue;
-		foreach my $value (keys %{$fields{$field}}) {
-			my $freq = $fields{$field}{$value};
-			if ($freq > $maxFreq) {
-				$maxFreq = $freq;
-				$maxValue = $value;
-			}
-		}
-		undef $maxValue if $maxValue eq "UNDEFINED";
-		$Defaults->$field($maxValue);
-	}
-	
-	#use Data::Dumper;
-	#warn "Frequencies: ", Dumper(\%fields);
-	#warn "Consensus defaults: ", Dumper($Defaults);
-	
-	return $Defaults;
 }
 
 1;
