465a9fd5bab69e6b6820381cd0707d43c67faaee
dpvc <dpvc@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-02-12 21:32:31 +0000

    Completely rewrote the file to use a different technique.  Instead of looking for unbeforeunload events (which didn't work reliably, since Firefox will reload the page before it has a chance to install the handler), we look for F5 key events and F5 ignore keydown events, but reload the page on F5 keyup instead.  This skips the autorepeating keys and only reloads the page when the user lets go of the key.  So there is only one reload.  After several autorepeated F5 keypresses, a dialog will appear asking you to check if something is holding down the F5 key.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4775 c0722133-6baf-4dd8-8699-98d999cd4f06

--- htdocs/js/limit-reloads.js
+++ htdocs/js/limit-reloads.js
@@ -1,14 +1,49 @@
+/*******************************************************
+ *
+ *  This file implements a JavaScript-based hack to prevent
+ *  an autorepeating F5 key from swamping your server with
+ *  requests.  it does this by ignoring F5 keydown events
+ *  and reloading the page on an F5 keyup event.  It will
+ *  also put up a dialog box when it has spotted repeated
+ *  F5 attempts to ask you to check if something is sitting
+ *  on the F5 button.
+ *
+ *  This only works in MSIE/PC and Firefox/PC.  It does
+ *  not work for Firefox/Mac or Opera (either platform).
+ *
+ *  Autorepeating the reload button should be considered
+ *  a browser bug, in my opinion, but I can't do much
+ *  about that.
+ *
+ ********************************************************/
+
 var wwLimitReloads = {
-  shortTime: 200,  // in milliseconds.  Reloads faster than this will be flagged
-  loadTime: new Date().getTime()
-};
+  maxF5count: 5, // warn after this many blocked F5 attempts
+  F5count: 0,
+  warned: 0
+}
 
-window.onbeforeunload = function () {
-  var now = new Date().getTime();
-  if (now - wwLimitReloads.loadTime < wwLimitReloads.shortTime) {
-    wwLimitReloads.loadTime = now;
-    return "You seem to be reloading the page very rapidly.\n"
-         + "Perhaps something is holding down your F5 key?";
-
+document.onkeydown = function (event) {
+  if (!event) {event = window.event}
+  if (event.keyCode == 116) {
+    if (wwLimitReloads.F5count++ == wwLimitReloads.maxF5count && !wwLimitReloads.warned) {
+      wwLimitReloads.warned = 1;
+      setTimeout(function () {
+        alert("You seem to be generating many F5 presses.\n"
+              + "See if something is holding the F5 key down.");
+      },10);
+    }
+    try {event.keyCode = 8} catch (err) {}; // make MSIE event cancelable
+    if (event.preventDefault) event.preventDefault();
+    if (event.stopPropagation) event.stopPropagation();
+    event.cancelBubble = true;
+    event.returnValue = false;
+    return false;
   }
+  return true;
+}
+
+document.onkeyup = function (event) {
+  if (!event) {event = window.event}
+  if (event.keyCode == 116) {window.location.reload()}
 }
