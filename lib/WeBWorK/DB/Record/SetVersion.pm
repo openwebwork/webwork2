c098bfb10fe56430e53e45a4e73866d33b13f854
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-02-20 00:01:47 +0000

    versioned subclasses of UserSet/UserProblem
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4785 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/WeBWorK/DB/Record/SetVersion.pm
@@ -0,0 +1,43 @@
+################################################################################
+# WeBWorK Online Homework Delivery System
+# Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
+# $CVSHeader: webwork2/lib/WeBWorK/DB/Record/UserSet.pm,v 1.15 2006/12/01 17:04:03 glarose Exp $
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
+
+package WeBWorK::DB::Record::SetVersion;
+use base WeBWorK::DB::Record::UserSet;
+
+=head1 NAME
+
+WeBWorK::DB::Record::SetVersion - represent a record from the virtual 
+set_version table.
+
+=cut
+
+use strict;
+use warnings;
+
+BEGIN {
+	our @ISA;
+	my $base = $ISA[0];
+	my $field_data = $base->FIELD_DATA;
+	my @keyfields = map { $_ => $field_data->{$_} } $base->KEYFIELDS;
+	my @nonkeyfields = map { $_ => $field_data->{$_} } $base->NONKEYFIELDS;
+	__PACKAGE__->_fields(
+		@keyfields,
+		version_id => { type=>"INT NOT NULL", key=>1 },
+		@nonkeyfields,
+	);
+}
+
+1;
