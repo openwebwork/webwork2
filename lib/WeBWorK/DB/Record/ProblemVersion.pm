a4ff88fdecde736d65254c4491e5f6a428f3190e
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-02-22 01:25:11 +0000

    list keyfields explicitly, so that we can have the ProblemVersion keyfields in the correct order (user_id,set_id,version_id,problem_id)
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4791 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/DB/Record/ProblemVersion.pm
+++ lib/WeBWorK/DB/Record/ProblemVersion.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/DB/Record/UserSet.pm,v 1.15 2006/12/01 17:04:03 glarose Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/DB/Record/ProblemVersion.pm,v 1.1 2007/02/20 00:01:47 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -31,11 +31,12 @@ BEGIN {
 	our @ISA;
 	my $base = $ISA[0];
 	my $field_data = $base->FIELD_DATA;
-	my @keyfields = map { $_ => $field_data->{$_} } $base->KEYFIELDS;
 	my @nonkeyfields = map { $_ => $field_data->{$_} } $base->NONKEYFIELDS;
 	__PACKAGE__->_fields(
-		@keyfields,
-		version_id => { type=>"INT NOT NULL", key=>1 },
+		user_id       => { type=>"TINYBLOB NOT NULL", key=>1 },
+		set_id        => { type=>"TINYBLOB NOT NULL", key=>1 },
+		version_id    => { type=>"INT NOT NULL", key=>1 },
+		problem_id    => { type=>"INT NOT NULL", key=>1 },
 		@nonkeyfields,
 	);
 }
