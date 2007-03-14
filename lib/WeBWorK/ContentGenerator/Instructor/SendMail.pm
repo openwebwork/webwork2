9ddbdf764bf8a885ea6adda62039efbdf6b5b1a5
sh002i <sh002i@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-03-14 23:38:11 +0000

    accidental commit of "die" that i added for testing (whoops).
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4881 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/SendMail.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/SendMail.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Instructor/SendMail.pm,v 1.60 2007/03/14 17:29:49 sh002i Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Instructor/SendMail.pm,v 1.61 2007/03/14 22:50:36 sh002i Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -775,7 +775,7 @@ sub mail_message_to_recipients {
 	my $merge_file            = $self->{merge_file};  
 	my $result_message        = '';
 	my $failed_messages        = 0;
-	die "this is a fatal error -- will i get to see it?";
+	
 	foreach my $recipient (@recipients) {
 			# warn "FIXME sending email to $recipient";
 			my $error_messages = '';
