3fbc41c89d0aab701938c6ee6b1066281de65744
glarose <glarose@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-03-22 13:50:07 +0000

    Added authentication mechanism Cosign.pm.  This implements WeBWorK authentication using cosign[1], an open-source single sign-on system developed at the University of Michigan.  This should be a reasonable model for other modules implementing external, directory or location based, authentication for WeBWorK.
    To use cosign authentication for WeBWorK, set $authen{user_module} in
    global.conf to WeBWorK::Authen::Cosign, and then configure apache to
    require cosign authentication to enter WeBWorK courses.  If $cosignoff
    is set to a true value in the course environment, standard WeBWorK
    authentication is used for that course.
    
    Note that cosign is a cookie-based authentication system.  This means
    that it is between very hard and impossible to authenticate multiple
    users on the same computer/web browser at the same time.  Thus it is
    not possible to have both $authen{user_module} and $authen{proctor_module}
    set to Cosign at the same time.
    
    Implementation:
      Cosign.pm inherits from Authen.pm.  It redefines get_credentials to
    force password authentication, and then assumes password checks succeed.
    Because cosign is a Location/Directory based apache authentication
    system, by the time the user gets to the Authen module we know that she
    has successfully authenticated against cosign.  We also modify the
    forget_verification method to return a redirect to the course environment
    variable $cosign_logout_script.
    
     [1] <http://www.umich.edu/~umweb/software/cosign/>
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4894 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ lib/WeBWorK/Authen/Cosign.pm
@@ -0,0 +1,137 @@
+################################################################################
+# WeBWorK Online Homework Delivery System
+# Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
+# $CVSHeader: webwork2/lib/WeBWorK/Authen/Proctor.pm,v 1.1 2006/04/12 18:50:11 sh002i Exp $
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of either: (a) the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version, or (b) the "Artistic License" which comes with this package.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+# Artistic License for more details.
+################################################################################
+
+package WeBWorK::Authen::Cosign;
+use base qw/WeBWorK::Authen/;
+
+=head1 NAME
+
+WeBWorK::Authen::Cosign - Authentication plug in for cosign
+
+to use: include in global.conf or course.conf
+  $authen{user_module} = "WeBWorK::Authen::Cosign";
+and add /webwork2 or /webwork2/courseName as a CosignProtected
+Location
+
+if $r->ce->{cosignoff} is set for a course, authentication reverts
+to standard WeBWorK authentication.
+
+=cut
+
+use strict;
+use warnings;
+use WeBWorK::Debug;
+
+# this is similar to the method in the base class, except that cosign 
+# ensures that we don't get to the address without a login.  this means
+# that we can't allow guest logins, but don't have to do any password
+# checking or cookie management.
+
+sub get_credentials {
+	my ($self) = @_;
+	my $r = $self->{r};
+	my $ce = $r->ce;
+	my $db = $r->db;
+	
+	if ( $ce->{cosignoff} ) {
+		return $self->SUPER::get_credentials( @_ );
+	} else {
+		if ( defined( $ENV{'REMOTE_USER'} ) ) {
+			$self->{'user_id'} = $ENV{'REMOTE_USER'};
+			$self->{r}->param("user", $ENV{'REMOTE_USER'});
+		} else {
+			return 0;
+		}
+		# set external auth parameter so that Login.pm knows
+		#    not to rely on internal logins if there's a check_user
+		#    failure.
+		$self->{external_auth} = 1;
+
+		# the session key isn't used (cosign is managing this 
+		#    for us), and we want to force checking against the 
+		#    site_checkPassword
+		$self->{'session_key'} = undef;
+		$self->{'password'} = 1;
+		$self->{'credential_source'} = "params";
+
+		return 1;
+	}
+}
+
+sub site_checkPassword { 
+	my ( $self, $userID, $clearTextPassword ) = @_;
+
+	if ( $self->{r}->ce->{cosignoff} ) {
+		return $self->SUPER::site_checkPassword( @_ );
+	} else {
+		# this is easy; if we're here at all, we've authenticated
+		# through cosign
+		return 1;
+	}
+}
+
+# disable cookie functionality
+sub maybe_send_cookie {
+	my ($self, @args) = @_;
+	if ( $self->{r}->ce->{cosignoff} ) {
+		return $self->SUPER::maybe_send_cookie( @_ );
+	} else {
+		# nothing to do here
+	}
+}
+sub fetchCookie {
+	my ($self, @args) = @_;
+	if ( $self->{r}->ce->{cosignoff} ) {
+		return $self->SUPER::fetchCookie( @_ );
+	} else {
+		# nothing to do here
+	}
+}
+sub sendCookie {
+	my ($self, @args) = @_;
+	if ( $self->{r}->ce->{cosignoff} ) {
+		return $self->SUPER::sendCookie( @_ );
+	} else {
+		# nothing to do here
+	}
+}
+sub killCookie {
+	my ($self, @args) = @_;
+	if ( $self->{r}->ce->{cosignoff} ) {
+		return $self->SUPER::killCookie( @_ );
+	} else {
+		# nothing to do here
+	}
+}
+
+# this is a bit of a cheat, because it does the redirect away from the
+#   logout script or what have you, but I don't see a way around that.
+sub forget_verification { 
+	my ($self, @args) = @_;
+	my $r = $self->{r};
+
+	if ( $r->ce->{cosignoff} ) {
+		return $self->SUPER::forget_verification( @_ );
+	} else {
+		$self->{was_verified} = 0;
+#		$r->headers_out->{"Location"} = $r->ce->{cosign_logout_script};
+#		$r->send_http_header;
+#		return;
+		$self->{redirect} = $r->ce->{cosign_logout_script};
+	}
+}
+
+1;
