9584078748a89c6cc012d6db0e2bfca67a881b85
glarose <glarose@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-03-30 19:07:55 +0000

    Some bugfixes, preliminary commit of relax_restrict_ip function.  this commit  - adds relax_restrict_ip and hide_score_by_problem columns to the set 	and set_user tables  - adds the corresponding fields to the Set and UserSet DB Records  - patches 'no location' handling in the location management routine 	in CourseAdmin.pm  - improves Authz->checkSet's handling of the case of location 	restriction when no addresses exist for a location, makes it 	use any cached set that may be available, and makes it honor 	relax_restrict_ip  - corrects a typo and some comments in DB.pm  - allows ip restriction fields in ProblemSetList when importing from 	or exporting to set definition files  - updates ProblemSetDetail.pm to handle the relax_restrict_ip field
    The relax_restrict_ip function has not been well tested.
    hide_score_by_problem isn't yet used.
    
    
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@4918 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/DB/Record/Set.pm
+++ lib/WeBWorK/DB/Record/Set.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2006 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/DB/Record/Set.pm,v 1.17 2007/03/13 15:42:06 glarose Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/DB/Record/Set.pm,v 1.18 2007/03/27 17:04:01 glarose Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -45,9 +45,11 @@ BEGIN {
 		version_last_attempt_time => { type=>"BIGINT" },
 		problems_per_page         => { type=>"INT" },
                 hide_score                => { type=>"ENUM('N','Y','BeforeAnswerDate')" },
+                hide_score_by_problem     => { type=>"ENUM('N','Y')" },
                 hide_work                 => { type=>"ENUM('N','Y','BeforeAnswerDate')" },
 		time_limit_cap            => { type=>"ENUM('0','1')" },
 		restrict_ip               => { type=>"ENUM('No','RestrictTo','DenyFrom')" },
+		relax_restrict_ip         => { type=>"ENUM('No','AfterAnswerDate','AfterVersionAnswerDate') DEFAULT 'No'" },
 	);
 }
 
