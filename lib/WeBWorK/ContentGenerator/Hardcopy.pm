2b69e741cf1aeea168f2a7843ddf1f372eeeceea
glarose <glarose@c0722133-6baf-4dd8-8699-98d999cd4f06>
2008-08-06 16:03:36 +0000

    Hardcopy.pm: bugfix to deal with empty values coming in for a set's hide_score parameter.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@5862 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Hardcopy.pm
+++ lib/WeBWorK/ContentGenerator/Hardcopy.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Hardcopy.pm,v 1.96 2007/08/13 22:59:55 sh002i Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Hardcopy.pm,v 1.97 2007/09/12 15:05:07 glarose Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -227,7 +227,8 @@ sub pre_header_initialize {
 						}
 
 						$canShowScore{"$uid!$sid"} = 
-						    ( ! defined( $userSet->hide_score ) ) ||
+						    ( ! defined( $userSet->hide_score ) ||
+						      $userSet->hide_score eq '' ) ||
 							( $userSet ->hide_score eq 'N' ||
 							  ( $userSet->hide_score eq 'BeforeAnswerDate' &&
 							    time >= $userSet->answer_date ) );
