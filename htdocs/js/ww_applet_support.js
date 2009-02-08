27684b26bec35a1e225253ce30af9717ea74103b
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2009-02-08 15:16:14 +0000

    Added copyright notice
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@6002 c0722133-6baf-4dd8-8699-98d999cd4f06

--- htdocs/js/ww_applet_support.js
+++ htdocs/js/ww_applet_support.js
@@ -1,3 +1,18 @@
+// ################################################################################
+// # WeBWorK Online Homework Delivery System
+// # Copyright © 2000-2009 The WeBWorK Project, http://openwebwork.sf.net/
+// # $CVSHeader: pg/macros/AppletObjects.pl,v 1.16 2009/02/07 23:08:34 gage Exp $
+// # 
+// # This program is free software; you can redistribute it and/or modify it under
+// # the terms of either: (a) the GNU General Public License as published by the
+// # Free Software Foundation; either version 2, or (at your option) any later
+// # version, or (b) the "Artistic License" which comes with this package.
+// # 
+// # This program is distributed in the hope that it will be useful, but WITHOUT
+// # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// # FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
+// # Artistic License for more details.
+// ################################################################################
 
 
 
