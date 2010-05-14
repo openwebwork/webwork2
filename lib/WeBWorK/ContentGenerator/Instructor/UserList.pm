9b6a4f03623bf70bfa179607771cd60d60d6502f
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2010-05-14 00:52:48 +0000

    Try to catch user_id's with bad characters -- not sure this is successful yet.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@6239 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/UserList.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/UserList.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Instructor/UserList.pm,v 1.94 2009/09/30 14:22:09 gage Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Instructor/UserList.pm,v 1.95 2010/01/23 21:48:26 gage Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -69,6 +69,7 @@ use warnings;
 #use CGI qw(-nosticky );
 use WeBWorK::CGI;
 use WeBWorK::File::Classlist;
+use WeBWorK::DB;
 use WeBWorK::Utils qw(readFile readDirectory cryptPassword);
 use constant HIDE_USERS_THRESHHOLD => 200;
 use constant EDIT_FORMS => [qw(cancelEdit saveEdit)];
@@ -1328,6 +1329,10 @@ sub importUsersFromCSV {
 		my %record = %$record;
 		my $user_id = $record{user_id};
 		
+		unless (WeBWorK::DB::check_user_id($user_id) ) {  # try to catch lines with bad characters
+			push @skipped, $user_id;
+			next;
+		}
 		if ($user_id eq $user) { # don't replace yourself!!
 			push @skipped, $user_id;
 			next;
