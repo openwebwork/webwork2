1734fa7c19a1c8d5ca370eed069b5b2525914246
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2010-05-14 00:58:02 +0000

    minor changes to configuration section
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@6242 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/Apache/WeBWorK.pm
+++ lib/Apache/WeBWorK.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/Apache/WeBWorK.pm,v 1.80 2006/07/15 14:04:48 sh002i Exp $
+# $CVSHeader$
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
