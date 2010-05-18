9e8698ddf7f52959762c4279d4a3ba54636c30bd
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2010-05-18 23:26:04 +0000

    Fixed a misplaced period in a flag name
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@6269 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/ContentGenerator/Instructor/PGProblemEditor.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/PGProblemEditor.pm
@@ -1,7 +1,7 @@
 ################################################################################
 # WeBWorK Online Homework Delivery System
 # Copyright © 2000-2007 The WeBWorK Project, http://openwebwork.sf.net/
-# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Instructor/PGProblemEditor.pm,v 1.98 2009/12/17 03:55:01 gage Exp $
+# $CVSHeader: webwork2/lib/WeBWorK/ContentGenerator/Instructor/PGProblemEditor.pm,v 1.99 2010/05/18 18:39:40 apizer Exp $
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
@@ -1662,7 +1662,7 @@ sub save_as_form {  # calls the save_as_handler
     my $rh_new_problem_options = {
     			 -type      => 'radio',
     			 -name      => "action.save_as.saveMode",
-    			 -value     => "new_independent_problem.",
+    			 -value     => "new_independent_problem",
     			 -onfocus   => $onChange,
     			 };
     $rh_new_problem_options->{checked}=1 unless $can_add_problem_to_set;
