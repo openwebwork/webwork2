9c4d49ea702a4eaacf307a27314699039f20d088
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2011-06-23 01:12:05 +0000

    adjusting CSS code
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@6881 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Constants.pm
+++ lib/WeBWorK/Constants.pm
@@ -136,7 +136,7 @@ $WeBWorK::ContentGenerator::Instructor::Config::ConfigValues = [
 		  doc => 'Theme (refresh page after saving changes to reveal new theme)',
 		  doc2 => 'There are currently five themes (or skins) to choose from: ur, math, math2, and dgage.  The theme
 specifies a unified look and feel for the WeBWorK course web pages.',
-		  values => [qw(math math2 ur dgage  union)],
+		  values => [qw(math math2 math3 ur dgage  union)],
 		  type => 'popuplist'},
 		{ var => 'sessionKeyTimeout',
 		  doc => 'Inactivity time before a user is required to login again',
