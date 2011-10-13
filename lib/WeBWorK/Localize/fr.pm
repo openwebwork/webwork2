348d9bf8b5d7e95f91dade48b2ee470ec18c43fa
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2011-10-13 16:51:47 +0000

    committing a more up-to-date version of the french lexicon authored by Stephanie Lanthier and Sebastien Labbe  of the Universite dur Quebec a Montreal (UQAM)
    git-svn-id: http://svn.webwork.maa.org/system/trunk/webwork2@7070 c0722133-6baf-4dd8-8699-98d999cd4f06

--- lib/WeBWorK/Localize/fr.pm
+++ lib/WeBWorK/Localize/fr.pm
@@ -1,5 +1,20 @@
 ## WeBWorK-tr  French language lexicon
 
+## AUTHORS:
+##
+##   - Michael Gage (initial version)
+##   - Stéphanie Lanthier, September 2011, Université du Québec à Montréal (UQAM)
+##   - Sébastien Labbé, September 2011, Université du Québec à Montréal (UQAM)
+##
+## WARNING:
+## 
+##     For now, this is incomplete. It is a work in progress...
+##
+## AVERTISSEMENT:
+## 
+##     Ce fichier est incomplet. Le travail est en cours...
+##
+
 package WeBWorK::Localize::fr;
 
 use base qw(WeBWorK::Localize);
@@ -8,209 +23,272 @@ use vars qw(%Lexicon);
 
 %Lexicon = (
 
-## File locations
-"navPrevGrey" =>			"images_fr/navPrevGrey",
-"navPrev" =>				"images_fr/navPrev",
-"navProbListGrey" =>	    "images_fr/navProbListGrey",
-"navProbList" =>			"images_fr/navProbList",
-"navNextGrey" =>			"images_fr/navNextGrey",
-"navNext" =>				"images_fr/navNext",
-"navUp" =>				    "images_fr/navUp",
+# ## File locations
+# "navPrevGrey" =>			"images_fr/navPrevGrey",
+# "navPrev" =>				"images_fr/navPrev",
+# "navProbListGrey" =>	    "images_fr/navProbListGrey",
+# "navProbList" =>			"images_fr/navProbList",
+# "navNextGrey" =>			"images_fr/navNextGrey",
+# "navNext" =>				"images_fr/navNext",
+# "navUp" =>				    "images_fr/navUp",
 
-## UNTRANSLATED
+## File locations
+"navPrevGrey" =>			"Précédent",
+"navPrev" =>				"Précédent",
+"navProbListGrey" =>	    "Liste de problèmes",
+"navProbList" =>			"Liste de problèmes",
+"navNextGrey" =>			"Suivant",
+"navNext" =>				"Suivant",
+"navUp" =>				    "Haut",
+
 
 "The selected problem([_1]) is not a valid problem for set [_2]." =>
-	"The selected problem([_1]) is not a valid problem for set [_2].", 
+	"Le problème choisi([_1]) n'est pas valide pour le devoir [_2].", 
 
 "Download Hardcopy for Selected [plural,_1,Set,Sets]" => 
-	"Seçili Setleri Yazdır: [plural,_1,Set,Sets]",  
+	"Télécharger une copie de : [plural,_1,Set,Sets]",  
 
+## Hardcopy Generator
+"Hardcopy Generator" => 
+	"Imprimer dans un fichier",  
+"You may choose to show any of the following data. Correct answers and solutions are only available after the answer date of the homework set." =>
+	"Options. Les bonnes réponses et les solutions ne sont disponibles qu'après la date de remise.", ## ozcan
+"Show:" => "Afficher : ",
+"Student answers" => "Réponses de l'étudiant",
+"Correct answers" => "Bonnes réponses",
+"Hints" => "Indices",
+"Solutions" => "Solutions",
+"Hardcopy Format:" => "Format :",
+"Generate Hardcopy" => "Générer le fichier",
+
+## Scoring Tools
+"Include Index"                    => "Inclure l'index",
+"Record Scores for Single Sets"    => "Ajouter les résultats de chaque devoir",
+"Pad Fields"                       => "Remplir les champs",
+"Score selected set(s) and save to:" => 
+           "Sauvegarder les résultats sélectionnés sous",
 
 
 "[_1]: Problem [_2]." =>		"[_1]: Problème [_2].", ## gage
 "Next Problem" =>			    "Problème suivant",  ## gage
 "Previous Problem" =>			"Problème précédent", ## gage
 "Problem List" =>			    "Liste des problèmes", ## gage
-"now open, due " =>			    "açık, bitiş tarihi: ", ## gage
-"Set" =>				        "Mis Devoirs", ## gage
-"Score" =>				        "Marque", ## gage
+"now open, due " =>			    "Disponible, date de remise : ", ## gage
+"Set" =>				        "Devoirs", ## gage
+"Score" =>				        "Résultat", ## gage
 "Problems" =>				    "Problèmes", ## gage
 "You can earn partial credit on this problem." =>
-	"Bu sorudan kısmi puan alabilirsiniz.", ## ozcan
+	"Vous pouvez obtenir une partie des points pour ce problème.", ## ozcan
 "You have [negquant,_1,unlimited attempts,attempt,attempts] remaining." =>
-	"[negquant,_1,Sınırsız deneme hakkı,deneme hakkı,deneme hakkı] kaldı.", ## ozcan
+	"Il reste [negquant,_1,un nombre illimité d'essais,essai,essais].", ## ozcan
 
 ## TRANSLATED BY SALIH
+## Traduction : Stéphanie Lanthier
 
-"over time: closed." =>			"süre bitti: kapalı.",
-"open: complete by [_1]" =>		"açık: [_1] tarihine kadar tamamlayın",
-"will open on [_1]" =>			"[_1] tarihinde açılacak",
-"closed, answers on [_1]" =>		"kapalı, cevaplar [_1] tarihinde",
-"closed, answers recently available" =>	"kapalı, cevaplar yeni açıklandı",
-"closed, answers available" =>		"kapalı, cevaplar açıklandı",
-"Viewing temporary file: " =>		"Geçici dosyayı görüntülüyor: ",
-"Course Info" =>			"Ders bilgileri",
-"~[edit~]" =>				"~[düzenle~]",    ## edited - ozcan
-"Course Administration" =>		"Ders Yönetimi",
-"Feedback" =>				"Geri Bildirim",
-"Grades" =>				"Notlar",
-"Instructor Tools" =>			"Eğitmen Araçları",
-"Classlist Editor" =>			"Sınıf Listesi Düzenleyici",
-"Hmwk Sets Editor" =>			"Ödev Setleri Düzenleyici",
-"Add Users" =>				"Kullanıcı Ekle",
-"Course Configuration" =>		"Ders Seçenekleri",
-"Library Browser" =>			"Kütüphane Tarayıcı",
-"File Manager" =>			"Dosya Yöneticisi",
-"Problem Editor" =>			"Soru Düzenleyici",
-"Scoring Tools" =>			"Notlama Araçları",
-"Scoring Download" =>			"Notları İndir",
-"Email" =>				"E-posta",
-"Email instructor" =>			"Eğitmene E-posta",
-"Logout" => 				"Çıkış",
-"Password/Email" =>			"Şifre/E-posta",
-"Statistics" =>				"İstatistikler",
+"over time: closed." =>			"Le temps est écoulé. Fin.",
+"open: complete by [_1]" =>		"Disponible : date de remise : [_1]",
+"will open on [_1]" =>			"sera disponible le [_1]",
+"closed, answers on [_1]" =>		"échu, réponses disponibles le [_1]",
+"closed, answers recently available" =>	"échu, réponses disponibles depuis peu",
+"closed, answers available" =>		"échu, réponses disponbles",
+"Viewing temporary file: " =>		"Visionnement du fichier temporaire : ",
+"Course Info" =>			"Informations sur le cours",
+"~[edit~]" =>				"~[modifier~]",    ## edited - ozcan
+"Course Administration" =>		"Administration du cours",
+"Feedback" =>				"Commentaires",
+"Grades" =>				"Résultats",
+"Instructor Tools" =>			"Outils pour l'enseignant",
+"Classlist Editor" =>			"Liste d'étudiants",
+"Hmwk Sets Editor" =>			"Éditeur de devoirs",
+"Add Users" =>				"Ajouter des usagers",
+"Course Configuration" =>		"Configuration du cours",
+"Library Browser" =>			"Choisir des problèmes",
+"Library Browser 2" =>			"Choisir des problèmes 2",
+"File Manager" =>			"Gestionnaire de fichiers",
+"Problem Editor" =>			"Éditeur de problèmes",
+"Scoring Tools" =>			"Exporter les résultats",
+"Scoring Download" =>			"Télécharger les résultats",
+"Email" =>				"Courriel",
+"Clear" =>				"Désélectionner",
+"Email instructor" =>			"Envoyer un courriel à l'enseignant",
+"Logout" => 				"Déconnexion",
+"Password/Email" =>			"Mot de passe/courriel",
+"Statistics" =>				"Statistiques",
+"Student Progress" =>		        "Progrès des étudiants",
+"Help" =>		                "Aide",
+
+## Sets
+"Sets" => "Devoirs",
+
+## Display Options
+"Display Options" => "Options d'affichage",
+"View equations as" => "Afficher les équations à l'aide de",
+"Apply Options" => "Appliquer",
+
+## Set Info
+"Set Info" => "Informations",
+"WeBWorK Assignment [_1] is due : [_2]." => 
+       "Date de remise du devoir [_1] : [_2]",
+
+## E-mail Instructor
+"E-mail Instructor" =>	"Envoyer un courriel à l'enseignant",
+"From:" =>  "De :",
+"Use this form to report to your professor a problem with the WeBWorK system or an error in a problem you are attempting. Along with your message, additional information about the state of the system will be included." => "Utiliser ce formulaire pour signaler à l'enseignant un problème avec le système WeBWorK ou une erreur dans un problème d'un devoir. Des informations additionnelles sur l'état du système utilisé accompagneront votre message.",
+"E-mail:" =>  "Corps du message :",
+"Send E-mail:" =>  "Envoyer",
+"Cancel Email" =>  "Annuler",
+
 
 ## TRANSLATED by OZCAN
+## Traduction : Stéphanie Lanthier
 
+"Courses" =>		"Cours",
+"Homework Sets" => 	"Devoirs",
+"Problem [_1]" => 	"Problème [_1]",
+"Library Browser" =>    "Choisir des problèmes",
+"Report bugs" => 	"Signaler un bogue",
 
-"Courses" =>		"Dersler",
-"Homework Sets" => 	"Soru Grupları",
-"Problem [_1]" => 	"Soru [_1]",
-"Library Browser" => 	"Soru Kütüphaneleri",
-"Report bugs" => 	"Hataları bildir",
+"Logged in as [_1]. " => "Connecté sous le nom [_1]",
+"Log Out" => 		"Se déconnecter",
+"Not logged in." => 	"Non connecté.",
+"Acting as [_1]. " =>  	"Prendre le rôle de [_1].",
+"Stop Acting" => 	"Cesser de jouer le rôle",
 
-"Logged in as [_1]. " => "[_1] olarak giriş yaptınız.",
-"Log Out" => 		"Çıkış Yap",
-"Not logged in." => 	"Giriş yapmadınız.",
-"Acting as [_1]. " =>  	"[_1] gibi davranılıyor.",
-"Stop Acting" => 	"Rol Yapmayı bırak",
+"Welcome to WeBWorK!" => "Bienvenue sur WeBWorK!",
+"Messages" => 		"Messages",
+"Entered" => 		"Saisi",
+"Result" => 		"Résultat",
+"Answer Preview" => 	"Aperçu des réponses",
 
-"Welcome to WeBWorK!" => "WeBWorK sitemize hoşgeldiniz!",
-"Messages" => 		"Mesajlar",
-"Entered" => 		"Yanıt",
-"Result" => 		"Sonuç",
-"Answer Preview" => 	"Gösterim",
+"Correct" => 		"Correct",
+"correct" => 		"correct",
+"[_1]% correct" => 	"[_1]% correct",
+"incorrect" => 		"erroné",
+"Published" => 		"Publié",
 
-"Correct" => 		"Doğru yanıt",
-"correct" => 		"doğru",
-"[_1]% correct" => 	"[_1]% doğru",
-"incorrect" => 		"yanlış",
-"Published" => 		"Yayınlandı",
+"Unpublished" => 	"Non publié",
+"Show Hints" => 	"Recourir à des indices",
+"Show Solutions" => 	"Afficher les solutions",
+"Preview Answers" => 	"Visualiser les réponses",
+"Check Answers" => 	"Vérifier les réponses",
 
-"Unpublished" => 	"Yayınlanmadı",
-"Show Hints" => 	"İpuçlarını göster",
-"Show Solutions" => 	"Çözümleri göster",
-"Preview Answers" => 	"Yanıtları görüntüle",
-"Check Answers" => 	"Yanıtları kontrol et",
-
-"Submit Answers" => 	"Yanıtları Gönder",
+"Submit Answers" => 	"Soumettre ses réponses",
 "Submit Answers for [_1]" => 
-	"[_1] için Yanıtları Gönder",
-"times" => 		"deneme hakkı",
-"time" => 		"deneme hakkı",
-"unlimited" =>  	"sınırsız",
+	"[_1] Soumettre ses réponses",
+"times" => 		"reprises",
+"time" => 		"reprise",
+"unlimited" =>  	"illimité",
 
-"attempts" =>  		"deneme hakkı",
-"attempt" => 		"deneme hakkı",
-"Name" => 		"Grup Adı", ## edited - ozcan
-"Attempts" => 		"Deneme sayısı",
-"Remaining" => 		"Kalan hak",
+"attempts" =>  		"essais",
+"attempt" => 		"essai",
+"Name" => 		"Nom", ## edited - ozcan
+"Attempts" => 		"Essais",
+"Remaining" => 		"Nombre d'essais restants",
 
-"Worth" => 		"Değeri",
-"Status" => 		"Durumu",
-"Change Password" => 		"Şifre Değiştir",
-"[_1]'s Current Password" => 	"[_1] için Mevcut Şifre",
-"[_1]'s New Password" => 	"[_1] için Yeni Şifre",
+"Worth" => 		"Pondération",
+"Status" => 		"Statut",
+"Change Password" => 		"Changer le mot de passe",
+"[_1]'s Current Password" => 	"Mot de passe actuel [_1]",
+"[_1]'s New Password" => 	"Nouveau mot de passe [_1]",
 
-"Change Email Address" => 	"E-posta Adresi Değiştir",
-"[_1]'s Current Address" => 	"[_1] için Mevcut Adres",
-"[_1]'s New Address" => 	"[_1] için Yeni Adres",
-"Change User Options" => 	"Kullanıcı bilgilerini güncelle",
-"Your score was recorded." => 	"Notunuz kaydedildi.",
+"Change Email Address" => 	"Changer l'adresse courriel",
+"[_1]'s Current Address" => 	"Courriel actuel [_1]",
+"[_1]'s New Address" => 	"Nouveau courriel [_1]",
+"Change User Options" => 	"Changer les options d'usager",
+"Your score was recorded." => 	"Votre résultat a été enregistré",
 
-"Show correct answers" => 	"Doğru yanıtları göster",
-"This homework set is closed." => "Bu soru grubu kapalı.",
-"Show Past Answers" => 		"Önceki Yanıtları Göster",
-"Log In Again" => 		"Tekrar giriş yap",
+"Show correct answers" => 	"Montrer les bonnes réponses",
+"This homework set is closed." => "Ce devoir est échu.",
+"Show Past Answers" => 		"Afficher les réponses précédentes",
+"Log In Again" => 		"Se connecter à nouveau",
 
 
 "The answer above is correct." => 
-	"Yukarıdaki yanıt doğru.",
+	"La réponse ci-dessus est correcte.",
 
 "The answer above is NOT [_1]correct." => 
-	"Yukarıdaki yanıt [_1]doğru değil.",
+	"La réponse ci-dessus N'EST PAS [_1]correcte.",
 
 "All of the answers above are correct." => 
-	"Yanıtların tümü doğru",
+	"Les réponses ci-dessus sont toutes correctes.",
 
 "At least one of the answers above is NOT [_1]correct." => 
-	"Yanıtların en az bir tanesi [_1]doğru değil.",
+	"Au moins une des réponses ci-dessus N'EST PAS [_1]correcte.",
 
 "[quant,_1,of the questions remains,of the questions remain] unanswered." => 
-	"Soruların [_1] tanesi yanıtsız bırakıldı.",
+	"Certaines questions [_1] restent sans réponse.",
 
 "This set is [_1] students." =>	
-	"Bu soru grubu öğrenciler tarafından [_1].",
+	"Ce devoir est [_1] pour les étudiants.",
 
 "visible to" => 
-	"görüntüleniyor",
+	"visible",
 
 "hidden from" => 
-	"görüntülenmiyor",
+	"invisible",
 
 "This problem will not count towards your grade." => 
-	"Bu soru, notunuzu etkilemeyecektir.",
+	"Ce problème n'est pas noté.",
 
 "The selected problem ([_1]) is not a valid problem for set [_2]." =>
-	"Seçilen problem ([_1]), [_2] soru drubu için geçerli bir soru değil.",
+	"Le problème choisi ([_1]), n'est pas valide pour l'ensemble [_2].",
 
 "You do not have permission to view the details of this error." => 
-	"Bu hatanın detaylarını görmeye yetkiniz yok.",
+	"Les droits qui vous sont assignés ne permettent pas de voir les détails de cette erreur.",
 
 "Your score was not recorded because there was a failure in storing the problem record to the database." =>
-	"Notunuz kaydedilemedi, çünkü notun veritabanına kaydı sırasında bir hata oluştu.",
+	"Votre résultat n'a pas été enregistré à cause d'une défaillance dans le processus d'enregistrement dans la base de données.",
 
 "Your score was not recorded because this homework set is closed." =>
-	"Notunuz kaydedilemedi çünkü bu soru grubu kapalı.",
+	"Votre résultat n'a pas été enregistré car la date de remise est passée.",
 
 "Your score was not recorded because this problem has not been assigned to you." =>
-	"Notunuz kaydedilmedi çünkü bu soru size yöneltilmedi.",
+	"Votre résultat n'a pas été enregistré car ce problème ne vous a pas été assigné.",
 
 "Viewing temporary file: " => 	
-	"Geçici dosya görüntüleniyor: ",
+	"Visionnement du fichier temporaire : ",
 
 "ANSWERS ONLY CHECKED -- ANSWERS NOT RECORDED" => 
-	"YANITLAR SADECE KONTROL EDİLİYOR -- YANITLAR KAYDEDİLMEDİ",
+	"Les réponses ont été vérifiées -- MAIS LES RÉPONSES N'ONT PAS ÉTÉ ENREGISTRÉES",
 
 "PREVIEW ONLY -- ANSWERS NOT RECORDED" => 
-	"GÖSTERİM -- YANITLAR KAYDEDİLMEDİ ",
+	"Afficher seulement  --  NE PAS ENREGISTRER LES RÉPONSES",
 
 "submit button clicked" =>
- 	"gönder düğmesine basıldı",
+ 	"On a appuyé sur le bouton Soumission",
 
 "This homework set is not yet open." => 
-	"Bu soru grubu henüz açık değil.",
+	"Ce devoir n'est pas encore disponible.",
+
+"This set is visible to students." => 
+	"Les étudiants voient ce devoir.",
 
 "(This problem will not count towards your grade.)" => 
-	"(Bu soru notunuzu etkilemeyecektir.)",
+	"(Ce problème ne sera pas noté.)",
 
 "You have attempted this problem [quant,_1,time,times]." => 
-	"Bu soru için [quant,_1,deneme hakkı,deneme hakkı] kullandınız.",
+	"Vous avez essayé ce problème à [quant,_1,reprise,reprises].",
 
 "You received a score of [_1] for this attempt." => 
-	"Bu soru için [_1] puan aldınız.",
+	"Résultat pour cette tentative : [_1].",
 
 "Your overall recorded score is [_1].  [_2]" => 
-	"Ortalama puanınız: [_1].  [_2]",
+	"Le résultat final est : [_1].  [_2]",
 
 "You have [_1] [_2] remaining." => 
-	"[_1] [_2] kaldı.",
+	"Il reste [_1] [_2].",
 
 "Download a hardcopy of this homework set." => 
-	"Bu soru grubunu basılı halde indirin." ,
+	"Télécharger une copie de ce devoir." ,
+
+"Download PDF or TeX Hardcopy for Current Set" => 
+	"Télécharger une copie PDF ou TeX de ce devoir.",
+
+"Download PDF or TeX Hardcopy for Selected Sets" => 
+	"Télécharger une copie PDF ou TeX des devoirs sélectionnés.",
 
 "This homework set contains no problems." => 
-	"Bu soru grubunda soru bulunmamaktadır.",
+	"Ce devoir ne contient aucun problème.",
 
 "Can't get password record for user '[_1]': [_2]" => 
 	"Can't get password record for user '[_1]': [_2]",
@@ -219,47 +297,47 @@ use vars qw(%Lexicon);
 	"Can't get password record for effective user '[_1]': [_2]",
 
 "Couldn't change [_1]'s password: [_2]" => 
-	"[_1] adlı kullanıcının şifresi değiştirilemedi: [_2]",
+	"Il n'a pas été possible de modifier le mot de passei : [_2] de l'usager : [_1]",
 
 "[_1]'s password has been changed." => 
-	"[_1] adlı kullanıcının şifresi değiştirildi.",
+	"Le mot de passe de l'usager [_1] a été modifié.",
 
 "The passwords you entered in the [_1] and [_2] fields don't match. Please retype your new password and try again." =>
-   "[_1] ve [_2]  alanlarına girdiğiniz şifreler uyuşmadı. Yeni şifrenizi girerek tekrar deneyiniz.",
+   "Les mots de passe saisis dans les champs [_1] et [_2] sont différents. Veuiller essayer à nouveau",
 
 "Confirm [_1]'s New Password" => 
-	"[_1] için Şifre Onayı",
+	"Confirmer le nouveau mot de passe [_1]",
 
 "[_1]'s new password cannot be blank." => 
-	"[_1] için yeni şifre boş bırakılamaz.",
+	"Le nouveau mot de passe ne peut rester vide. [_1]",
 
 "The password you entered in the [_1] field does not match your current password. 
 Please retype your current password and try again." => 
-	"[_1] alanına girdiğiniz şifre ile mevcut şifreniz uyuşmamaktadır. Lütfen şirfenizi girerek tekrar deneyiniz.",
+	"Le mot de passe que vous avez entré dans la case [_1] est erroné. Veuillez le corriger et essayer de nouveau.",
 
 "You do not have permission to change your password." => 
-	"Şifre değiştirmek için yetkiniz yok.",
+	"Vos droits ne vous permettent pas de modifier votre mot de passe.",
 
 "Couldn't change your email address: [_1]" => 
-	"E-posta adresiniz değiştirilemedi: [_1]", 
+	"Il n'a pas été possible de modifier votre courriel : [_1]", 
 
 "Your email address has been changed." => 
-	"E-posta adresiniz değiştirildi.",
+	"Votre courriel a été modifié.",
 
 "You do not have permission to change email addresses." => 
-	"Adres değiştirmek için yetkiniz yok.",
+	"Vos droits ne vous permettent pas de modifier votre courriel.",
 
 "You have been logged out of WeBWorK." => 
-	"WeBWorK sisteminden çıkış yaptınız.", 
+	"Vous êtes déconnecté de WeBWorK.", 
 
 "Invalid user ID or password." =>
-	"Yanlış kullanıcı adı ya da şifre.",
+	"Code d'usager ou mot de passe invalides",
 
 "You must specify a user ID." =>
-	"Bir kullanıcı adı girmelisiniz.",
+	"Spécifier le code d'usager.",
 
 "Your session has timed out due to inactivity. Please log in again." => 
-	"Oturumunuz zaman aşımına uğradı. Lütfen tekrar giriş yapınız.",
+	"Cette session est échue. Veuillez vous reconnecter",
 
 "_REQUEST_ERROR" => q{
  WebWork bu problemi işlerken bir yazılım hatası ile karşılaştı. Problemin kendisinde bir hata olması muhtemeldir. Eğer bir öğrenci iseniz bu hatayı ilgili kişilere bildiriniz. Eğer yetkili bir kişiyseniz daha fazla bilgi için alttaki hata raporunu inceleyiniz.
