3f55c60bd1ed770ae942f2e5f795b0c22b51856d
Geoff Goehle <goehle@gmail.com>
2012-06-04 17:22:05 -0400

    Fixed borked URLPaths merge

--- lib/WeBWorK/URLPath.pm
+++ lib/WeBWorK/URLPath.pm
@@ -46,6 +46,7 @@ PLEASE FOR THE LOVE OF GOD UPDATE THIS IF YOU CHANGE THE HEIRARCHY BELOW!!!
  
  course_admin                        /admin/ -> logout, options, instructor_tools
  html2xml                            /html2xml/
+ instructorXMLHandler     			     /instructorXMLHandler/
  set_list                            /$courseID/
  
  equation_display                    /$courseID/equation/
@@ -67,9 +68,18 @@ PLEASE FOR THE LOVE OF GOD UPDATE THIS IF YOU CHANGE THE HEIRARCHY BELOW!!!
  instructor_user_detail              /$courseID/instructor/users/$userID/
  instructor_sets_assigned_to_user    /$courseID/instructor/users/$userID/sets/
  
+ instructor_user_list2                /$courseID/instructor/users2/
+ instructor_user_detail2              /$courseID/instructor/users2/$userID/ #not created yet
+ instructor_sets_assigned_to_user2    /$courseID/instructor/users2/$userID/sets/ #not created yet
+
+ 
  instructor_set_list                 /$courseID/instructor/sets/
  instructor_set_detail               /$courseID/instructor/sets/$setID/
  instructor_users_assigned_to_set    /$courseID/instructor/sets/$setID/users/
+ 
+ instructor_set_list2                 /$courseID/instructor/sets2/
+ instructor_set_detail2               /$courseID/instructor/sets2/$setID/ #not created yet
+ instructor_users_assigned_to_set2    /$courseID/instructor/sets2/$setID/users/ #not created yet
  
  instructor_add_users                /$courseID/instructor/add_users/
  instructor_set_assigner             /$courseID/instructor/assigner/
@@ -77,6 +87,7 @@ PLEASE FOR THE LOVE OF GOD UPDATE THIS IF YOU CHANGE THE HEIRARCHY BELOW!!!
  instructor_file_manager             /$courseID/instructor/file_manager/
  instructor_set_maker                /$courseID/instructor/setmaker/
  instructor_set_maker2               /$courseID/instructor/setmaker2/
+ instructor_set_maker3               /$courseID/instructor/setmaker3/
  instructor_get_target_set_problems  /$courseID/instructor/GetTargetSetProblems/
  instructor_get_library_set_problems /$courseID/instructor/GetLibrarySetProblems/
  instructor_config                   /$courseID/instructor/config/
@@ -86,6 +97,11 @@ PLEASE FOR THE LOVE OF GOD UPDATE THIS IF YOU CHANGE THE HEIRARCHY BELOW!!!
  instructor_problem_editor_withset   /$courseID/instructor/pgProblemEditor/$setID/
  instructor_problem_editor_withset_withproblem
                                      /$courseID/instructor/pgProblemEditor/$setID/$problemID/
+                                     
+ instructor_problem_editor2           /$courseID/instructor/pgProblemEditor2/
+ instructor_problem_editor2_withset   /$courseID/instructor/pgProblemEditor2/$setID/
+ instructor_problem_editor2_withset_withproblem
+                                     /$courseID/instructor/pgProblemEditor2/$setID/$problemID/
  
  instructor_scoring                  /$courseID/instructor/scoring/
  instructor_scoring_download         /$courseID/instructor/scoringDownload/
@@ -119,7 +135,7 @@ our %pathTypes = (
 	root => {
 		name    => 'WeBWorK',
 		parent  => '',
-		kids    => [ qw/course_admin  html2xml set_list / ],
+		kids    => [ qw/course_admin  html2xml instructorXMLHandler set_list / ],
 		match   => qr|^/|,
 		capture => [ qw// ],
 		produce => '/',
@@ -145,7 +161,15 @@ our %pathTypes = (
 		produce => 'html2xml/',
 		display => 'WeBWorK::ContentGenerator::renderViaXMLRPC',
 	},
-
+	instructorXMLHandler => {
+		name => 'instructorXMLHandler',
+		parent => 'root',
+		kids => [ qw// ],
+		match   => qr|^instructorXMLHandler/|,
+		capture => [ qw// ],
+		produce => 'instructorXMLHandler/',
+		display => 'WeBWorK::ContentGenerator::instructorXMLHandler',
+	},
 	set_list => {
 		name    => '$courseID',
 		parent  => 'root',
@@ -286,7 +310,9 @@ our %pathTypes = (
 		kids    => [ qw/instructor_user_list instructor_user_list2 instructor_set_list instructor_set_list2 
 		    instructor_add_users instructor_achievement_list
 			instructor_set_assigner instructor_file_manager
-			instructor_problem_editor instructor_set_maker instructor_set_maker2 instructor_get_target_set_problems instructor_get_library_set_problems instructor_compare
+			instructor_problem_editor instructor_problem_editor2 
+			instructor_set_maker instructor_set_maker2 instructor_set_maker3 
+			instructor_get_target_set_problems instructor_get_library_set_problems instructor_compare
 			instructor_config
 			instructor_scoring instructor_scoring_download instructor_mail_merge
 			instructor_answer_log instructor_preflight instructor_statistics
@@ -308,6 +334,15 @@ our %pathTypes = (
 		capture => [ qw// ],
 		produce => 'users/',
 		display => 'WeBWorK::ContentGenerator::Instructor::UserList',
+	},
+	instructor_user_list2 => {
+		name    => 'Classlist Editor2',
+		parent  => 'instructor_tools',
+		kids    => [ qw/instructor_user_detail/ ],
+		match   => qr|^users2/|,
+		capture => [ qw// ],
+		produce => 'users2/',
+		display => 'WeBWorK::ContentGenerator::Instructor::UserList2',
 	},
 	instructor_user_detail => {
 		name    => 'Sets assigned to $userID',
@@ -338,6 +373,15 @@ our %pathTypes = (
 		capture => [ qw// ],
 		produce => 'sets/',
 		display => 'WeBWorK::ContentGenerator::Instructor::ProblemSetList',
+	},
+	instructor_set_list2 => {
+		name    => 'Hmwk Sets Editor2',
+		parent  => 'instructor_tools',
+		kids    => [ qw/instructor_set_detail/ ],
+		match   => qr|^sets2/|,
+		capture => [ qw// ],
+		produce => 'sets2/',
+		display => 'WeBWorK::ContentGenerator::Instructor::ProblemSetList2',
 	},
 	instructor_set_detail => {
 		name    => 'Set Detail for set $setID',
@@ -415,6 +459,15 @@ our %pathTypes = (
 		produce => 'setmaker2/',
 		display => 'WeBWorK::ContentGenerator::Instructor::SetMaker2',
 	},
+		instructor_set_maker3 => {
+		name    => 'Library Browser 3',
+		parent  => 'instructor_tools',
+		kids    => [ qw// ],
+		match   => qr|^setmaker3/|,
+		capture => [ qw// ],
+		produce => 'setmaker3/',
+		display => 'WeBWorK::ContentGenerator::Instructor::SetMaker3',
+	},
 	instructor_get_target_set_problems => {
 		name    => 'Get Target Set Problems',
 		parent  => 'instructor_tools',
@@ -451,10 +504,28 @@ our %pathTypes = (
 		produce => 'pgProblemEditor/',
 		display => 'WeBWorK::ContentGenerator::Instructor::PGProblemEditor',
 	},
+	instructor_problem_editor2 => {
+		name    => 'Problem Editor2',
+		parent  => 'instructor_tools',
+		kids    => [ qw/instructor_problem_editor2_withset/ ],
+		match   => qr|^pgProblemEditor2/|,
+		capture => [ qw// ],
+		produce => 'pgProblemEditor2/',
+		display => 'WeBWorK::ContentGenerator::Instructor::PGProblemEditor2',
+	},
 	instructor_problem_editor_withset => {
 		name    => '$setID',
 		parent  => 'instructor_problem_editor',
 		kids    => [ qw/instructor_problem_editor_withset_withproblem/ ],
+		match   => qr|^([^/]+)/|,
+		capture => [ qw/setID/ ],
+		produce => '$setID/',
+		display => undef,
+	},
+	instructor_problem_editor2_withset => {
+		name    => '$setID',
+		parent  => 'instructor_problem_editor2',
+		kids    => [ qw/instructor_problem_editor2_withset_withproblem/ ],
 		match   => qr|^([^/]+)/|,
 		capture => [ qw/setID/ ],
 		produce => '$setID/',
@@ -468,6 +539,15 @@ our %pathTypes = (
 		capture => [ qw/problemID/ ],
 		produce => '$problemID/',
 		display => 'WeBWorK::ContentGenerator::Instructor::PGProblemEditor',
+	},
+	instructor_problem_editor2_withset_withproblem => {
+		name    => '$problemID',
+		parent  => 'instructor_problem_editor2_withset',
+		kids    => [ qw// ],
+		match   => qr|^([^/]+)/|,
+		capture => [ qw/problemID/ ],
+		produce => '$problemID/',
+		display => 'WeBWorK::ContentGenerator::Instructor::PGProblemEditor2',
 	},
 	instructor_scoring => {
 		name    => 'Scoring Tools',
@@ -544,7 +624,6 @@ our %pathTypes = (
 		produce => 'student/$userID/',
 		display => 'WeBWorK::ContentGenerator::Instructor::Stats',
 	},
-
 
 	################################################################################
 
