56cd512db9b63e1560450ec5b3afe528118ecc77
root <basyrova@uwstout.edu>
2012-08-08 11:27:24 -0500

    changes to dragmath.js for better form identification; changes to gateway.template to enable dragmath

--- htdocs/js/dragmath.js
+++ htdocs/js/dragmath.js
@@ -1,10 +1,23 @@
 var thedata;
 var newwin;
 var thenumber;
+var ourform;
 function dragmathedit(textarea)
 {
     thenumber = textarea;
-    theform = 'problemMainForm';
+    if (document.forms.length == 1){
+        ourform = 0;
+    } else {
+        for (i=0; i < document.forms.length; i++){
+            if (document.forms[i].name == 'problemMainForm'){
+                 ourform = i;
+            }
+            if (document.forms[i].name == 'gwquiz'){
+                 ourform = i;
+            }
+        }
+    }
+    theform = ourform;
     thedata = document.forms[theform].elements[textarea].value;
     newwin = window.open("/webwork2_files/dragmathedit/webwork-popup.html","","width=600,height=450,resizable");
 }
