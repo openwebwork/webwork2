0f31aca2f7b6823ce3ff594f428dd54f1efa5456
Geoff Goehle <goehle@gmail.com>
2013-11-13 11:43:55 -0500

    Removed tr:'s from localize.  These are not appropriate for flags because they appear on the content pages and are confusing.

--- lib/WeBWorK/Localize.pm
+++ lib/WeBWorK/Localize.pm
@@ -105,7 +105,7 @@ in the fields below, and hit the \"Take Action!\" button at the bottom of the fo
 the page contains a table displaying the sets and several pieces of relevant information.",
 },
 "_CLASSLIST_EDITOR_DESCRIPTION" => q{
-tr: This is the classlist editor page, where you can view and edit the records of all the students
+This is the classlist editor page, where you can view and edit the records of all the students
 currently enrolled in this course.  The top of the page contains forms which allow you to filter
 which students to view, sort your students in a chosen order, edit student records, give new
 passwords to students, import/export student records from/to external files, or add/delete students.
@@ -121,17 +121,17 @@ This is the Achievement Editor.  It is used to edit the achievements available t
 -The \"level\" category is used for the achievements associated to a users level.;
 },
 "_REDUCED_CREDIT_MESSAGE_1" => q{
-tr: This assignment has a Reduced Credit Period that begins [_1] and
+This assignment has a Reduced Credit Period that begins [_1] and
 ends on the due date, [_2].  During this period all additional work done counts [_3]\% of the
 original.
 },
 
 "_REDUCED_CREDIT_MESSAGE_2" => q{
-tr: This assignment had a Reduced Credit Period that began [_1] and
+This assignment had a Reduced Credit Period that began [_1] and
 ended on the due date, [_2].  During that period all additional work done counted [_3]\% of the
 original.
 },
-"_GUEST_LOGIN_MESSAGE"   => q{tr: This course supports guest logins. Click [_1] to log into this course as a guest.},
+"_GUEST_LOGIN_MESSAGE"   => q{This course supports guest logins. Click [_1] to log into this course as a guest.},
 
 "_EXTERNAL_AUTH_MESSAGE" => q{[_1] uses an external authentication system.  You've authenticated through that system, but aren't allowed to log in to this course.},
 
