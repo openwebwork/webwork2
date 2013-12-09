54ee7b25941da9c3ecec1124920dc2b7d3535c93
John Jones <jj@asu.edu>
2013-12-09 15:15:07 -0700

    Make sure problem level is passed from javascript when writing problem tags.

--- lib/WeBWorK/ContentGenerator/instructorXMLHandler.pm
+++ lib/WeBWorK/ContentGenerator/instructorXMLHandler.pm
@@ -194,6 +194,7 @@ sub pre_header_initialize {
 		    library_subjects	    => $r->param("library_subjects") ||undef,
 		    library_chapters	    => $r->param("library_chapters") ||undef,
 		    library_sections	    => $r->param("library_sections") ||undef,
+		    library_levels		    => $r->param("library_levels") ||undef,
 		    library_textbook	    => $r->param("library_textbook") ||undef,
 		    library_keywords	    => $r->param("library_keywords") ||undef,
 		    library_textchapter     => $r->param("library_textchapter") ||undef,
