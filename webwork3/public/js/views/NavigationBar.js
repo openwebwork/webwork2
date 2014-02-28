45d64748b5e35d15ff7cf694e908b6a4fc6fbc58
Peter Staab <peter.staab@gmail.com>
2014-02-28 05:23:58 -0500

    Trying to make the Homework Manager more modular.

--- webwork3/public/js/views/NavigationBar.js
+++ webwork3/public/js/views/NavigationBar.js
@@ -10,14 +10,14 @@ define(['backbone'], function(Backbone){
 				this.trigger("change-view",{link: $(evt.target).data("link"), name: $(evt.target).data("name")});
 			},
 			"click .main-help-button": function(evt){
-				this.trigger("help");
+				this.trigger("open-option",{link: "helpSidepane", name: "Help"});
 			},
 			"click .option-menu a": function(evt){
 				this.trigger("open-option",{link: $(evt.target).data("link"), name: $(evt.target).data("name")});
 			}
 		},
 		setPaneName: function(name){
-			this.$(".pane-name").text(name);
+			this.$(".main-view-name").text(name);
 		}, 
 		setLoginName: function(name){
 			this.$(".logged-in-as").text(name);
