4168043d93ddedb40277b4738d3b5b4293af959f
Peter Staab <peter.staab@gmail.com>
2014-03-19 16:51:23 -0400

    move the Settings messages.

--- webwork3/public/js/apps/CourseManager/main-views/SettingsView.js
+++ webwork3/public/js/apps/CourseManager/main-views/SettingsView.js
@@ -8,31 +8,57 @@ define(['backbone','config','views/WWSettingsView','views/MainView'],function(Ba
 
         this.categories = this.settings.chain().pluck("attributes").pluck("category")
             .unique().difference("timezone").value();
+        this.setMessages();
      }, 
      events: {
         "shown.bs.tab a[data-toggle='tab']": "changeSettingTab"
      },
      render: function () {
+        this.currentCategory = this.currentCategory || "General";
         // get all of the categories except for timezone (include it somewhere?)
         this.$el.html(_.template($("#settings-template").html(),{categories: this.categories}));
-        var settings = this.settings.where({category: this.categories[0]});
-        this.$(".tab-content .active").empty().append((new WWSettingsView({settings: settings})).render().el);
-        this.$('.nav-tabs a:first').tab('show');
+        this.changeSettingTab(this.currentCategory);
+        this.$(".settings-tabs a[href='#setting-tab"+_(this.categories).indexOf(this.currentCategory)+"']").tab("show");
         return this;
 
      },
      changeSettingTab: function(evt){
-        this.currentCategory = $(evt.target).text();
+        this.currentCategory = _.isString(evt)? evt : $(evt.target).text();
         var settings = this.settings.where({category: this.currentCategory});
         this.$(".tab-content .active").empty().append((new WWSettingsView({settings: settings})).render().el);
+        this.eventDispatcher.trigger("save-state");
 
      }, 
      getHelpTemplate: function (){
         return $("#settings-help-template").html();
      },
      getState: function () {
-        return this.currentCategory;
-     }
+        return {subview: this.currentCategory};
+     },
+     setState: function(state){
+        if(state){
+            this.currentCategory = state.subview;
+        }
+        return this;
+    },
+     setMessages: function () {
+        var self = this; 
+                /* Set the events for the settings */
+        this.settings.on({
+            change: function(setting){
+                setting.changingAttributes=_.pick(setting._previousAttributes,_.keys(setting.changed));
+            },
+            sync: function(setting){
+                _(_.keys(setting.changingAttributes)).each(function(key){
+                    self.eventDispatcher.trigger("add-message",{type: "success",
+                        short: config.msgTemplate({type:"setting_saved",opts:{varname:setting.get("var")}}), 
+                        text: config.msgTemplate({type:"setting_saved_details"
+                                ,opts:{varname:setting.get("var"), oldValue: setting.changingAttributes[key],
+                                    newValue: setting.get("value") }})}); 
+                });
+            }
+        }); 
+    }
 });
 
 return SettingsView;
