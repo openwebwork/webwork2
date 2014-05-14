3c71146c85c8c185f9f84b131615746931f748f2
Peter Staab <peter.staab@gmail.com>
2014-05-14 12:20:06 -0400

    Fixed the settings help bug in Issue #378

--- webwork3/public/js/views/WWSettingsView.js
+++ webwork3/public/js/views/WWSettingsView.js
@@ -9,6 +9,7 @@ function(Backbone, _,config){
         initialize: function (options) {
             _.bindAll(this,'render');
             this.settings = options.settings;
+            this.rowTemplate = _.template($("#row-setting-template").html());
 
             _(this.settings).each(function(setting){
                 setting.on("change", function (model) {
@@ -22,39 +23,46 @@ function(Backbone, _,config){
             this.$el.html($("#settings-table-template").html());
             var table = this.$(".settings-table tbody");
             _(this.settings).each(function(setting){
+                var opts, propHTML; 
                 switch(setting.get("type")){
-                    case "boolean":
-                        var opts = [{label: "true", value: "1"}, {label: "false", value: "0"}];
-                        var propHtml = "<select class='select-list'></select>";
-                        table.append((new SelectSettingView({model: setting, theOptions: opts,
-                                                                 prop_html: propHtml})).render().el);
-                        break;
-                    case "checkboxlist":
-                        var propHtml = "<select multiple='multiple' class='select-list'></select>";
-                        var opts = _(setting.get('values')).map(function(opt){ return {label: opt, value: opt}; } );
-                        table.append((new SelectSettingView({model: setting, theOptions: opts,
-                                                                 prop_html: propHtml})).render().el);
-                        break;
-                    case "popuplist": 
-                        var propHtml = "<select class='select-list'></select>";
-                        var opts = _(setting.get('values')).map(function(opt){ return {label: opt, value: opt}; } );
-                        table.append((new SelectSettingView({model: setting, theOptions: opts,
-                                                                prop_html: propHtml})).render().el);
-                        break;
-                    case "permission":
-                        var propHtml = "<select class='select-list'></select>";
-                        // the settings have the labels stored instead of number values for permissions
-                        // perhaps we should change this? 
-                        var opts = _(config.permissions).map(function(perm){ return {label: perm.label, value: perm.label}});
-                        table.append((new SelectSettingView({model: setting, theOptions: opts,
-                                                                prop_html: propHtml})).render().el);
-                        break;
                     case "text":
                     case "number": 
-                        table.append((new TextSettingView({model: setting, 
-                                                prop_html: "<input class='property'>"})).render().el);
+                        propHTML = "<input class='property'>";
+                        opts = [];
+                        break;
+                    case "boolean":
+                        opts = [{label: "true", value: "1"}, {label: "false", value: "0"}];
+                        propHtml = "<select class='select-list'></select>";
+                        break;
+                    case "checkboxlist":
+                        propHtml = "<select multiple='multiple' class='select-list'></select>";
+                        opts = _(setting.get('values')).map(function(opt){ return {label: opt, value: opt}; } );
+                        break;
+                    case "popuplist": 
+                        propHtml = "<select class='select-list'></select>";
+                        opts = _(setting.get('values')).map(function(opt){ return {label: opt, value: opt}; } );
+                        break;
+                    case "permission":
+                        propHtml = "<select class='select-list'></select>";
+                        opts = _(config.permissions).map(function(perm){ return {label: perm.label, value: perm.label}});
                         break;
                 }
+                switch(setting.get("type")){
+                    case "text":
+                    case "number": 
+                        table.append((new TextSettingView({model: setting, prop_html: propHTML, 
+                            rowTemplate: self.rowTemplate})).render().el);
+                        break;
+                    case "boolean":
+                    case "checkboxlist":
+                    case "popuplist":
+                    case "permission":
+                        table.append((new SelectSettingView({model: setting, theOptions: opts,rowTemplate: self.rowTemplate,
+                                                                    prop_html: propHtml})).render().el);
+                        break;
+
+                }
+
             });
         return this;
         }
@@ -64,10 +72,13 @@ function(Backbone, _,config){
         tagName: "tr",
         initialize: function(options){
             this.templateOptions = _.pick(options,"prop_html");
+            this.rowTemplate = options.rowTemplate; 
+            _.extend(this.templateOptions,this.model.attributes);
             _.extend(this.bindings,options.bindings);
         },
         render: function () {
-            this.$el.html(_.template($("#row-setting-template").html(),this.templateOptions));
+            this.$el.html(this.rowTemplate(this.templateOptions));
+            this.helpPane = this.$(".help-hidden");
             this.stickit();
             return this;
         },
@@ -75,15 +86,25 @@ function(Backbone, _,config){
             ".doc" : { observe: 'doc',  updateMethod: 'html'},
             ".doc2": { observe: "doc2", updateMethod: "html"}
         },
-        events: {
-                "click .help-button": "openHelp",
+        additionalEvents: {
+            "click .help-button": "toggleHelp",
             "click .close": "closeHelp"
         },
-        openHelp: function (evt){
-            $(evt.target).siblings(".help-hidden").css("display","block");
+        events: function () {
+            return _.extend({},this.childEvents,this.additionalEvents);
+        },  
+        toggleHelp: function(){
+            if(this.helpPane.css("display")==="none"){
+                this.openHelp();
+            } else {
+                this.closeHelp();
+            }
         },
-        closeHelp: function (evt){
-            $(evt.target).parent().css("display","none");
+        openHelp: function (){
+            this.helpPane.css("display","block");
+        },
+        closeHelp: function (){
+            this.helpPane.css("display","none");
         }
     });
 
@@ -91,22 +112,22 @@ function(Backbone, _,config){
 
     var SelectSettingView = SettingView.extend({
         initialize: function (options) {
-            _.bindAll(this,'render');
+            //_.bindAll(this,'render');
             this.theOptions = options.theOptions;
             var theBindings = { ".select-list" : {observe: "value", selectOptions: { collection: "this.theOptions"}}};
             options.bindings = options.bindings? _.extend(options.bindings,theBindings) : theBindings;
-            this.constructor.__super__.initialize.apply(this,[options]);
+            SettingView.prototype.initialize.apply(this,[options]);
         }
     });
 
     var TextSettingView = SettingView.extend({
         initialize: function (options) {
-            _.bindAll(this,'render');
+            //_.bindAll(this,'render');
             var theBindings = { ".property": {observe: "value", events: ['blur']}};
             options.bindings = options.bindings? _.extend(options.bindings,theBindings) : theBindings;
-            this.constructor.__super__.initialize.apply(this,[options]);
+            SettingView.prototype.initialize.apply(this,[options]);
         },
-        events: {"keyup": "checkForEnter"},
+        childEvents: {"keyup": "checkForEnter"},
         checkForEnter: function(evt){
             if(evt.keyCode == 13){
                 $(evt.target).blur();
