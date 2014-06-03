33252adbf4003b0e85511ac530966c186179805b
Peter Staab <peter.staab@gmail.com>
2014-06-03 10:29:45 -0400

    Show/hide reduced scoring dates when applicable.  Before it hide the date, but not the clock icon.

--- webwork3/public/js/apps/config.js
+++ webwork3/public/js/apps/config.js
@@ -145,10 +145,13 @@ define(['backbone','underscore','moment','backbone-validation','stickit','jquery
     Backbone.Stickit.addHandler({
         selector: '.edit-datetime',
         update: function($el, val, model, options){
-            var theDate = moment.unix(val);
-            $el.html(_.template($("#edit-date-time-template").html(),
-                {date: model.get("enable_reduced_scoring")==0 && options.observe==="reduced_scoring_date"
-                    ?"":theDate.format("MM/DD/YYYY")}));
+            //var theDate = moment.unix(val);
+            if(options.observe==="reduced_scoring_date" && ! model.get("enable_reduced_scoring")){
+                $el.html("");
+            } else {
+                $el.html(_.template($("#edit-date-time-template").html(),{date: moment.unix(val).format("MM/DD/YYYY")}));    
+            }
+            
             var setDate = function(evt){
                 var newDate = moment(evt.data.$el.children(".wwdate").val(),"MM/DD/YYYY");
                 var theDate = moment.unix(evt.data.model.get(evt.data.options.observe));
