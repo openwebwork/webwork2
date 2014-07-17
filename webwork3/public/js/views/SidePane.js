a62ea09d5fc3f1eba3312bad944644a8a6810f41
Peter Staab <peter.staab@gmail.com>
2014-07-17 16:57:14 -0400

    Initial work on saving the state of the sidebars

--- webwork3/public/js/views/SidePane.js
+++ webwork3/public/js/views/SidePane.js
@@ -1,5 +1,8 @@
 define(['backbone'],function(Backbone){
 	var SidePane = Backbone.View.extend({
+		initialize: function(){
+			this.state = new Backbone.Model({});
+		},
 		setMainView: function(view){
 			this.mainView = view;
 			return this;
@@ -7,11 +10,22 @@ define(['backbone'],function(Backbone){
 		isOpen: false,
 		render: function() {
 			// this makes sure that the content fits vertically in the sidepane. 
-
 			var h = $(window).height()-$("#menu-navbar-collapse").height()-$("#sidepane-container .sidepane-name").height() - 110; 
 			this.$el.height(h);
-			$(this.$el.children().get(0)).height(h).css("overflow-y","auto")
-		}
+			$(this.$el.children().get(0)).height(h).css("overflow-y","auto");
+			return this;
+		},
+		// the follow can be overridden if the state is not stored in a Backbone Model called this.state.
+		getState: function () {
+            return this.state.attributes;
+        },
+        setState: function (_state) {
+            if(_state){
+                this.state.set(_state);
+            }
+            return this;
+        },
+
 	});
 
 	return SidePane;
