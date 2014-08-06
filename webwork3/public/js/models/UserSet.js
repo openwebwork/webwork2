caa04b555180f28cd327c35bb14e1bede6032108
Peter Staab <peter.staab@gmail.com>
2014-08-06 10:24:09 -0700

    Made the UserSet extend from ProblemSet.

--- webwork3/public/js/models/UserSet.js
+++ webwork3/public/js/models/UserSet.js
@@ -9,41 +9,10 @@
 
 define(['backbone', 'underscore','config','models/ProblemSet','models/UserProblemList','apps/util'], 
     function(Backbone, _,config,ProblemSet,UserProblemList,util){
-    var UserSet = Backbone.Model.extend({
-        defaults: {
-            user_id: "",
-            set_id: "",
-            psvn: "",
-            set_header: "defaultHeader", 
-            hardcopy_header: "",
-            open_date: "",
-            due_date: "",
-            answer_date: "",
-            reduced_scoring_date: "",
-            visible: false,
-            enable_reduced_scoring: false,
-            assignment_type: "",
-            description: "",
-            restricted_release: "",
-            restricted_status: "",
-            attempts_per_version: "",
-            time_interval: "",
-            versions_per_interval: "",
-            version_time_limit: "",
-            version_creation_time: "",
-            problem_randorder: "",
-            version_last_attempt_time: "",
-            problems_per_page: "",
-            hide_score: "",
-            hide_score_by_problem: "",
-            hide_work: "",
-            time_limit_cap: "",
-            restrict_ip: "",
-            relax_restrict_ip: "",
-            restricted_login_proctor: "",
-            hide_hint:"" 
+    var UserSet = ProblemSet.extend({
+        defaults: function () {
+            _.extend({user_id: ""},ProblemSet.prototype.defaults)    
         },
-        idAttribute: "_id",
         url: function () {
             return config.urlPrefix + "courses/" + config.courseSettings.course_id + "/users/" + this.get("user_id") +
             "/sets/" + this.get("set_id");
