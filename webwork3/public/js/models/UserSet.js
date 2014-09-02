dfc4340fd2391129547cbb57600ccb8c7a470ac4
Peter Staab <peter.staab@gmail.com>
2014-09-02 17:52:58 -0400

    made so the UserSet and UserSetList parses data correctly similar to ProblemSet and ProblemSetList.

--- webwork3/public/js/models/UserSet.js
+++ webwork3/public/js/models/UserSet.js
@@ -49,18 +49,22 @@ define(['backbone', 'underscore','config','models/ProblemSet','models/UserProble
         initialize: function(opts){
             if(_.isObject(opts)){
                 _(this.attributes).extend(_(util.parseAsIntegers(opts,this.integerFields)).pick(this.integerFields));    
+                var pbs = (opts && opts.problems) ? opts.problems : [];
+                if(pbs instanceof UserProblemList){
+                    this.problems = pbs;
+                } else {
+                    this.problems = new UserProblemList(pbs,{user_id: this.get("user_id")});    
+                }
+                this.attributes.problems = this.problems;
             }
-            var pbs = (opts && opts.problems) ? opts.problems : [];
-            this.problems = new UserProblemList(pbs,{user_id: this.get("user_id")});
-            this.attributes.problems = this.problems;
         },
         url: function () {
             return config.urlPrefix + "courses/" + config.courseSettings.course_id + "/users/" + this.get("user_id") +
             "/sets/" + this.get("set_id");
         },
         parse: function(response){
-            if(response.problems){
-                response.problems = new UserProblemList(response.problems,{user_id: data.user_id,set_id: data.set_id});
+            if(response.problems && _.isArray(response.problems)){
+                response.problems = new UserProblemList(response.problems,{user_id: response.user_id,set_id: response.set_id});
             }
             response = util.parseAsIntegers(response,this.integerFields);
             return response;
