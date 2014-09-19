5347de777027688c731fb1cc298afa3491a07f13
Geoff Goehle <goehle@gmail.com>
2014-09-19 10:56:36 -0400

    get_server_version depreciated, using get_server_banner instead.

--- lib/WeBWorK/Authen.pm
+++ lib/WeBWorK/Authen.pm
@@ -907,7 +907,7 @@ sub write_log_entry {
 
 	# If its apache 2.4 then it has to also mod perl 2.0 or better
 	my $APACHE24 = 0;
-	if (MP2 && Apache2::ServerUtil::get_server_version() =~ 
+	if (MP2 && Apache2::ServerUtil::get_server_banner() =~ 
 	  m:^Apache/(\d\.\d+\.\d+):) {
 	    $APACHE24 = version->parse($1) >= version->parse('2.4.00');
 	}
