2146973b957dcd4f4d2a147d0cc4dfb268ae51a7
Geoff Goehle <goehle@gmail.com>
2015-04-15 14:36:47 -0400

    Keep past answers from improperly encoding utf8

--- lib/WeBWorK/ContentGenerator/Instructor/ShowAnswers.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/ShowAnswers.pm
@@ -29,7 +29,7 @@ use warnings;
 #use CGI;
 use WeBWorK::CGI;
 use WeBWorK::Utils qw(sortByName ); 
-use HTML::Entities;
+use PGcore;
 
 sub initialize {
 	my $self       = shift;
@@ -298,11 +298,11 @@ sub body {
 			    if ($answer eq '') {		    
 				$answerstring  = CGI::small(CGI::i("empty")) if ($answer eq "");
 			    } elsif (!$renderAnswers) {
-				$answerstring = HTML::Entities::encode_entities($answer);
+				$answerstring = PGcore::encode_pg_and_html($answer);
 			    } elsif ($answerType eq 'Value (Formula)') {
 				# We need to escape some gateway strings which 
 				# might appear here
-				$answerstring = HTML::Entities::encode_entities($answer);
+				$answerstring = PGcore::encode_pg_and_html($answer);
 				if ($answerstring =~ /\[(submit|preview|newPage)\]/) {
 				    if ($answerstring !~ /No answer entered/) {
 					$answerstring =~ s/\[(submit|preview|newPage)\](.*)/\[$1\] `$2`/; 
@@ -312,17 +312,17 @@ sub body {
 				}
 				$td->{class} = 'formula';
 			    } elsif ($answerType eq 'essay') {
-				$answerstring = HTML::Entities::encode_entities($answer);
+				$answerstring = PGcore::encode_pg_and_html($answer);
 				$td->{class} = 'essay';
 			    } else {
-				$answerstring = HTML::Entities::encode_entities($answer);
+				$answerstring = PGcore::encode_pg_and_html($answer);
 			    }
 			    
 			    push(@row,CGI::td({width=>20}),CGI::td($td,$answerstring));
 			}
 			
 			if ($pastAnswer->comment_string) {
-			    push(@row,CGI::td({width=>20}),CGI::td({class=>'comment'},"Comment: ".HTML::Entities::encode_entities($pastAnswer->comment_string)));
+			    push(@row,CGI::td({width=>20}),CGI::td({class=>'comment'},"Comment: ".PGcore::encode_pg_and_html($pastAnswer->comment_string)));
 			}
 			
 			print CGI::Tr($rowOptions,@row);
