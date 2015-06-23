8465386bb9cf41aa9b1a6165a33a3be6c55b9128
Geoff Goehle <goehle@gmail.com>
2015-06-23 15:10:50 -0400

    Added some basic files for sticky problems.

--- lib/WebworkClient.pm
+++ lib/WebworkClient.pm
@@ -641,6 +641,64 @@ $self->{outputformats}->{simple}= <<ENDPROBLEMTEMPLATE;
 
 ENDPROBLEMTEMPLATE
 
+
+$self->{outputformats}->{sticky}= <<ENDPROBLEMTEMPLATE;
+
+<html>
+<head>
+<link rel="shortcut icon" href="/webwork2_files/images/favicon.ico"/>
+
+<!-- CSS Loads -->
+<link rel="stylesheet" type="text/css" href="/webwork2_files/js/vendor/bootstrap/css/bootstrap.css"/>
+<link href="/webwork2_files/js/vendor/bootstrap/css/bootstrap-responsive.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="/webwork2_files/css/vendor/font-awesome/css/font-awesome.min.css"/>
+<link rel="stylesheet" type="text/css" href="/webwork2_files/themes/math4/math4.css"/>
+<link href="/webwork2_files/css/knowlstyle.css" rel="stylesheet" type="text/css" />
+
+<!-- JS Loads -->
+<script type="text/javascript" src="/webwork2_files/js/vendor/jquery/jquery.js"></script>
+<script type="text/javascript" src="/webwork2_files/mathjax/MathJax.js?config=TeX-MML-AM_HTMLorMML-full"></script>
+<script type="text/javascript" src="/webwork2_files/js/jquery-ui-1.9.0.js"></script>
+<script type="text/javascript" src="/webwork2_files/js/vendor/bootstrap/js/bootstrap.js"></script>
+<script src="/webwork2_files/js/apps/AddOnLoad/addOnLoadEvent.js" type="text/javascript"></script>
+<script src="/webwork2_files/js/legacy/java_init.js" type="tesxt/javascript"></script>
+<script src="/webwork2_files/js/apps/InputColor/color.js" type="text/javascript"></script>
+<script src="/webwork2_files/js/apps/Base64/Base64.js" type="text/javascript"></script>
+<script src="/webwork2_files/mathjax/MathJax.js?config=TeX-MML-AM_HTMLorMML-full" type="text/javascript"></script>
+<script type="textx/javascript" src="/webwork2_files/js/vendor/underscore/underscore.js"></script>
+<script type="text/javascript" src="/webwork2_files/js/legacy/vendor/knowl.js"></script>
+<script src="/webwork2_files/js/apps/Problem/problem.js" type="text/javascript"></script>
+<script type="text/javascript" src="/webwork2_files/themes/math4/math4.js"></script>	
+
+
+<base href="$XML_URL">
+<title>$XML_URL WeBWorK Editor using host: $XML_URL, format: simple</title>
+</head>
+<body>
+			
+<h2> WeBWorK Editor using host: $XML_URL,  format: simple</h2>
+		    $answerTemplate
+		    <form action="$FORM_ACTION_URL" method="post">
+			$problemText
+	       <input type="hidden" name="answersSubmitted" value="1"> 
+	       <input type="hidden" name="sourceFilePath" value = "$sourceFilePath">
+	       <input type="hidden" name="problemSource" value="$encodedSource"> 
+	       <input type="hidden" name="problemSeed" value="1234"> 
+	       <input type="hidden" name="pathToProblemFile" value="$fileName">
+	       <input type="hidden" name=courseName value="$courseID">
+	       <input type="hidden" name=courseID value="$courseID">
+	       <input type="hidden" name="userID" value="$userID">
+	       <input type="hidden" name="password" value="$password">
+	       <input type="hidden" name="passwd" value="$password">
+	       <input type="hidden" name="session_key" value="$session_key">
+	       <input type="hidden" name="outputformat" value="simple">
+	       <p><input type="submit" name="submit" value="ubmit answers"></p>
+	     </form>
+</body>
+</html>
+
+ENDPROBLEMTEMPLATE
+
 $self->{outputformats}->{debug}= 
 qq{
 
