7c9dc92cd79ab4050f1122e75f7e0d303def18b0
Geoff Goehle <goehle@gmail.com>
2015-06-23 17:22:00 -0400

    Created a "sticky" format which saves previous answers locally so that you can come back to them later.

--- htdocs/js/apps/LocalStorage/localstorage.js
+++ htdocs/js/apps/LocalStorage/localstorage.js
@@ -1,1 +1,51 @@
-adsf
+var WWLocalStorage = function(givenContainer) {
+
+    var container;
+
+    if (givenContainer) {
+	container = givenContainer;
+    } else {
+	container = $('#problemMainForm');
+    }
+
+    var identifier = $("input[name='sourceFilePath']").val()+
+	$("input[name='problemSeed']").val();
+
+    var storedData = $.jStorage.get(identifier);
+
+    if (!storedData) {
+	storedData = {};
+    }
+
+    var storeData = function () {
+	//event.preventDefault();
+
+	if (!storedData['inputs']) {
+	    storedData['inputs'] = {};
+	}
+	
+	var inputs = $(container).find(":input")
+	    .each(function(index,input) {
+		if (!/previous_/.test($(input).attr('name'))) {
+		
+		    storedData['inputs'][$(input).attr('name')] = $(input).val();
+		}
+	    });
+	$.jStorage.set(identifier,storedData);
+    }
+
+    $(container).find(":submit").click(storeData);
+
+    if (storedData) {
+	if (storedData['inputs']) {
+	    var keys = Object.keys(storedData['inputs']);
+	    
+	    keys.forEach(function(key) {
+	    $(container).find('[name="'+key+'"]').val(storedData['inputs'][key]);
+	    });	    
+	}
+    }
+
+}
+
+    
--- lib/WebworkClient.pm
+++ lib/WebworkClient.pm
@@ -724,10 +724,10 @@ $self->{outputformats}->{simple}= <<ENDPROBLEMTEMPLATE;
 
 ENDPROBLEMTEMPLATE
 
-
 $self->{outputformats}->{sticky}= <<ENDPROBLEMTEMPLATE;
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
 <link rel="shortcut icon" href="/webwork2_files/images/favicon.ico"/>
 
@@ -743,6 +743,8 @@ $self->{outputformats}->{sticky}= <<ENDPROBLEMTEMPLATE;
 <script type="text/javascript" src="/webwork2_files/mathjax/MathJax.js?config=TeX-MML-AM_HTMLorMML-full"></script>
 <script type="text/javascript" src="/webwork2_files/js/jquery-ui-1.9.0.js"></script>
 <script type="text/javascript" src="/webwork2_files/js/vendor/bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="/webwork2_files/js/vendor/jquery/modules/jquery.json.min.js"></script>
+<script type="text/javascript" src="/webwork2_files/js/vendor/jquery/modules/jstorage.js"></script>
 <script src="/webwork2_files/js/apps/AddOnLoad/addOnLoadEvent.js" type="text/javascript"></script>
 <script src="/webwork2_files/js/legacy/java_init.js" type="tesxt/javascript"></script>
 <script src="/webwork2_files/js/apps/InputColor/color.js" type="text/javascript"></script>
@@ -750,33 +752,46 @@ $self->{outputformats}->{sticky}= <<ENDPROBLEMTEMPLATE;
 <script src="/webwork2_files/mathjax/MathJax.js?config=TeX-MML-AM_HTMLorMML-full" type="text/javascript"></script>
 <script type="textx/javascript" src="/webwork2_files/js/vendor/underscore/underscore.js"></script>
 <script type="text/javascript" src="/webwork2_files/js/legacy/vendor/knowl.js"></script>
+<script src="/webwork2_files/js/apps/LocalStorage/localstorage.js" type="text/javascript"></script>
 <script src="/webwork2_files/js/apps/Problem/problem.js" type="text/javascript"></script>
 <script type="text/javascript" src="/webwork2_files/themes/math4/math4.js"></script>	
 
-
 <base href="$XML_URL">
-<title>$XML_URL WeBWorK Editor using host: $XML_URL, format: simple</title>
+<title>$XML_URL WeBWorK Editor using host: $XML_URL, format: sticky seed: $problemSeed</title>
 </head>
 <body>
-			
-<h2> WeBWorK Editor using host: $XML_URL,  format: simple</h2>
-		    $answerTemplate
-		    <form action="$FORM_ACTION_URL" method="post">
-			$problemText
-	       <input type="hidden" name="answersSubmitted" value="1"> 
-	       <input type="hidden" name="sourceFilePath" value = "$sourceFilePath">
-	       <input type="hidden" name="problemSource" value="$encodedSource"> 
-	       <input type="hidden" name="problemSeed" value="1234"> 
-	       <input type="hidden" name="pathToProblemFile" value="$fileName">
-	       <input type="hidden" name=courseName value="$courseID">
-	       <input type="hidden" name=courseID value="$courseID">
-	       <input type="hidden" name="userID" value="$userID">
-	       <input type="hidden" name="password" value="$password">
-	       <input type="hidden" name="passwd" value="$password">
-	       <input type="hidden" name="session_key" value="$session_key">
-	       <input type="hidden" name="outputformat" value="simple">
-	       <p><input type="submit" name="submit" value="ubmit answers"></p>
-	     </form>
+<div class="container-fluid">
+<div class="row-fluid">
+<div class="span12 problem">			
+$answerTemplate
+<form id="problemMainForm" class="problem-main-form" name="problemMainForm" action="$FORM_ACTION_URL" method="post">
+<div class="problem-content">
+$problemText
+</div>
+<input type="hidden" name="answersSubmitted" value="1"> 
+<input type="hidden" name="sourceFilePath" value = "$sourceFilePath">
+<input type="hidden" name="problemSource" value="$encodedSource"> 
+<input type="hidden" name="problemSeed" value="$problemSeed"> 
+<input type="hidden" name="pathToProblemFile" value="$fileName">
+<input type="hidden" name=courseName value="$courseID">
+<input type="hidden" name=courseID value="$courseID">
+<input type="hidden" name="userID" value="$userID">
+<input type="hidden" name="password" value="$password">
+<input type="hidden" name="passwd" value="$password">
+<input type="hidden" name="displayMode" value="$displayMode">
+<input type="hidden" name="session_key" value="$session_key">
+<input type="hidden" name="outputformat" value="sticky">
+<p>
+<input type="submit" name="preview"  value="Preview" /> 
+<input type="submit" name="WWsubmit" value="Submit answer"/> 
+<input type="submit" name="WWgrade" value="Show correct answer"/>
+</p>
+</form>
+</div>
+</div>
+</div>
+<!-- Activate local storage js -->
+<script type="text/javascript">WWLocalStorage();</script>
 </body>
 </html>
 
