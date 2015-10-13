764f5973c31fb0366a215705579b336e24fec259
Michael Gage <gage@math.rochester.edu>
2015-10-12 21:02:07 -0400

    Change the way the directory is specified so that the directory path will always be from the root of the one entered.
    This helps in allowing the templatesDirectory to be defined correctly.

--- clients/check_problems_in_dir.sh
+++ clients/check_problems_in_dir.sh
@@ -1,10 +1,10 @@
 #usage 
 #   check_problems_in_dir.s  directory_name
-cd $1 
+#cd $1 
 echo "Checking problems in directory $1"
 echo "Results sent to" 
 echo "webwork2/DATA/bad_problems.txt";
-find . -name "*.pg" -exec /Volumes/WW_test/opt/local/bin/perl $WEBWORK_ROOT/clients/checkProblem.pl {} ';' &
+find $1 -name "*.pg" -exec /Volumes/WW_test/opt/local/bin/perl $WEBWORK_ROOT/clients/checkProblem.pl {} ';' &
 tail -f /Volumes/WW_test/opt/webwork/webwork2/DATA/bad_problems.txt
 
 
