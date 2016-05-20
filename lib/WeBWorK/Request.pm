c1daeea56a802d764b806034503e6bd705d0ed4f
Geoff Goehle <goehle@gmail.com>
2016-05-20 11:49:40 -0400

    Remove xXx debug

--- lib/WeBWorK/Request.pm
+++ lib/WeBWorK/Request.pm
@@ -191,7 +191,7 @@ sub maketext {
 	# $self->{language_handle}->maketext(@_);
 	&{ $self->{language_handle} }(@_);
 	# uncomment to check that your strings are run through maketext
-	return 'xXx'.&{ $self->{language_handle} }(@_).'xXx';
+	# return 'xXx'.&{ $self->{language_handle} }(@_).'xXx';
 }
 
 =item location()
