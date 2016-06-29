2fba808ffd904f23a884d4f881a5e0c9d61614ef
Aori Nevo <aori.nevo@bloomingdales.com>
2016-06-28 20:15:28 -0400

    Added custom categories

--- htdocs/js/apps/GradeBook/GradeBook.js
+++ htdocs/js/apps/GradeBook/GradeBook.js
@@ -5,8 +5,24 @@
 
 $(document).ready(function(){
 
-	$( '.delete-student' ).on('click', function(){
-			console.log('delete-student');		
+	$( '.delete-student' ).on('click', function(e){
+			e.preventDefault();
+			var deleteUrl = e.currentTarget.href,
+				studentName = $(e.currentTarget).parent().parent().parent().find('div').html()
+			$('#confirm-delete-modal .modal-header').html('<h3>Delete Student</h3>');
+			$('#confirm-delete-modal .modal-body').html('You are about to delete '+studentName+'.  Please confirm to permanently remove this student from the course.');			
+			$('#confirm-delete-button').attr("href",deleteUrl);
+			$('#confirm-delete-modal').modal();
+	});
+
+	$( '.delete-assignment' ).on('click', function(e){
+			e.preventDefault();			
+			var deleteUrl = e.currentTarget.href,
+				setName = $(e.currentTarget).parent().parent().parent().find('div').html()
+			$('#confirm-delete-modal .modal-header').html('<h3>Delete Assignment</h3>');
+			$('#confirm-delete-modal .modal-body').html('You are about to delete '+setName+'.  Please confirm to permanently remove this assignment from the course.');
+			$('#confirm-delete-button').attr("href",deleteUrl);
+			$('#confirm-delete-modal').modal();
 	});
 
 });
