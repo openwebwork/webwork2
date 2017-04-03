c9b533f8bd1bd6facb6456129349f8c3ea6aafa9
Alexander <barnhart@duq.edu>
2017-04-02 21:43:54 -0400

    updates FindRand.js to the repository and moves it to a new place.

---
+++ htdocs/duq/js/FindRand.js
@@ -0,0 +1,66 @@
+/*
+ * @author Alexander Barnhart
+ */
+function splitAndInsertModified(pgString, beginIndex, endIndex, hintStr){
+	//original author: Sean McShane 
+	//modified by: Alexander Barnhart	
+	var beginText = pgString.substr(0, beginIndex);
+	var endText = pgString.substr(endIndex, pgString.length);
+
+	pgString = beginText + hintStr + endText;
+	return pgString;
+}
+
+function findRand(PGString)
+{
+	/* this function will take a pg file in string form and search within
+	 * it for all random tags([rand]...[/rand]). the program will put these
+	 * into an array-of-arrays called randTags[]. the format will be:
+	 * randTags[n] = [startIndex, endIndex, [string1, string2, ... , lastString] ];
+	 * this is with the assumption that the tagged string will be in the format:
+	 * string1,string2,string3,etc.
+	 */
+	//the pg file in string form
+	//the array to return.
+	var randTags = [];
+	
+	var startIndex = 0;	//used for conditional.
+	var index = 0;		//actual pointer to area in string. used for navigation.
+	
+	var beginString = 0;	//begin substring.
+	var endString = 0;	//end substring.
+
+	/* count all the occurences of [rand]...[/rand].
+	 * this while loop's structure was adapted from code found on: 
+	 * http://stackoverflow.com/questions/16897772/looping-through-string-to-find-multiple-indexes
+	*/	
+	 
+	while((index = PGString.indexOf("[rand]", startIndex)) > -1) {
+		beginString = PGString.indexOf("[rand]", startIndex) + 6;
+		endString = PGString.indexOf("[/rand]",beginString);
+		randTags.push(new Array(beginString, endString, PGString.substring(beginString, endString).split(",")));
+		startIndex = endString;
+	}
+	var recievedArr = [];
+	
+	//calls another function on each element of the randTags array
+		
+	for(var i = 0; i < randTags.length; i++)	
+	{
+		expectedArr.push(jamesfunction(randTags[i]));
+	}
+	
+	var n = 0;
+	startIndex = 0;
+	while((index = PGString.indexOf("[rand]", startIndex)) > -1) {
+		beginString = PGString.indexOf("[rand]", startIndex);
+		endString = PGString.indexOf("[/rand]", beginString) + 7;
+		
+		var newstring = splitAndInsertModified(PGString, beginString, endString, expectedArr[n]);
+		PGString = newstring;
+		n++;
+		startIndex = 0;
+	}
+	return PGString;
+	
+}
