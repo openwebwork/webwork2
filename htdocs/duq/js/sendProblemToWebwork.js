187524470c4d8a448e7c808ed7e032de9ae53f95
Tyler Gaona <gaonat@duq.edu>
2017-04-18 09:23:50 -0400

    Added functionality for going back to webwork editor on clicking the submit button in fillinblankperl.html

--- htdocs/duq/js/sendProblemToWebwork.js
+++ htdocs/duq/js/sendProblemToWebwork.js
@@ -1,5 +1,12 @@
-document.getElementById("submitButton").onclick = function() {
-    var pgString = generateTrueFalse();
+var submitButton = document.getElementById("submitButton");
+submitButton.onclick = function() {
+    var pgString;
+    if (submitButton.classList.contains("truefalse")) {
+	pgString = generateTrueFalse();
+    }
+    else if (submitButton.classList.contains("fillinblanks")) {
+	pgString = generateFillInBlanks();
+    }
     // Remove line breaks.
     pgString = pgString.replace(/<br>/g, '');
     var paramMap = getURLParams(window.location.href);
@@ -99,5 +106,103 @@ ENDDOCUMENT();";
 
     var output = document.getElementById("outputCode");
     output.innerHTML = "All the Perl Code is : "  + pgString;
+    return pgString;
+}
+
+function generateFillInBlanks()
+{
+	addKnowlHelper();
+	var question = document.getElementById("knowlOutput").innerHTML;
+	var answer = document.getElementById("answer").value;
+	var solution = document.getElementById("solution").value;
+
+	var section1 = '<br><br>DOCUMENT();\n<br>\
+	\n<br>\
+	loadMacros(\n<br>\
+	"PGstandard.pl",\n<br>\
+	"MathObjects.pl",\n<br>\
+	);\n<br>\
+	\n<br>\
+	TEXT(beginproblem());\n<br>\
+	\n<br>\
+    	Context()->strings->add("';
+    
+   	var section2 = '"=>{});\n<br>';
+	//$answer = String("';
+	var section2_String = '$answer = String("';
+	var section2_Real ='$answer = Real("';
+
+
+	var section3 ='");\n<br>\
+	\n<br>\
+	Context()->texStrings;\n<br>\
+	BEGIN_TEXT\n<br>';
+	
+	var section4 = "<br>$BR\n<br>\
+	$BR\n<br>\
+	\\{ ans_rule(20) \\}\n<br>\
+	END_TEXT\n<br>\
+	Context()->normalStrings;\n<br>\
+	\n<br>\
+	$showPartialCorrectAnswers = 1;\n<br>\
+	\n<br>\
+	ANS( $answer-> cmp() );\n<br>\
+	\n<br>\
+	Context()->texStrings;\n<br>\
+	SOLUTION(EV3(<<'END_SOLUTION'));\n<br>\
+	$PAR SOLUTION $PAR\n<br>";
+	
+	var section5 = "\n<br>END_SOLUTION\n<br>\
+	Context()->normalStrings;\n<br>\
+	\n<br>\
+	ENDDOCUMENT();";
+
+
+	var pgString = section1 + answer + section2 + section2_String + answer + section3 + question + section4 + answer + solution + section5;
+//Quan from Team Brandon: 
+//I think there is a bug in your code in the line above so I fixed it but not sure. 
+//Check it please
+
+	/*Tolerance requires a numeric input.  Currently, the code is always a 		 *string, which is problematic.  Therefore, as long as there is
+
+	 */
+
+	if(usingPGML(pgString)){}
+
+	else{	
+		if(getSelectedType() == "none"){
+			pgString = section1 + answer + section2 +
+				section2_String + answer + section3 +
+				question + section4 + solution + section5;
+		}	
+		else{ 
+			pgString = section1 + answer + section2 + section2_Real + answer + section3 + 					question + section4 + solution + section5;
+		}
+	}
+	
+	//Insert hint/image PG code, if there is a hint or image
+        if(document.getElementById("hintText").value != "" || 
+                document.getElementById("imageHintText").value != ""
+                        || document.getElementById("imageText").value != ""){
+                        pgString = translateToPG(pgString,
+                                document.getElementById("hintText").value, 
+                                        [document.getElementById("imageHintText").value, document.getElementById("imageHintWidth").value, document.getElementById("imageHintHeight").value], 
+                                                [document.getElementById("imageText").value, document.getElementById("imageWidth").value, document.getElementById("imageHeight").value]);
+        }
+
+	if(getSelectedType() != "none" && 
+		document.getElementById("toleranceText").value != ""){
+
+		pgString = checkPGorPGML(pgString, 
+			document.getElementById("toleranceText").value,
+			getSelectedType());
+	}
+
+    //var output = document.getElementById("codeOutput"); 
+    //output.innerHTML = "All the Perl code is : <span id='code'>"  + pgString + "</span>";
+	
+	// Save the state and make the updates in WebWorK
+    saveState();
+
     return pgString;
 }
