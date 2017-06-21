c3f39205ba847735916bf993bbf5ffea5ba9a7d4
Michael Gage <gage@math.rochester.edu>
2017-06-21 14:20:37 -0400

    Fix typo in message.

--- lib/WeBWorK/ContentGenerator/Instructor/Scoring.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/Scoring.pm
@@ -122,7 +122,7 @@ sub initialize {
 		if (!$scoringFileNameOK) { # fileName is not properly formed
 			$self->addbadmessage($r->maketext("Your file name is not valid! ")).
 		    $self->addbadmessage($r->maketext("A file name cannot begin with a dot, it cannot be empty, it cannot contain a " .
-				 "directory path component and only the characters ~[^-_.a-zA-Z0-9 ~]  are allowed.")
+				 "directory path component and only the characters ~[-_.a-zA-Z0-9 ~]  are allowed.")
 			); # ~ is needed to escape [ and ] within maketext
 		}
 	}
