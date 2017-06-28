9f0d4ff352d30b586acfff909c449d78d2916051
Andrew Gardener <andrew.e.gardener@gmail.com>
2017-06-28 12:48:07 -0700

    Fix docker setup
    addcourse causes error if conf files are missing (happens on fresh install)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,18 +7,6 @@ if [ "${1:0:1}" = '-'  ]; then
 fi
 
 if [ "$1" = 'apache2' ]; then
-    # create admin course if not existing
-    if [ ! -d "$APP_ROOT/courses/admin"  ]; then
-        # wait for db to start up
-        echo "Waiting for database to start..."
-        while ! timeout 1 bash -c "(cat < /dev/null > /dev/tcp/$WEBWORK_DB_HOST/$WEBWORK_DB_PORT) >/dev/null 2>&1"; do sleep 0.5; done
-        newgrp www-data
-        umask 2
-        cd $APP_ROOT/courses
-        WEBWORK_ROOT=$APP_ROOT/webwork2 $APP_ROOT/webwork2/bin/addcourse admin --db-layout=sql_single --users=$APP_ROOT/webwork2/courses.dist/adminClasslist.lst --professors=admin
-        chown root:www-data -R $APP_ROOT/courses/admin
-        echo "Admin course is created."
-    fi
     # generate conf files if not exist
     for i in site.conf localOverrides.conf; do
         if [ ! -f $APP_ROOT/webwork2/conf/$i ]; then
@@ -37,6 +25,18 @@ if [ "$1" = 'apache2' ]; then
             fi
         fi
     done
+    # create admin course if not existing
+    if [ ! -d "$APP_ROOT/courses/admin"  ]; then
+        # wait for db to start up
+        echo "Waiting for database to start..."
+        while ! timeout 1 bash -c "(cat < /dev/null > /dev/tcp/$WEBWORK_DB_HOST/$WEBWORK_DB_PORT) >/dev/null 2>&1"; do sleep 0.5; done
+        newgrp www-data
+        umask 2
+        cd $APP_ROOT/courses
+        WEBWORK_ROOT=$APP_ROOT/webwork2 $APP_ROOT/webwork2/bin/addcourse admin --db-layout=sql_single --users=$APP_ROOT/webwork2/courses.dist/adminClasslist.lst --professors=admin
+        chown root:www-data -R $APP_ROOT/courses/admin
+        echo "Admin course is created."
+    fi
     # generate apache2 reload config if needed
     if [ $DEV -eq 1 ]; then
         echo "PerlModule Apache2::Reload" > /etc/apache2/conf-enabled/apache2-reload.conf
