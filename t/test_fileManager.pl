74c8827a39cdc1bb1548b3f9ab259941e22345df
Derek Steinmoeller <Derek.Steinmoeller@D2L.com>
2017-11-06 08:55:11 -0500

    Tests weren't working because aliasing the namespace didn't work as intended. Fully qualifying the sub calls instead. Now everything is fine.

--- t/test_fileManager.pl
+++ t/test_fileManager.pl
@@ -1,28 +1,25 @@
+#!/Volumes/WW_test/opt/local/bin/perl -w
 use 5.010;
 use warnings;
 
 BEGIN {
-	require "./grab_course_environment.pl";
-	eval "use lib '$WebworkBase::RootPGDir/lib'"; die $@ if $@;
-	eval "use lib '$WebworkBase::RootWebwork2Dir/lib'"; die $@ if $@;
+        require "./grab_course_environment.pl";
+        eval "use lib '$WebworkBase::RootPGDir/lib'"; die $@ if $@;
+        eval "use lib '$WebworkBase::RootWebwork2Dir/lib'"; die $@ if $@;
 }
 
-
 use Test::More tests => 12;
+use WeBWorK::ContentGenerator::Instructor::FileManager qw/isText/;
 
-use WeBWorK::ContentGenerator::Instructor::FileManager;
-
-my $fm = WeBWorK::ContentGenerator::Instructor::FileManager::;
-
-ok $fm->isText("abcdghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), 'ordinary string is text';
-ok $fm->isText("abc\ndef"), 'string with newline is text';
-ok $fm->isText("abc\r\ndef"), 'string with CRLF (windows style) is text';
-ok !$fm->isText("abcdef\x01\x02abcdef"), 'string with 2 consecutive control characters is not text';
-ok !$fm->isText("\x01\x1F\x7FHD08U23NV\x00\x08"), 'string with multiple blocks of 2+ control characters is not text';
-ok $fm->isText("abcdef\x0Aasdfd"), 'text with 1 control character is still considered text';
-ok $fm->isText("\x00abcdef\x1Fghijklm"), 'text with multiple instances of single isolated control characters is still considered text';
-ok $fm->isText("abc\n\ndef\n\n\n\nghijklm"), 'text with multiple consecutive newlines is text';
-ok $fm->isText("abc\r\n\r\ndef\r\n\r\n\r\n\r\nghijklm"), 'text with multiple consecutive CRLF\'s (windows style) is text';
-ok $fm->isText("                 "), 'string of all spaces is text';
-ok $fm->isText("      \r\n\t\r\n\t        "), 'string of all spaces, tabs, CRs and newlines is text';
-ok $fm->isText("abcdef\t\t\t\tasdfjka"), 'string with multiple consecutive tabs is text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("abcdghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), 'ordinary string is text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("abc\ndef"), 'string with newline is text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("abc\r\ndef"), 'string with CRLF (windows style) is text';
+ok !WeBWorK::ContentGenerator::Instructor::FileManager::isText("abcdef\x01\x02abcdef"), 'string with 2 consecutive control characters it not text';
+ok !WeBWorK::ContentGenerator::Instructor::FileManager::isText("\x01\x1F\x7FHD08U23NV\x00\x08"), 'string with multiple blocks of 2+ control characters is not text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("abcdef\x0Aasdfd"), 'text with 1 control character is still considered text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("\x00abcdef\x1Fghijklm"), 'text with multiple instances of single isolated control characters is still considered text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("abc\n\ndef\n\n\n\nghijklm"), 'text with multiple consecutive newlines is text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("abc\r\n\r\ndef\r\n\r\n\r\n\r\nghijklm"), 'text with multiple consecutive CRLF\'s (windows style) is text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("                 "), 'string of all spaces is text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("      \r\n\t\r\n\t        "), 'string of all spaces, tabs, CRs and newlines is text';
+ok WeBWorK::ContentGenerator::Instructor::FileManager::isText("abcdef\t\t\t\tasdfjka"), 'string with multiple consecutive tabs is text';
