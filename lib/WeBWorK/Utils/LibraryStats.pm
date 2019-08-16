f63ed9f96a81e42799a435a45b19bd94cd2a5f8a
Nathan Wallach <taniwallach@gmail.com>
2019-08-16 17:06:30 +0300

    Have OPL-update call load-OPL-global-statistics to load the global statistics data from the OPL after running update-OPL-statistics. Remove the reloading of the global statistics data from update-OPL-statistics but print a warning that it no longer does that. Also fix the warning in lib/WeBWorK/Utils/LibraryStats.pm to inform users to use load-OPL-global-statistics instead of update-OPL-statistics when the global statistics data is missing.

--- lib/WeBWorK/Utils/LibraryStats.pm
+++ lib/WeBWorK/Utils/LibraryStats.pm
@@ -92,7 +92,7 @@ sub getGlobalStats {
 
     unless ($selectstm->execute($source_file)) {
       if ($selectstm->errstr =~ /Table .* doesn't exist/) {
-	warn "Couldn't find the OPL global statistics table.  Did you download the latest OPL and run update-OPL-statistics?"
+	warn "Couldn't find the OPL global statistics table.  Did you download the latest OPL and run load-OPL-global-statistics?"
       }
       die $selectstm->errstr;
     }
