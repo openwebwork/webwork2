c94c9ceaa146c7c5bc69cae9c1c9fcf02efb0941
Michael Gage <1203580+mgage@users.noreply.github.com>
2019-09-26 20:34:06 -0400

    Fix spacing

--- lib/FormatRenderedProblem.pm
+++ lib/FormatRenderedProblem.pm
@@ -261,7 +261,7 @@ sub formatRenderedProblem {
 	my $userID           =  $self->{userID};
 	my $course_password  =  $self->{course_password};
 	my $problemSeed      =  $self->{inputs_ref}->{problemSeed}//4444;
-        my $psvn             =  $self->{inputs_ref}->{psvn}//54321;
+	my $psvn             =  $self->{inputs_ref}->{psvn}//54321;
 	my $session_key      =  $rh_result->{session_key}//'';
 	my $displayMode      =  $self->{inputs_ref}->{displayMode};
 	
