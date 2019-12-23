9cfd6d6ab5c50bdcff8780a7debdfcdee65cf2c7
Michael Gage <1203580+mgage@users.noreply.github.com>
2019-12-23 16:58:38 -0500

    Add 'graphics'   to 'macros', 'email'  and a key words associated with directories that are to be ignored when creating the list of problems inside directories such as Contrib.

--- lib/WeBWorK/ContentGenerator/Instructor/GetLibrarySetProblems.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/GetLibrarySetProblems.pm
@@ -75,7 +75,7 @@ use constant MOVED => (1 << 4);
 my %problib;	## This is configured in defaults.config
 my %ignoredir = (
 	'.' => 1, '..' => 1, 'Library' => 1, 'CVS' => 1, 'tmpEdit' => 1,
-	'headers' => 1, 'macros' => 1, 'email' => 1, '.svn' => 1,
+	'headers' => 1, 'macros' => 1, 'graphics' => 1, 'email' => 1, '.svn' => 1,
 );
 
 sub prepare_activity_entry {
