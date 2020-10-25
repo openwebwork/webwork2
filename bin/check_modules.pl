6e1aef2c099d673a6f56177eef7e52b6c445af39
Nathan Wallach <taniwallach@gmail.com>
2020-10-25 17:45:34 +0200

    Refactor the Cookie code: 1. Use CGI::Cookie instead of Apache2:Cookie, as the new code needs support for the samesite attribute. 2. Added CGI::Cookie to bin/check_modules.pl and Dockerfile.    Note: The support for samesite dates to June 2019 in CGI::Cookie 4.45. 3. Remove obsolete, commented out, code using cookies from    lib/WeBWorK/ContentGenerator/Logout.pm. 4. Drop the constant COOKIE_LIFESPAN and instead allow setting cookie    lifespan using site / course environment configuration variables.    $CookieLifeTime  - for when cookie based session management IS      in use - default to 6 hours.    $CookieLifeTime2 - for when cookie based session management is NOT      in use, defaults to 30 days. 5. Allow setting value of cookies samesite and secure attribute using    site / course environment configuration variable:      $CookieSameSite      $CookieSecure

--- bin/check_modules.pl
+++ bin/check_modules.pl
@@ -32,7 +32,6 @@ my @apache1ModulesList = qw(
 
 my @apache2ModulesList = qw(
 	Apache2::Request
-	Apache2::Cookie
 	Apache2::ServerRec
 	Apache2::ServerUtil
 );
@@ -45,6 +44,7 @@ my @modulesList = qw(
 	Benchmark
 	Carp
 	CGI
+	CGI:Cookie
 	Class::Accessor
 	Dancer
 	Dancer::Plugin::Database
