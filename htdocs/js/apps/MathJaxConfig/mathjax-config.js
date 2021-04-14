e4864cfc7c239f880a47f980b4d8c5da3dfb54f3
Glenn Rice <grice1@missouriwestern.edu>
2021-04-14 17:44:28 -0500

    Wrap answer previews in `<script type="math/tex"></script>` tags in the attempts table.  Add the appropriate MathJax configuration to handle this to the mathjax-config.js file.

--- htdocs/js/apps/MathJaxConfig/mathjax-config.js
+++ htdocs/js/apps/MathJaxConfig/mathjax-config.js
@@ -14,6 +14,22 @@ if (!window.MathJax) {
 				}
 				return MathJax.startup.defaultReady()
 			}
+		},
+		options: {
+			renderActions: {
+				findScript: [10, function (doc) {
+					document.querySelectorAll('script[type^="math/tex"]').forEach(function(node) {
+						var display = !!node.type.match(/; *mode=display/);
+						var math = new doc.options.MathItem(node.textContent, doc.inputJax[0], display);
+						var text = document.createTextNode('');
+						node.parentNode.replaceChild(text, node);
+						math.start = {node: text, delim: '', n: 0};
+						math.end = {node: text, delim: '', n: 0};
+						doc.math.push(math);
+					});
+				}, '']
+			}
 		}
+
 	};
 }
