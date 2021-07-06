fd87dbf8f723322199f836f9cc4c0147677496bc
Michael Gage <gage@math.rochester.edu>
2021-07-06 19:58:59 -0400

    turn showLibraryLocalStats off, remove extraneous repeated line

--- docker-config/docker-entrypoint.sh
+++ docker-config/docker-entrypoint.sh
@@ -79,7 +79,6 @@ if [ "$1" = 'apache2' ]; then
                     -e 's/^\$database_username ="webworkWrite"/$database_username =$ENV{"WEBWORK_DB_USER"}/' \
                     -e 's/^\$database_password ='\''passwordRW'\''/$database_password =$ENV{"WEBWORK_DB_PASSWORD"}/' \
                     -e 's/mail{smtpServer} = '\'''\''/mail{smtpServer} = $ENV{"WEBWORK_SMTP_SERVER"}/' \
-                    -e 's/mail{smtpSender} = '\'''\''/mail{smtpSender} = $ENV{"WEBWORK_SMTP_SENDER"}/' \
                     -e 's/siteDefaults{timezone} = "America\/New_York"/siteDefaults{timezone} = $ENV{"WEBWORK_TIMEZONE"}/' \
                     -e 's/^# $server_userID     = '\''www-data/$server_userID     = '\''www-data/'  \
                     -e 's/^# $server_groupID    = '\''www-data/$server_groupID    = '\''www-data/' $WEBWORK_ROOT/conf/site.conf
@@ -88,7 +87,8 @@ if [ "$1" = 'apache2' ]; then
             fi
 
             if [ $i == 'localOverrides.conf' ]; then
-                sed -i -e 's/#$pg{specialPGEnvironmentVars}{Rserve} = {host => "r"};/$pg{specialPGEnvironmentVars}{Rserve} = {host => "r"};/' $WEBWORK_ROOT/conf/localOverrides.conf
+                sed -i -e 's/#$pg{specialPGEnvironmentVars}{Rserve} = {host => "r"};/$pg{specialPGEnvironmentVars}{Rserve} = {host => "r"};/' \
+                       -e 's/$problemLibrary{showLibraryLocalStats} = 1;/$problemLibrary{showLibraryLocalStats} = 0;/' $WEBWORK_ROOT/conf/localOverrides.conf
                 echo "$WEBWORK_ROOT/conf/$i has been modified."
             fi
         fi
