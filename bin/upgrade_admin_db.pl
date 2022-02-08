6f9684e5dabd972c6beb4caeb87821beeff2dfc4
Glenn Rice <grice1@missouriwestern.edu>
2022-02-08 08:28:03 -0600

    Fix the importClassList.pl and upgrade_admin_db.pl scripts for the move of WWSafe.pm to the pg side.

--- bin/upgrade_admin_db.pl
+++ bin/upgrade_admin_db.pl
@@ -4,31 +4,29 @@
 # WeBWorK Online Homework Delivery System
 # Copyright &copy; 2000-2018 The WeBWorK Project, http://openwebwork.sf.net/
 # $CVSHeader: webwork2/bin/wwdb,v 1.13 2006/01/25 23:13:45 sh002i Exp $
-# 
+#
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of either: (a) the GNU General Public License as published by the
 # Free Software Foundation; either version 2, or (at your option) any later
 # version, or (b) the "Artistic License" which comes with this package.
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
 # Artistic License for more details.
 ################################################################################
 
-BEGIN{ die('You need to set the WEBWORK_ROOT environment variable.\n') 
-	   unless($ENV{WEBWORK_ROOT});}
+my $pg_dir;
+BEGIN {
+	die('You need to set the WEBWORK_ROOT environment variable.\n')
+		unless ($ENV{WEBWORK_ROOT});
+	$pg_dir = $ENV{PG_ROOT} // "$ENV{WEBWORK_ROOT}/../pg";
+	die "The pg directory must be defined in PG_ROOT" unless (-e $pg_dir);
+}
 
 use lib "$ENV{WEBWORK_ROOT}/lib";
+use lib "$pg_dir/lib";
 use WeBWorK::CourseEnvironment;
-
-BEGIN {
-    my $ce = new WeBWorK::CourseEnvironment({
-	webwork_dir => $ENV{WEBWORK_ROOT},
-					    });
-    my $pg_dir = $ce->{pg_dir};
-    eval "use lib '$pg_dir/lib'";
-}
 
 use WeBWorK::DB;
 use WeBWorK::Utils::CourseIntegrityCheck;
@@ -38,17 +36,17 @@ use WeBWorK::Utils::CourseIntegrityCheck;
 ##########################
 my $upgrade_courseID = 'admin';
 
-my $ce2 = new WeBWorK::CourseEnvironment({
-    webwork_dir => $ENV{WEBWORK_ROOT},
-    courseName => $upgrade_courseID,
-					 });
-#warn "do_upgrade_course: updating |$upgrade_courseID| from" , join("|",@upgrade_courseIDs); 
+my $ce = new WeBWorK::CourseEnvironment({
+	webwork_dir => $ENV{WEBWORK_ROOT},
+	courseName  => $upgrade_courseID,
+});
+#warn "do_upgrade_course: updating |$upgrade_courseID| from" , join("|",@upgrade_courseIDs);
 #############################################################################
 # Create integrity checker
 #############################################################################
 
 my $update_error_msg = '';
-my $CIchecker = new WeBWorK::Utils::CourseIntegrityCheck(ce=>$ce2);
+my $CIchecker = new WeBWorK::Utils::CourseIntegrityCheck(ce => $ce);
 
 #############################################################################
 # Add missing tables and missing fields to existing tables
@@ -56,7 +54,7 @@ my $CIchecker = new WeBWorK::Utils::CourseIntegrityCheck(ce=>$ce2);
 
 my ($tables_ok,$dbStatus) = $CIchecker->checkCourseTables($upgrade_courseID);
 my @schema_table_names = keys %$dbStatus;  # update tables missing from database;
-my @tables_to_create = grep {$dbStatus->{$_}->[0] == WeBWorK::Utils::CourseIntegrityCheck::ONLY_IN_A()} @schema_table_names;	
+my @tables_to_create = grep {$dbStatus->{$_}->[0] == WeBWorK::Utils::CourseIntegrityCheck::ONLY_IN_A()} @schema_table_names;
 my @tables_to_alter  = grep {$dbStatus->{$_}->[0] == WeBWorK::Utils::CourseIntegrityCheck::DIFFER_IN_A_AND_B()} @schema_table_names;
 $update_error_msg = $CIchecker->updateCourseTables($upgrade_courseID, [@tables_to_create]);
 foreach my $table_name (@tables_to_alter) {	#warn "do_upgrade_course: adding new fields to table $table_name in course $upgrade_courseID";
