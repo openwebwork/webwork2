9ad7ce3dd5b64edd3aa484d06cb3b8e7275b6571
Glenn Rice <grice1@missouriwestern.edu>
2022-04-03 18:20:41 -0500

    Adjust the times used by flatpickr to account for the difference between the browser's timezone and the course timezone.

--- htdocs/js/apps/ProblemSetList/problemsetlist.js
+++ htdocs/js/apps/ProblemSetList/problemsetlist.js
@@ -44,6 +44,16 @@
 	// Initialize the date/time picker for the import form.
 	const importDateShift = document.getElementById('import_date_shift');
 	if (importDateShift) {
+		// Compute the time difference between the current browser timezone and the the course timezone.
+		// flatpickr gives the time in the browser's timezone, and this is used to adjust to the course timezone.
+		// Note that this is converted to microseconds.
+		const timezoneAdjustment =
+			parseInt(Intl.DateTimeFormat('en-US', { timeZoneName: 'shortOffset' })
+				.format(new Date).split(' ')[1].slice(3) || '0') * 3600000
+			- parseInt(Intl.DateTimeFormat('en-US',
+				{ timeZone: importDateShift.dataset.timezone ?? 'UTC', timeZoneName: 'shortOffset' })
+				.format(new Date).split(' ')[1].slice(3) || '0') * 3600000
+
 		flatpickr(importDateShift.parentNode, {
 			allowInput: true,
 			enableTime: true,
@@ -72,23 +82,28 @@
 				if (!selectedDates.length) this.input.value = '';
 			},
 			parseDate(datestr, format) {
-				// Deal with the case of a unix timestamp.
-				if (format === 'U') return new Date(parseInt(datestr) * 1000);
-				// Next attempt to parse the datestr with the current format.
+				// Deal with the case of a unix timestamp on initial load.  At this time the timezone needs to be
+				// adjusted backward as flatpickr is going to use the browser's time zone.
+				if (format === 'U') return new Date(parseInt(datestr) * 1000 - timezoneAdjustment);
+				// Next attempt to parse the datestr with the current format.  This should not be adjusted.
 				const date = new Date(Date.parse(datestr, format));
 				if (!isNaN(date.getTime())) return date;
-				// Finally, fall back to the previous value in the original input if that failed.
-				return new Date(parseInt(importDateShift.value) * 1000);
+				// Finally, fall back to the previous value in the original input if that failed.  This also needs
+				// to be adjusted back since the adjusted timestamp is saved in the input.
+				return new Date(parseInt(importDateShift.value) * 1000 - timezoneAdjustment);
 			},
 			formatDate(date) {
+				// flatpickr gives the date in the browser's time zone.  So it needs to be adjusted to the timezone
+				// of the course.
+
 				// Flatpickr sets the value of the original input to the parsed time.
 				// So set that back to the unix timestamp.
-				importDateShift.value = date.getTime() / 1000;
+				importDateShift.value = date.getTime() / 1000 + timezoneAdjustment / 1000;
 
 				// Return the localized time string.
 				return Intl.DateTimeFormat(importDateShift.dataset.locale?.replaceAll(/_/g, '-') ?? 'en',
 					{ dateStyle: 'short', timeStyle: 'short', timeZone: importDateShift.dataset.timezone ?? 'UTC' })
-					.format(date);
+					.format(new Date(date.getTime() + timezoneAdjustment));
 			}
 		});
 	}
