42325dc84b751d50eba401081080def1a119fdc3
K. Andrew Parker <kparker@citytech.cuny.edu>
2022-04-22 15:01:37 -0400

    Eliminate errant space in maketext string

--- lib/WeBWorK/ContentGenerator/Instructor/Scoring.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/Scoring.pm
@@ -117,7 +117,7 @@ sub initialize {
 		if (!$scoringFileNameOK) { # fileName is not properly formed
 			$self->addbadmessage($r->maketext("Your file name is not valid! "));
 		    $self->addbadmessage($r->maketext("A file name cannot begin with a dot, it cannot be empty, it cannot contain a " .
-				 "directory path component and only the characters -_.a-zA-Z0-9 and space  are allowed.")
+				 "directory path component and only the characters -_.a-zA-Z0-9 and space are allowed.")
 			);
 		}
 	}
