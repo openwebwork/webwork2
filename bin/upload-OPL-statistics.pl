766234e3b3b4a48eb8bd052284109dbd65ed296b
K. Andrew Parker <kparker@citytech.cuny.edu>
2022-04-27 15:16:51 -0400

    update OPL statistics upload target

--- bin/upload-OPL-statistics.pl
+++ bin/upload-OPL-statistics.pl
@@ -177,8 +177,10 @@ print "Zipping files\n";
 `tar -czf $tar_file $output_file $desc_file`;
 
 print "Uploading file\n";
+`echo "put $tar_file" | sshpass -p 'wwdata_upload' sftp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null wwdata_upload\@146.111.135.122:wwdata/`;
 
-`echo "put $tar_file" | sftp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oPort=57281 wwdata\@52.88.32.79`;
+print "Cleaning up\n";
+`rm $desc_file $tar_file $output_file`;
 
-
+print "Done\n";
 1;
