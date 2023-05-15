7e590c3af395c8e00e5c7ee0063bc1d3213edf58
Glenn Rice <grice1@missouriwestern.edu>
2023-05-14 20:50:34 -0500

    Remove the message file "Open" button and the merge file "View" button.
    Instead trigger the opening of the message file or merge file by
    javascript immediately when the selects are changed.

--- htdocs/js/apps/SendMail/sendmail.js
+++ htdocs/js/apps/SendMail/sendmail.js
@@ -8,4 +8,20 @@
 			else previewUserNameSpan.textContent = previewUserNameSpan.dataset.default;
 		});
 	}
+
+	for (const select of [
+		['openfilename', 'openMessage'],
+		['merge_file', 'viewMergeFile']
+	]) {
+		document.getElementById(select[0])?.addEventListener('change', () => {
+			const mailForm = document.forms['mail-main-form'];
+			if (!mailForm) return;
+			const submit = document.createElement('input');
+			submit.type = 'submit';
+			submit.name = select[1];
+			submit.style.display = 'none';
+			mailForm.append(submit);
+			submit.click();
+		});
+	}
 })();
