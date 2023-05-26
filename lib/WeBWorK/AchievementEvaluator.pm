07af2ce47d436e18c871e6133534f138d1efbe45
Glenn Rice <grice1@missouriwestern.edu>
2023-05-26 06:43:14 -0500

    Fix the Mojolicious/Safe SIG conflict.

--- lib/WeBWorK/AchievementEvaluator.pm
+++ lib/WeBWorK/AchievementEvaluator.pm
@@ -89,6 +89,12 @@ sub checkForAchievements ($problem_in, $pg, $c, %options) {
 		$dtCourseTime->month, $dtCourseTime->year, $dtCourseTime->day_of_week
 	);
 
+	# Mojolicious sets the INT and TERM signal handlers, and perl Safe overrides those (as those signals can be used to
+	# break out of Safe) which causes an error later when the Mojolicious signals handlers are not called as they should
+	# be.  So the current signal handlers are cached here and restored after the Safe reval is completed.
+	my %ORIG_SIG;
+	$ORIG_SIG{$_} = $SIG{$_} for keys %SIG;
+
 	my $compartment = Safe->new;
 
 	#initialize things that are ""
@@ -259,6 +265,9 @@ sub checkForAchievements ($problem_in, $pg, $c, %options) {
 		$db->putUserAchievement($userAchievement);
 
 	}    #end for loop
+
+	# Restore the original signal handlers.
+	local $SIG{$_} = $ORIG_SIG{$_} for keys %ORIG_SIG;
 
 	#nfreeze_base64 globalData and store
 	$globalUserAchievement->frozen_hash(nfreeze_base64($globalData));
