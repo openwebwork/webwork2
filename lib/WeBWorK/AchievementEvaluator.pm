381bf3f98987878fd60f358a4ea24a41ec827933
K. Andrew Parker <kenneth.parker46@citytech.cuny.edu>
2023-11-27 16:18:54 -0500

    Update lib/WeBWorK/AchievementEvaluator.pm
    Co-authored-by: Glenn Rice <47527406+drgrice1@users.noreply.github.com>

--- lib/WeBWorK/AchievementEvaluator.pm
+++ lib/WeBWorK/AchievementEvaluator.pm
@@ -262,7 +262,7 @@ sub checkForAchievements ($problem_in, $pg, $c, %options) {
 					setID           => $set_id,
 					nextLevelPoints => $nextLevelPoints || 0,
 					pointsEarned    => $achievementPoints,
-					remote_host     => $c->tx->remote_host || "UNKNOWN",
+					remote_host     => $c->tx->remote_address || "UNKNOWN",
 				} ]
 			) if ($achievement->email_template);
 		}
