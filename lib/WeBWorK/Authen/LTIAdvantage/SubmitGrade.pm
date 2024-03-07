ed8f2eaf460d9bbe68142cdcda0fdb17dce1e301
Glenn Rice <grice1@missouriwestern.edu>
2024-03-06 20:43:17 -0600

    Use the client ID for the the iss claim also.
    This switches from using the webwork server url for the iss claim to
    using the client id in the JWT sent when requesting an access token from
    the LMS for grade passback.
    
    This is confirmed to work on Moodle, Canvas, and D2L.

--- lib/WeBWorK/Authen/LTIAdvantage/SubmitGrade.pm
+++ lib/WeBWorK/Authen/LTIAdvantage/SubmitGrade.pm
@@ -138,7 +138,7 @@ async sub get_access_token ($self) {
 		encode_jwt(
 			payload => {
 				aud => $ce->{LTI}{v1p3}{AccessTokenAUD},
-				iss => $c->url_for('root')->to_abs->to_string,
+				iss => $ce->{LTI}{v1p3}{ClientID},
 				sub => $ce->{LTI}{v1p3}{ClientID},
 				jti => $private_key->{kid}
 			},
