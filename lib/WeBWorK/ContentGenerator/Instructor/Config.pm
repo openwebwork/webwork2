ae98d30d095a145f2eb9d71ae7293f2e4a2583ec
Glenn Rice <grice1@missouriwestern.edu>
2024-04-07 05:50:09 -0500

    Make it so that changing tabs on the course configuration page does not need page reload.
    Instead all tabs are present always and are in bootstrap collapses.
    Clicking a different tab just toggles visibility.  I.e., the usual nav
    tabs setup.
    
    This means that changing tabs does not need a page reload.  The main
    advantage of this change is that when settings are saved, all settings
    are saved. Even those that are not in the active tab. So you can make
    changes in the "General" tab, and without clicking "Save Changes",
    select the "Optional Modules" tab and make changes there. Then select
    another tab and make changes there.  Then if you click "Save Changes"
    all of those changes are saved.
    
    This is much more intuitive behavior, and what most users expect when
    seeing a tab layout such as is utilized on this page. As a result, those
    new to WeBWorK will often attempt to do what is described in the
    paragraph above, and then be surprised to see that the settings from the
    not visible tabs are all reset to what they were before the changes the
    instructor believed were just changed.
    
    Although this is natural behavior I added a note in the help about this
    behavior.  This note is probably more for those that have been using
    WeBWorK, and expect the old behavior.
    
    Note that this is built on top of #2392 to avoid needing to resolve
    conflicts with that pull request later.

--- lib/WeBWorK/ContentGenerator/Instructor/Config.pm
+++ lib/WeBWorK/ContentGenerator/Instructor/Config.pm
@@ -44,39 +44,14 @@ sub writeFile ($outputFilePath, $contents) {
 	if (open my $OUTPUTFILE, '>:encoding(UTF-8)', $outputFilePath) {
 		print $OUTPUTFILE $contents;
 		close $OUTPUTFILE;
-	} else {
-		return (
-			"I could not open $outputFilePath",
-			'We will not be able to make configuration changes unless the permissions '
-				. 'are set so that the web server can write to this file.'
-		);
+		return 1;
 	}
-
-	return;
+	return 0;
 }
 
 # Make a new config object from data
 sub objectify ($c, $data) {
 	return "WeBWorK::ConfigObject::$data->{type}"->new($data, $c);
-}
-
-sub generate_navigation_tabs ($c, $current_tab, @tab_names) {
-	my $tabs = $c->c;
-	for my $tab (0 .. (scalar(@tab_names) - 1)) {
-		if ($current_tab eq "tab$tab") {
-			push(@$tabs, $c->tag('span', class => 'nav-link active', $c->maketext($tab_names[$tab])));
-		} else {
-			push(
-				@$tabs,
-				$c->link_to(
-					$c->maketext($tab_names[$tab]) =>
-						$c->systemLink($c->url_for, params => { section_tab => "tab$tab" }),
-					class => 'nav-link'
-				)
-			);
-		}
-	}
-	return $c->tag('nav', class => 'config-tabs nav nav-pills justify-content-center my-4', $tabs->join(''));
 }
 
 sub pre_header_initialize ($c) {
@@ -89,7 +64,7 @@ sub pre_header_initialize ($c) {
 
 	if ($c->param('make_changes')) {
 		# Get a copy of the course environment which does not have simple.conf loaded
-		my $ce3 = WeBWorK::CourseEnvironment->new({
+		my $ce2 = WeBWorK::CourseEnvironment->new({
 			courseName          => $ce->{courseName},
 			web_config_filename => 'noSuchFilePlease'
 		});
@@ -103,37 +78,39 @@ sub pre_header_initialize ($c) {
 
 			END_SIMPLE_CONF_HEADER
 
-		# Get the number of the current tab
-		my $tab = ($c->param('section_tab') || 'tab0') =~ s/tab//r;
-
-		# We completely rewrite the simple configuration file, so we need to go through all sections.
+		# Write all settings that are different from the default to the simple configuration file.
 		for my $configSection (@{ $c->stash->{configValues} }) {
 			my @configSectionArray = @$configSection;
 			shift @configSectionArray;
 			for my $con (@configSectionArray) {
 				my $conobject = $c->objectify($con);
-				if (
-					$tab
-					|| (defined $ce->{permissionLevels}{"change_config_$con->{var}"}
-						&& !$c->authz->hasPermissions($c->param('user'), "change_config_$con->{var}"))
-					)
+				if (defined $ce->{permissionLevels}{"change_config_$con->{var}"}
+					&& !$c->authz->hasPermissions($c->param('user'), "change_config_$con->{var}"))
 				{
-					# This configuration value is on a hidden tab or the user does not have permission to change this
-					# configuration value.  So use the current course environment value.
-					$fileoutput .= $conobject->save_string($conobject->get_value($ce3), 1);
+					# The user does not have permission to change this configuration value.
+					# So use the current course environment value.
+					$fileoutput .= $conobject->save_string($conobject->get_value($ce2), 1);
 				} else {
-					# The visible tab contains this configuration value and the user has permission to change it.
-					$fileoutput .= $conobject->save_string($conobject->get_value($ce3));
+					$fileoutput .= $conobject->save_string($conobject->get_value($ce2));
 				}
 			}
-			$tab--;
 		}
 
-		my @write_result = writeFile("$ce->{courseDirs}{root}/simple.conf", $fileoutput);
-		if (@write_result) {
-			$c->addbadmessage($c->c(@write_result)->join($c->tag('br')));
-		} else {
+		if (writeFile("$ce->{courseDirs}{root}/simple.conf", $fileoutput)) {
 			$c->addgoodmessage($c->maketext('Changes saved.'));
+		} else {
+			$c->addbadmessage(
+				$c->c(
+					$c->tag('div', $c->maketext('Unable to open the "~[COURSEROOT~]/simple.conf" file.')),
+					$c->tag(
+						'div',
+						$c->maketext(
+							'Contact the site administrator to ensure that the permissions '
+								. 'are set so that the web server can write to this file.'
+						)
+					)
+				)->join('')
+			);
 		}
 	}
 
