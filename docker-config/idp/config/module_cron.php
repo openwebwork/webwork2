819b5e5ae870836ad2fcd201eb0d22ffbb293651
John Hsu <john.hsu@ubc.ca>
2024-08-14 11:36:29 -0700

    FIX docker-config/idp whitespace reformat
    Reformatted to follow .editorconfig rules.

--- docker-config/idp/config/module_cron.php
+++ docker-config/idp/config/module_cron.php
@@ -1,8 +1,8 @@
 <?php
 
 $config = [
-    'key' => 'healthcheck',
-    'allowed_tags' => ['docker'],
-    'debug_message' => true,
-    'sendemail' => false,
+	'key' => 'healthcheck',
+	'allowed_tags' => ['docker'],
+	'debug_message' => true,
+	'sendemail' => false,
 ];
