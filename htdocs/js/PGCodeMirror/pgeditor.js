918c551b3a73bbed0e297de57fea84a527a9659d
Glenn Rice <grice1@missouriwestern.edu>
2024-10-05 22:12:10 -0500

    Upgrade to CodeMirror 6.
    Some features of this change are improved syntax highlighting and
    the addition of autocompletion.  To see available autocompletions at the
    current cursor location, you can type Ctrl-Space.  In many cases,
    autocompletions are offered automatically when certain things have been
    typed.
    
    CodeMirror 6 can not be used via script tags.  So a PGProblemEditor
    package has been created for this.  The repository for that is
    https://github.com/openwebwork/pg-problem-editor.  An initial package
    has been published in npm at
    https://www.npmjs.com/package/@openwebwork/pg-codemirror-editor. The
    real work is done in the codemirror-lang-pg package that adds support
    for the PG language in CodeMirror 6.  The repository for that package is
    https://github.com/openwebwork/codemirror-lang-pg, and the npm package
    at https://www.npmjs.com/package/@openwebwork/codemirror-lang-pg.
    
    The pg-problem-editor package is designed so that it can be used by
    webwork2, the standalone renderer, and possible others that want a PG
    problem editor.  It may need a few tweaks to make it work for webwork3
    in the future, but should also work for that.
    
    The codemirror-lang-pg package consists of a base PG parser that is
    largely derived from the codemirror-lang-perl package (at
    https://github.com/drgrice1/codemirror-lang-perl and
    https://www.npmjs.com/package/codemirror-lang-perl) with a modifications
    specific to the perl available in a PG problem.  The main differences
    are that its escape sequence is ~~, much of what can not be used in
    the safe compartment has been stripped out, and there are special blocks
    for PGML, PG text, and LaTeX image code.  Then there are two other
    parsers.  Those are the PGML and PG text parsers.  Those parsers run on
    the contents of the PGML and PG text blocks found by the base PG parser.
    Note that the PGML parser is actually a javascript implementation of the
    parser in PGML.pl with some modifications to track position and things
    needed to associate the parsed tree with the original code.
    
    Note that the configuration of theme, keymaps, and such are now part of
    the pg-codemirror-editor, and are shown in a panel of the editor
    instead of below as before.
    
    Emacs and Vim keymaps are available, but at this point there is not a
    Sublime keymap for CodeMirror 6.
    
    The themes that are available include a basic default theme (that is
    part of the pg-problem-editor package and just adds some syntax
    highlighting that is missing from the CodeMirror 6 default), the OndDark
    theme that is the only other theme provided by the CodeMirror 6
    developers directly, the themes in the thememirror package, and the
    themes from https://github.com/craftzdog/cm6-themes which are in several
    separate npm packages.  Note that both thememirror and
    craftzdog/cm6-themes provide a solarized light theme, and that is why
    there are two of those.  They are slightly different though.
    
    I am sure that there is a lot more work to do on this, and there are
    other things available with CodeMirror 6 that could probably be
    implemented.  In any case, this is a good start.

--- htdocs/js/PGCodeMirror/pgeditor.js
+++ htdocs/js/PGCodeMirror/pgeditor.js
@@ -12,137 +12,18 @@
  * Artistic License for more details.
  */
 
-(async function () {
-	if (!CodeMirror) return;
+(async () => {
+	const editorContainer = document.querySelector('.code-mirror-editor');
+	if (!PGCodeMirrorEditor || !editorContainer) return;
 
-	const loadResource = async (src) => {
-		return new Promise((resolve, reject) => {
-			let shouldAppend = false;
-			let el;
-			if (/\.js(?:\?[0-9a-zA-Z=^.]*)?$/.exec(src)) {
-				el = document.querySelector(`script[src="${src}"]`);
-				if (!el) {
-					el = document.createElement('script');
-					el.async = false;
-					el.src = src;
-					shouldAppend = true;
-				}
-			} else if (/\.css(?:\?[0-9a-zA-Z=^.]*)?$/.exec(src)) {
-				el = document.querySelector(`link[href="${src}"]`);
-				if (!el) {
-					el = document.createElement('link');
-					el.rel = 'stylesheet';
-					el.href = src;
-					shouldAppend = true;
-				}
-			} else {
-				reject();
-				return;
-			}
+	const editorInput = document.getElementsByName(editorContainer.id)[0];
 
-			if (el.dataset.loaded) {
-				resolve();
-				return;
-			}
+	const cm = (webworkConfig.pgCodeMirror = new PGCodeMirrorEditor.View(editorContainer, {
+		source: editorInput?.value ?? '',
+		language: editorContainer.dataset.language ?? 'pg'
+	}));
 
-			el.addEventListener('error', reject);
-			el.addEventListener('abort', reject);
-			el.addEventListener('load', () => {
-				if (el) el.dataset.loaded = 'true';
-				resolve();
-			});
+	new ResizeObserver(() => cm.refresh('window-resize')).observe(editorContainer);
 
-			if (shouldAppend) document.head.appendChild(el);
-		});
-	};
-
-	const loadConfig = async (file) => {
-		const configName =
-			[...file.matchAll(/.*\/([^.]*?)(?:\.min)?\.(?:js|css)(?:\?[0-9a-zA-Z=^.]*)?$/g)][0]?.[1] ?? 'default';
-		if (configName !== 'default') {
-			try {
-				await loadResource(file);
-			} catch {
-				return 'default';
-			}
-		}
-		return configName;
-	};
-
-	const mode = document.querySelector('.codeMirrorEditor')?.dataset.mode ?? 'PG';
-	const options = {
-		mode,
-		indentUnit: 4,
-		tabMode: 'spaces',
-		lineNumbers: true,
-		lineWrapping: true,
-		extraKeys: {
-			Tab: (cm) => cm.execCommand('insertSoftTab'),
-			'Shift-Ctrl-[': (cm) => cm.foldCode(cm.getCursor(), { scanUp: true }),
-			'Cmd-Alt-[': (cm) => cm.foldCode(cm.getCursor(), { scanUp: true }),
-			'Ctrl-Alt-[': (cm) => CodeMirror.commands.foldAll(cm),
-			'Ctrl-Alt-]': (cm) => CodeMirror.commands.unfoldAll(cm)
-		},
-		highlightSelectionMatches: { annotateScrollbar: true },
-		matchBrackets: true,
-		inputStyle: 'contenteditable',
-		spellcheck: localStorage.getItem('WW_PGEditor_spellcheck') === 'true',
-		gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter']
-	};
-
-	if (mode === 'PG') {
-		options.extraKeys['Ctrl-/'] = (cm) => cm.execCommand('toggleComment');
-		options.extraKeys['Cmd-/'] = (cm) => cm.execCommand('toggleComment');
-		options.foldGutter = { rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.PG) };
-		options.fold = 'PG';
-	} else {
-		options.foldGutter = true;
-	}
-
-	const cm = (webworkConfig.pgCodeMirror = CodeMirror.fromTextArea(
-		document.querySelector('.codeMirrorEditor'),
-		options
-	));
-	cm.setSize('100%', '550px');
-
-	// Refresh the CodeMirror instance anytime the containing div resizes so that if line wrapping changes,
-	// the mouse cursor will still go to the correct place when the user clicks on the CodeMirror window.
-	new ResizeObserver(() => cm.refresh()).observe(document.querySelector('.CodeMirror'));
-
-	const currentThemeFile = localStorage.getItem('WW_PGEditor_selected_theme') ?? 'default';
-	const currentThemeName = await loadConfig(currentThemeFile);
-	cm.setOption('theme', currentThemeName);
-
-	const currentKeymapFile = localStorage.getItem('WW_PGEditor_selected_keymap') ?? 'default';
-	const currentKeymapName = await loadConfig(currentKeymapFile);
-	cm.setOption('keyMap', currentKeymapName);
-
-	const selectTheme = document.getElementById('selectTheme');
-	selectTheme.value = currentThemeName === 'default' ? 'default' : currentThemeFile;
-	selectTheme.addEventListener('change', async () => {
-		const themeName = await loadConfig(selectTheme.value);
-		cm.setOption('theme', themeName);
-		localStorage.setItem('WW_PGEditor_selected_theme', themeName === 'default' ? 'default' : selectTheme.value);
-	});
-
-	const selectKeymap = document.getElementById('selectKeymap');
-	selectKeymap.value = currentKeymapName === 'default' ? 'default' : currentKeymapFile;
-	selectKeymap.addEventListener('change', async () => {
-		const keymapName = await loadConfig(selectKeymap.value);
-		cm.setOption('keyMap', keymapName);
-		localStorage.setItem('WW_PGEditor_selected_keymap', keymapName === 'default' ? 'default' : selectKeymap.value);
-	});
-
-	const enableSpell = document.getElementById('enableSpell');
-	enableSpell.checked = localStorage.getItem('WW_PGEditor_spellcheck') === 'true';
-	enableSpell.addEventListener('change', () => {
-		cm.setOption('spellcheck', enableSpell.checked);
-		localStorage.setItem('WW_PGEditor_spellcheck', enableSpell.checked);
-		cm.focus();
-	});
-
-	const forceRTL = document.getElementById('forceRTL');
-	forceRTL.addEventListener('change', () => {
-		cm.setOption('direction', forceRTL.checked ? 'rtl' : 'ltr');
-	});
+	editorInput?.form.addEventListener('submit', () => (editorInput.value = cm.source));
 })();
