f7df5cb639f4b815c4c8d683b32a247a6f81866a
Peter Staab <peter.staab@gmail.com>
2025-05-28 09:40:59 -0400

    run prettier-format

--- htdocs/js/SampleProblemViewer/search.js
+++ htdocs/js/SampleProblemViewer/search.js
@@ -3,7 +3,6 @@
 	about every page to be searched (macro POD and sample problems).
 */
 (() => {
-
 	// ChatGPT generated throttle function similar to Lodash.
 	function throttle(func, wait) {
 		let lastCallTime = 0;
@@ -18,23 +17,23 @@
 			lastContext = this;
 
 			if (remaining <= 0 || remaining > wait) {
-			if (timeout) {
-				clearTimeout(timeout);
-				timeout = null;
-			}
-			lastCallTime = now;
-			func.apply(lastContext, lastArgs);
-			} else if (!timeout) {
-			timeout = setTimeout(() => {
-				lastCallTime = Date.now();
-				timeout = null;
+				if (timeout) {
+					clearTimeout(timeout);
+					timeout = null;
+				}
+				lastCallTime = now;
 				func.apply(lastContext, lastArgs);
-			}, remaining);
+			} else if (!timeout) {
+				timeout = setTimeout(() => {
+					lastCallTime = Date.now();
+					timeout = null;
+					func.apply(lastContext, lastArgs);
+				}, remaining);
 			}
 		};
 	}
 
-	const miniSearch = new MiniSearch({fields: ['terms', 'filename', 'name', 'description']});
+	const miniSearch = new MiniSearch({ fields: ['terms', 'filename', 'name', 'description'] });
 	let pages;
 	// This is the data from sample-problems/macros POD.
 	fetch('../../DATA/search.json')
@@ -45,24 +44,23 @@
 		})
 		.catch((e) => console.error(e));
 
-	const resultList = document.getElementById("searchResults");
-	const searchBox = document.getElementById("searchDocs");
-	document.getElementById("clearSearchButton").addEventListener('click', () => {
+	const resultList = document.getElementById('searchResults');
+	const searchBox = document.getElementById('searchDocs');
+	document.getElementById('clearSearchButton').addEventListener('click', () => {
 		searchBox.value = '';
 		resultList.innerHTML = '';
 	});
-
 
 	const search = throttle(() => {
 		const results = miniSearch.search(searchBox.value);
 		const ids = results.map((p) => p.id);
 
 		resultList.innerHTML = '';
-		ids.forEach(id => {
-			const item = document.createElement("div");
-			item.classList.add("card");
-			const p = pages[id-1];
-			const file = p.filename.replace('.pg','');
+		ids.forEach((id) => {
+			const item = document.createElement('div');
+			item.classList.add('card');
+			const p = pages[id - 1];
+			const file = p.filename.replace('.pg', '');
 			const path = p.type == 'sample problem' ? 'sampleproblems' : p.type == 'macro' ? 'pod' : '';
 
 			// This is the search results for each page.
@@ -75,12 +73,9 @@
 				</div>
 				`;
 
-			resultList.appendChild(item)
+			resultList.appendChild(item);
 		});
-
 	}, 250);
 
 	searchBox.addEventListener('keypress', search);
-
-
 })();
