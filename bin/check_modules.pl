a344b081d81aa4e131540cb6d517c22c40d8094a
Glenn Rice <grice1@missouriwestern.edu>
2025-06-13 08:06:33 -0500

    Rework check_modules.pl.
    The script now uses the settings in the course environment for external
    programs and to check the database driver (DBD::mysql or DBD::MariaDB).
    In order for that to work, first the script checks that all of the
    needed modules are found, load, and are at the sufficient version. That
    is except for the database driver module.  If any are not found, then
    the script exits with a message stating that it is unable to continue
    without the required modules.
    
    If all modules are good, then the script checks the database driver
    module next. For this it "runtime" loads the course environment and its
    dependencies (as well as a runtime detection of the webwork2 root and
    lib directories).
    
    This is to address issue #2740.
    
    In addition the following modules are removed that are no longer needed:
    
    * Array::Utils
    * Email::Sender::Simple - still used but is a dependency of Email::Stuffer and doesn't need to be checked separately
    * HTML::Tagset
    * HTML::Template
    * IO::Socket::SSL
    * Net::LDAPS
    * Net::SMPT
    * Net::SSLeay
    * PadWalker
    * Path::Class
    * Safe - we use our local version (WWSafe.pm) and still check Opcode which it uses
    * Statistics::R::IO
    * Template
    
    `npm` is added to the list of required applications.
    
    Most of these things were noted in issue #1917.

--- bin/check_modules.pl
+++ bin/check_modules.pl
@@ -1,20 +1,5 @@
 #!/usr/bin/env perl
 
-################################################################################
-# WeBWorK Online Homework Delivery System
-# Copyright &copy; 2000-2024 The WeBWorK Project, https://github.com/openwebwork
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of either: (a) the GNU General Public License as published by the
-# Free Software Foundation; either version 2, or (at your option) any later
-# version, or (b) the "Artistic License" which comes with this package.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See either the GNU General Public License or the
-# Artistic License for more details.
-################################################################################
-
 =head1 NAME
 
 check_modules.pl - Check to ensure that applications and perl modules needed by
