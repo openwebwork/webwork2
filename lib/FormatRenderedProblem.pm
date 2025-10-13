d67fcb106b526d399f117497455d682fdc53ff72
Glenn Rice <grice1@missouriwestern.edu>
2025-10-13 16:04:25 -0500

    Update MathJax to version 4.0.
    For the most part, there isn't much that is needed.  The javascript
    files are no longer in the `es5` subdirectory, and the workaround for
    changing the math renderer was removed (as noted in the comment above
    it).
    
    The `mathInteraction.pg` problem in the student orientation has been
    updated, although further updates may be needed. The screen shots the
    problem uses were updated, but someone else might need to create better
    screen shots.  Mine seem to have come out quite a bit smaller than the
    previous ones.
    
    I removed the rather old examples of the `$pg{specialPGEnvironmentVars}{problemPreamble}`
     and `$pg{specialPGEnvironmentVars}{problemPostamble}` variables in
    `defaults.config` and `localOverrides.conf.dist` that include examples
    of doing things with MathJax that weren't even valid for MathJax 3. I
    also made the deprecation statement for that variables stronger.
    
    There is a minor css tweak in `system.scss` to ensure that the MathJax
    expression eplorer help dialog is above the masthead and stick problem
    navigation bar.
    
    Further modification to the MathJax configuration (in
    `htdocs/js/MathJaxConfig/mathjax-config.js`) may be needed as per the
    discussion in issue #2828.  But that can wait until we have more
    information.

--- lib/FormatRenderedProblem.pm
+++ lib/FormatRenderedProblem.pm
@@ -90,12 +90,12 @@ sub formatRenderedProblem {
 		[ 'node_modules/jquery/dist/jquery.min.js',                            0, {} ],
 		[ 'node_modules/jquery-ui-dist/jquery-ui.min.js',                      0, {} ],
 		[ 'node_modules/iframe-resizer/js/iframeResizer.contentWindow.min.js', 0, {} ],
-		[ 'js/MathJaxConfig/mathjax-config.js',                     0, { defer => undef } ],
-		[ 'node_modules/mathjax/es5/tex-svg.js',                    0, { defer => undef, id => 'MathJax-script' } ],
-		[ 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 0, { defer => undef } ],
-		[ 'js/Problem/problem.js',                                  0, { defer => undef } ],
-		[ 'js/System/system.js',                                    0, { defer => undef } ],
-		[ 'math4-overrides.js',                                     1, { defer => undef } ]
+		[ 'js/MathJaxConfig/mathjax-config.js',                                0, { defer => undef } ],
+		[ 'node_modules/mathjax/tex-svg.js',                                   0, { defer => undef } ],
+		[ 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',            0, { defer => undef } ],
+		[ 'js/Problem/problem.js',                                             0, { defer => undef } ],
+		[ 'js/System/system.js',                                               0, { defer => undef } ],
+		[ 'math4-overrides.js',                                                1, { defer => undef } ]
 	);
 
 	# Get the requested format.
