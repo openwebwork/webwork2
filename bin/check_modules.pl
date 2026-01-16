fd230a273556d4363dd1caf8ead2d8c524b208e7
Danny Glin <dlglin@ucalgary.ca>
2026-01-16 16:34:10 -0700

    Change list of perl modules from array to hash

--- bin/check_modules.pl
+++ bin/check_modules.pl
@@ -30,90 +30,258 @@ use feature 'say';
 use Getopt::Long qw(:config bundling);
 use Pod::Usage;
 
-my @modulesList = qw(
-	Archive::Tar
-	Archive::Zip
-	Archive::Zip::SimpleZip
-	Benchmark
-	Carp
-	Class::Accessor
-	Crypt::JWT
-	Crypt::PK::RSA
-	Data::Dump
-	Data::Dumper
-	Data::Structure::Util
-	Data::UUID
-	Date::Format
-	Date::Parse
-	DateTime
-	DBI
-	Digest::MD5
-	Digest::SHA
-	Email::Address::XS
-	Email::Sender::Transport::SMTP
-	Email::Stuffer
-	Errno
-	Exception::Class
-	File::Copy
-	File::Copy::Recursive
-	File::Fetch
-	File::Find
-	File::Find::Rule
-	File::Path
-	File::Spec
-	File::stat
-	File::Temp
-	Future::AsyncAwait
-	GD
-	GD::Barcode::QRcode
-	Getopt::Long
-	Getopt::Std
-	HTML::Entities
-	HTTP::Async
-	IO::File
-	Iterator
-	Iterator::Util
-	Locale::Maketext::Lexicon
-	Locale::Maketext::Simple
-	LWP::Protocol::https
-	MIME::Base32
-	MIME::Base64
-	Math::Random::Secure
-	Minion
-	Minion::Backend::SQLite
-	Mojolicious
-	Mojolicious::Plugin::NotYAMLConfig
-	Mojolicious::Plugin::RenderFile
-	Net::IP
-	Net::OAuth
-	Opcode
-	Pandoc
-	Perl::Critic
-	Perl::Tidy
-	PHP::Serialization
-	Pod::Simple::Search
-	Pod::Simple::XHTML
-	Pod::Usage
-	Pod::WSDL
-	Scalar::Util
-	SOAP::Lite
-	Socket
-	SQL::Abstract
-	String::ShellQuote
-	SVG
-	Text::CSV
-	Text::Wrap
-	Tie::IxHash
-	Time::HiRes
-	Time::Zone
-	Types::Serialiser
-	URI::Escape
-	UUID::Tiny
-	XML::LibXML
-	XML::Parser
-	XML::Parser::EasyTree
-	XML::Writer
-	YAML::XS
+my %modulesList = (
+	'Archive::Tar' => {
+		package => { deb => 'perl-modules', },
+	},
+	'Archive::Zip' => {
+		package => { deb => 'libarchive-zip-perl', },
+	},
+	'Archive::Zip::SimpleZip' => {},
+	'Benchmark' => {
+		package => { deb => 'perl-modules', },
+	},
+	'Carp' => {
+		package => { deb => 'perl-modules', },
+	},
+	'Class::Accessor' => {
+		package => { deb => 'libclass-accessor-perl', },
+	},
+	'Crypt::JWT' => {
+		package => { deb => 'libcrypt-jwt-perl', },
+	},
+	'Crypt::PK::RSA' => {
+		package => { deb => 'libcryptx-perl', },
+	},
+	'Data::Dump' => {
+		package => { deb => 'libdata-dump-perl', },
+	},
+	'Data::Dumper' => {
+		package => { deb => 'libperl', },
+	},
+	'Data::Structure::Util' => {
+		package => { deb => 'libdata-structure-util-perl', },
+	},
+	'Data::UUID' => {
+		package => { deb => 'libossp-uuid-perl', },
+	},
+	'Date::Format' => {
+		package => { deb => 'libtimedate-perl', },
+	},
+	'Date::Parse' => {
+		package => { deb => 'libtimedate-perl', },
+	},
+	'DateTime' => {
+		package => { deb => 'libdatetime-perl', },
+	},
+	'DBI' => {
+		package => { deb => 'libdbi-perl', },
+	},
+	'Digest::MD5' => {
+		package => { deb => 'libperl', },
+	},
+	'Digest::SHA' => {
+		package => { deb => 'libperl', },
+	},
+	'Email::Address::XS' => {
+		package => { deb => 'libemail-address-xs-perl', },
+	},
+	'Email::Sender::Transport::SMTP' => {
+		package => { deb => 'libemail-sender-perl', },
+	},
+	'Email::Stuffer' => {
+		package => { deb => 'libemail-stuffer-perl', },
+	},
+	'Errno' => {
+		package => { deb => 'libperl', },
+	},
+	'Exception::Class' => {
+		package => { deb => 'libexception-class-perl', },
+	},
+	'File::Copy' => {
+		package => { deb => 'perl-modules', },
+	},
+	'File::Copy::Recursive' => {
+		package => { deb => 'libfile-copy-recursive-perl', },
+	},
+	'File::Fetch' => {
+		package => { deb => 'perl-modules', },
+	},
+	'File::Find' => {
+		package => { deb => 'perl-modules', },
+	},
+	'File::Find::Rule' => {
+		package => { deb => 'libfile-find-rule-perl', },
+	},
+	'File::Path' => {
+		package => { deb => 'perl-modules', },
+	},
+	'File::Spec' => {
+		package => { deb => 'perl-base', },
+	},
+	'File::stat' => {
+		package => { deb => 'perl-modules', },
+	},
+	'File::Temp' => {
+		package => { deb => 'perl-modules', },
+	},
+	'Future::AsyncAwait' => {
+		package => { deb => 'libfuture-asyncawait-perl', },
+		minversion => 0.52,
+	},
+	'GD' => {
+		package => { deb => 'libgd-perl', },
+	},
+	'GD::Barcode::QRcode' => {
+		package => { deb => 'libgd-barcode-perl', },
+	},
+	'Getopt::Long' => {
+		package => { deb => 'perl-modules', },
+	},
+	'Getopt::Std' => {
+		package => { deb => 'perl-modules', },
+	},
+	'HTML::Entities' => {
+		package => { deb => 'libhtml-parser-perl', },
+	},
+	'HTTP::Async' => {
+		package => { deb => 'libhttp-async-perl', },
+	},
+	'IO::File' => {
+		package => { deb => 'perl-base', },
+	},
+	'Iterator' => {
+		package => { deb => 'libiterator-perl', },
+	},
+	'Iterator::Util' => {
+		package => { deb => 'libiterator-util-perl', },
+	},
+	'Locale::Maketext::Lexicon' => {
+		package => { deb => 'liblocale-maketext-lexicon-perl', },
+	},
+	'Locale::Maketext::Simple' => {
+		package => { deb => 'perl-modules', },
+	},
+	'LWP::Protocol::https' => {
+		package => { deb => 'liblwp-protocol-https-perl', },
+		minversion => 6.06,
+	},
+	'MIME::Base32' => {
+		package => { deb => 'libmime-base32-perl', },
+	},
+	'MIME::Base64' => {
+		package => { deb => 'libperl', },
+	},
+	'Math::Random::Secure' => {
+		package => { deb => 'libmath-random-secure-perl', },
+	},
+	'Minion' => {
+		package => { deb => 'libminion-perl', },
+	},
+	'Minion::Backend::SQLite' => {
+		package => { deb => 'libminion-backend-sqlite-perl', },
+	},
+	'Mojolicious' => {
+		package => { deb => 'libmojolicious-perl', },
+		minversion => 9.34,
+	},
+	'Mojolicious::Plugin::NotYAMLConfig' => {
+		package => { deb => 'libmojolicious-perl', },
+	},
+	'Mojolicious::Plugin::RenderFile' => {
+		package => { deb => 'libmojolicious-plugin-renderfile-perl', },
+	},
+	'Net::IP' => {
+		package => { deb => 'libnet-ip-perl', },
+	},
+	'Net::OAuth' => {
+		package => { deb => 'libnet-oauth-perl', },
+	},
+	'Opcode' => {
+		package => { deb => 'libperl', },
+	},
+	'Pandoc' => {
+		package => { deb => 'libpandoc-wrapper-perl', },
+	},
+	'Perl::Critic' => {
+		package => { deb => 'libperl-critic-perl', },
+	},
+	'Perl::Tidy' => {
+		package => { deb => 'perltidy', },
+	},
+	'PHP::Serialization' => {
+		package => { deb => 'libphp-serialization-perl', },
+	},
+	'Pod::Simple::Search' => {
+		package => { deb => 'perl-modules', },
+	},
+	'Pod::Simple::XHTML' => {
+		package => { deb => 'perl-modules', },
+	},
+	'Pod::Usage' => {
+		package => { deb => 'perl-modules', },
+	},
+	'Pod::WSDL' => {
+		package => { deb => 'libpod-wsdl-perl', },
+	},
+	'Scalar::Util' => {
+		package => { deb => 'perl-base', },
+	},
+	'SOAP::Lite' => {
+		package => { deb => 'libsoap-lite-perl', },
+	},
+	'Socket' => {
+		package => { deb => 'perl-base', },
+	},
+	'SQL::Abstract' => {
+		package => { deb => 'libsql-abstract-perl', },
+		minversion => 2.000000,
+	},
+	'String::ShellQuote' => {
+		package => { deb => 'libstring-shellquote-perl', },
+	},
+	'SVG' => {
+		package => { deb => 'libsvg-perl', },
+	},
+	'Text::CSV' => {
+		package => { deb => 'libtext-csv-perl', },
+	},
+	'Text::Wrap' => {
+		package => { deb => 'perl-base', },
+	},
+	'Tie::IxHash' => {
+		package => { deb => 'libtie-ixhash-perl', },
+	},
+	'Time::HiRes' => {
+		package => { deb => 'libperl', },
+	},
+	'Time::Zone' => {
+		package => { deb => 'libtimedate-perl', },
+	},
+	'Types::Serialiser' => {
+		package => { deb => 'libtypes-serialiser-perl', },
+	},
+	'URI::Escape' => {
+		package => { deb => 'liburi-perl', },
+	},
+	'UUID::Tiny' => {
+		package => { deb => 'libuuid-tiny-perl', },
+	},
+	'XML::LibXML' => {
+		package => { deb => 'libxml-libxml-perl', },
+	},
+	'XML::Parser' => {
+		package => { deb => 'libxml-parser-perl', },
+	},
+	'XML::Parser::EasyTree' => {
+		package => { deb => 'libxml-parser-easytree-perl', },
+	},
+	'XML::Writer' => {
+		package => { deb => 'libxml-writer-perl', },
+	},
+	'YAML::XS' => {
+		package => { deb => 'libyaml-libyaml-perl', },
+	},
 );
 
 my %moduleVersion = (
@@ -196,7 +364,7 @@ sub check_modules {
 		use strict 'refs';
 	};
 
-	for my $module (@modulesList) {
+	for my $module (keys(%modulesList)) {
 		$checkModule->($module);
 	}
 
